72b8c5e0d4f00a7d29aedc6b44688cd7439729bf
Mathieu Amiot <amiot.mathieu@gmail.com>
2022-01-19 19:39:52 +0100

    feat(crypto-ffi): Generate Swift & Kotlin bindings 🎉

--- crypto-ffi/build.rs
+++ crypto-ffi/build.rs
@@ -1,4 +1,32 @@
+const UDL_FILE: &str = "./src/CoreCrypto.udl";
+
 fn main() {
-    // FIXME: That ain't it chief just yet
-    uniffi_build::generate_scaffolding("./src/CoreCrypto.udl").unwrap();
+    // Target aliases
+    #[cfg(any(
+        target = "aarch64-apple-ios",
+        target = "aarch64-apple-ios-sim",
+        target = "x86_64-apple-ios",
+    ))]
+    println!("cargo:rustc-cfg=ios");
+
+    #[cfg(any(
+        target = "wasm32-unknown-unknown",
+        target = "wasm32-wasi",
+        target = "wasm32-unknown-emscripten"
+    ))]
+    println!("cargo:rustc-cfg=wasm");
+
+    #[cfg(any(
+        target = "aarch64-linux-android",
+        target = "arm-linux-androideabi",
+        target = "armv7-linux-androideabi",
+        target = "i686-linux-android",
+        target = "thumbv7neon-linux-androideabi",
+        target = "x86_64-linux-android",
+    ))]
+    println!("cargo:rustc-cfg=android");
+
+    uniffi_build::generate_scaffolding(UDL_FILE).unwrap();
+    uniffi_bindgen::generate_bindings(UDL_FILE, None, vec!["kotlin"], Some("./bindings/kt/"), false).unwrap();
+    uniffi_bindgen::generate_bindings(UDL_FILE, None, vec!["swift"], Some("./bindings/swift/"), false).unwrap();
 }
