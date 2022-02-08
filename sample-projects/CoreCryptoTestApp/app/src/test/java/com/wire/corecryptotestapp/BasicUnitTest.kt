8ee89d06581f18df86aef29e461388eb46f4eb9e
Mathieu Amiot <amiot.mathieu@gmail.com>
2022-02-08 17:47:02 +0100

    Added android test app

---
+++ sample-projects/CoreCryptoTestApp/app/src/test/java/com/wire/corecryptotestapp/BasicUnitTest.kt
@@ -0,0 +1,12 @@
+package com.wire.corecryptotestapp
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class BasicUnitTest {
+    @Test
+    fun version_Works() {
+        assertEquals(com.wire.core.version(), "0.1.0")
+    }
+}
