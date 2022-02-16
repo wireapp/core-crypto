9866e8e68d8729824dced0e5476a9895f00158e6
Paolo Capriotti <paolo@capriotti.io>
2022-02-16 10:33:02 +0100

    Set up CLI with clap

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -7,22 +7,16 @@ use openmls_rust_crypto::{OpenMlsRustCrypto};
 use openmls::prelude::{TlsSerializeTrait};
 use core_crypto::prelude::ClientId;
 
-use uuid::Uuid;
+use clap::{Parser, Subcommand};
 use std::io::Write;
 
-fn main() {
+fn key_package(client_id: &ClientId) {
     let backend = OpenMlsRustCrypto::default();
     let ciphersuite_name = CiphersuiteName::default();
     let ciphersuite = Ciphersuite::new(ciphersuite_name).unwrap();
 
-    let identity = ClientId::new(
-        Uuid::parse_str("b455a431-9db6-4404-86e7-6a3ebe73fcaf").unwrap(),
-        "mls.example.com".to_string(),
-        988119381
-    );
-
     let credentials = CredentialBundle::new(
-        identity.as_bytes(),
+        client_id.as_bytes(),
         CredentialType::Basic,
         ciphersuite.signature_scheme(),
         &backend,
@@ -39,4 +33,32 @@ fn main() {
     let mut kp_bytes = Vec::new();
     kp.tls_serialize(&mut kp_bytes).unwrap();
     std::io::stdout().write_all(&kp_bytes).unwrap();
+}
+
+
+#[derive(Parser)]
+#[derive(Debug)]
+#[clap(name = "crypto-cli")]
+struct Cli {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand)]
+#[derive(Debug)]
+enum Command {
+    KeyPackage {
+        client_id: ClientId
+    },
+}
+
+fn execute(cmd: &Command) {
+    match cmd {
+        Command::KeyPackage { client_id } => key_package(client_id),
+    }
+}
+
+fn main() {
+    let cli = Cli::parse();
+    execute(&cli.command);
 }
