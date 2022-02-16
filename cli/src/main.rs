9fa5511c6928a46e3293c40b68498bd626d777dd
Paolo Capriotti <paolo@capriotti.io>
2022-02-16 10:07:22 +0100

    Use `ClientId` structure from core-crypto

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -5,41 +5,42 @@ use openmls::{
 };
 use openmls_rust_crypto::{OpenMlsRustCrypto};
 use openmls::prelude::{TlsSerializeTrait};
+use core_crypto::prelude::ClientId;
 
 use uuid::Uuid;
 use std::io::Write;
 
-// copied from crypto::client
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct ClientId {
-    user_id: uuid::Uuid,
-    domain: String,
-    client_id: u64,
-}
+// // copied from crypto::client
+// #[derive(Debug, Clone, PartialEq, Eq)]
+// pub struct ClientId {
+//     user_id: uuid::Uuid,
+//     domain: String,
+//     client_id: u64,
+// }
 
-impl ClientId {
-    pub fn as_bytes(&self) -> Vec<u8> {
-        let mut ret = vec![];
-        ret.extend_from_slice(self.user_id.as_hyphenated().to_string().as_bytes());
-        ret.push(b':');
-        ret.extend_from_slice(format!("{:x}", self.client_id).as_bytes());
-        ret.push(b'@');
-        ret.extend_from_slice(self.domain.as_bytes());
+// impl ClientId {
+//     pub fn as_bytes(&self) -> Vec<u8> {
+//         let mut ret = vec![];
+//         ret.extend_from_slice(self.user_id.as_hyphenated().to_string().as_bytes());
+//         ret.push(b':');
+//         ret.extend_from_slice(format!("{:x}", self.client_id).as_bytes());
+//         ret.push(b'@');
+//         ret.extend_from_slice(self.domain.as_bytes());
 
-        ret
-    }
-}
+//         ret
+//     }
+// }
 
 fn main() {
     let backend = OpenMlsRustCrypto::default();
     let ciphersuite_name = CiphersuiteName::default();
     let ciphersuite = Ciphersuite::new(ciphersuite_name).unwrap();
 
-    let identity = ClientId {
-        user_id: Uuid::parse_str("b455a431-9db6-4404-86e7-6a3ebe73fcaf").unwrap(),
-        domain: "mls.example.com".to_string(),
-        client_id: 988119381,
-    };
+    let identity = ClientId::new(
+        Uuid::parse_str("b455a431-9db6-4404-86e7-6a3ebe73fcaf").unwrap(),
+        "mls.example.com".to_string(),
+        988119381
+    );
 
     let credentials = CredentialBundle::new(
         identity.as_bytes(),
