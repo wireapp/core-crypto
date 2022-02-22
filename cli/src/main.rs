3a65ad6a7221200f244821077f5c91725741d34f
Paolo Capriotti <paolo@capriotti.io>
2022-02-22 14:08:23 +0100

    Add public-key command

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -5,13 +5,18 @@ use mls_crypto_provider::*;
 use clap::{Parser, Subcommand};
 use std::io::Write;
 
-fn key_package(path: &String, enc_key: &String, client_id: ClientId) {
-    let backend = MlsCryptoProvider::try_new(path, enc_key).unwrap();
+fn key_package(backend: &MlsCryptoProvider, client_id: ClientId) {
     let mut client = Client::init(client_id, &backend).unwrap();
     let kp = client.gen_keypackage(&backend).unwrap();
     let mut kp_bytes = Vec::new();
     kp.tls_serialize(&mut kp_bytes).unwrap();
     std::io::stdout().write_all(&kp_bytes).unwrap();
+}
+
+fn public_key(backend: &MlsCryptoProvider, client_id: ClientId) {
+    let client = Client::init(client_id, &backend).unwrap();
+    let pk = client.public_key();
+    std::io::stdout().write_all(pk).unwrap();
 }
 
 #[derive(Parser)]
@@ -32,12 +37,17 @@ enum Command {
     KeyPackage {
         client_id: ClientId
     },
+    PublicKey {
+        client_id: ClientId
+    },
 }
 
 
 fn main() {
     let cli = Cli::parse();
+    let backend = MlsCryptoProvider::try_new(&cli.store, &cli.enc_key).unwrap();
     match cli.command {
-        Command::KeyPackage { client_id } => key_package(&cli.store, &cli.enc_key, client_id),
+        Command::KeyPackage { client_id } => key_package(&backend, client_id),
+        Command::PublicKey { client_id } => public_key(&backend, client_id),
     }
 }
