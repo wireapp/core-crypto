40ffcabd4a97eb92819cf09680753d4bdf3f2f47
Paolo Capriotti <paolo@capriotti.io>
2022-02-28 16:37:38 +0100

    Add member command

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -3,20 +3,23 @@ use core_crypto::prelude::*;
 use mls_crypto_provider::*;
 
 use clap::{Parser, Subcommand};
-use std::io::Write;
+use std::io;
+use std::fs;
+use io::Write;
+use io::Read;
 
 fn key_package(backend: &MlsCryptoProvider, client_id: ClientId) {
     let mut client = Client::init(client_id, &backend).unwrap();
     let kp = client.gen_keypackage(&backend).unwrap();
     let mut kp_bytes = Vec::new();
     kp.tls_serialize(&mut kp_bytes).unwrap();
-    std::io::stdout().write_all(&kp_bytes).unwrap();
+    io::stdout().write_all(&kp_bytes).unwrap();
 }
 
 fn public_key(backend: &MlsCryptoProvider, client_id: ClientId) {
     let client = Client::init(client_id, &backend).unwrap();
     let pk = client.public_key();
-    std::io::stdout().write_all(pk).unwrap();
+    io::stdout().write_all(pk).unwrap();
 }
 
 fn group(backend: &MlsCryptoProvider, client_id: ClientId, group_id: &[u8]) {
@@ -25,7 +28,15 @@ fn group(backend: &MlsCryptoProvider, client_id: ClientId, group_id: &[u8]) {
     let group_config = MlsConversationConfiguration::openmls_default_configuration();
     let kp_hash = client.keypackage_hash(&backend).unwrap();
     let mut group = MlsGroup::new(backend, &group_config, group_id, &kp_hash).unwrap();
-    group.save(&mut std::io::stdout()).unwrap();
+    group.save(&mut io::stdout()).unwrap();
+}
+
+fn add_member(backend: &MlsCryptoProvider, group_data: &mut dyn Read, mut kp_data: &mut dyn Read) {
+    let mut group = MlsGroup::load(group_data).unwrap();
+    let mut kp = KeyPackage::tls_deserialize(&mut kp_data).unwrap();
+    let (handshake, welcome) = group
+        .add_members(backend, &[kp]).unwrap();
+    handshake.tls_serialize(&mut io::stdout()).unwrap();
 }
 
 #[derive(Parser)]
@@ -51,10 +62,32 @@ enum Command {
     },
     Group {
         client_id: ClientId,
-        group_id: Vec<u8>,
+        group_id: String,
+    },
+    Member {
+        #[clap(subcommand)]
+        command: MemberCommand,
     }
 }
 
+#[derive(Subcommand)]
+#[derive(Debug)]
+enum MemberCommand {
+    Add {
+        #[clap(short, long)]
+        group: String,
+        key_package: String
+    }
+}
+
+fn path_reader(path: String) -> io::Result<Box<dyn Read>> {
+    if path == "-" {
+        Ok(Box::new(io::stdin()))
+    }
+    else {
+        Ok(Box::new(fs::File::open(path)?))
+    }
+}
 
 fn main() {
     let cli = Cli::parse();
@@ -62,6 +95,11 @@ fn main() {
     match cli.command {
         Command::KeyPackage { client_id } => key_package(&backend, client_id),
         Command::PublicKey { client_id } => public_key(&backend, client_id),
-        Command::Group { client_id, group_id } => group(&backend, client_id, &group_id),
+        Command::Group { client_id, group_id } => group(&backend, client_id, group_id.as_bytes()),
+        Command::Member { command: MemberCommand::Add { group, key_package } } => {
+            let mut group_data = path_reader(group).unwrap();
+            let mut kp_data = path_reader(key_package).unwrap();
+            add_member(&backend, &mut group_data, &mut kp_data);
+        },
     }
 }
