bdd0fcea3ee0f41293ecc904b4903e746a8e4e59
beltram <beltram.maldant@wire.com>
2022-06-15 09:49:28 +0200

    chore: fix cli compilation and update it a bit

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -8,8 +8,93 @@ use io::Write;
 use std::fs;
 use std::io;
 
+#[derive(Parser, Debug)]
+#[clap(name = "crypto-cli")]
+struct Cli {
+    /// Sqlcipher backing file
+    #[clap(short, long)]
+    store: String,
+    /// Sqlcipher passphrase
+    #[clap(short, long)]
+    enc_key: String,
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand, Debug)]
+enum Command {
+    /// Generate a new keypackage for existing or new client
+    KeyPackage { client_id: ClientId },
+    /// Get existing or new client signature key
+    PublicKey { client_id: ClientId },
+    /// Create a new MLS group
+    Group { client_id: ClientId, group_id: String },
+    /// Update an existing MLS group
+    Member {
+        /// Existing group id
+        #[clap(short, long)]
+        group: String,
+        #[clap(subcommand)]
+        command: MemberCommand,
+    },
+    /// Send a message in an MLS group
+    Message {
+        /// Existing group id
+        #[clap(short, long)]
+        group: String,
+        /// Message to send
+        text: String,
+    },
+}
+
+#[derive(Subcommand, Debug)]
+enum MemberCommand {
+    /// Add a new member to an existing group
+    Add {
+        /// New member TLS serialized KeyPackage
+        key_package: String,
+        /// Output for Welcome message
+        #[clap(short, long)]
+        welcome_out: Option<String>,
+    },
+}
+
+fn path_reader(path: String) -> io::Result<Box<dyn Read>> {
+    if path == "-" {
+        Ok(Box::new(io::stdin()))
+    } else {
+        Ok(Box::new(fs::File::open(path)?))
+    }
+}
+
+fn main() {
+    let cli = Cli::parse();
+    let backend = MlsCryptoProvider::try_new(&cli.store, &cli.enc_key).unwrap();
+    match cli.command {
+        Command::KeyPackage { client_id } => key_package(&backend, client_id),
+        Command::PublicKey { client_id } => public_key(&backend, client_id),
+        Command::Group { client_id, group_id } => group(&backend, client_id, group_id.as_bytes()),
+        Command::Member {
+            group,
+            command: MemberCommand::Add {
+                key_package,
+                welcome_out,
+            },
+        } => {
+            let mut group_data = path_reader(group).unwrap();
+            let mut kp_data = path_reader(key_package).unwrap();
+            let wel_data = welcome_out.map(|path| fs::File::create(path).unwrap());
+            add_member(&backend, &mut group_data, &mut kp_data, wel_data);
+        }
+        Command::Message { group, text } => {
+            let mut group_data = path_reader(group).unwrap();
+            app_message(&backend, &mut group_data, text);
+        }
+    }
+}
+
 fn key_package(backend: &MlsCryptoProvider, client_id: ClientId) {
-    let mut client = Client::init(client_id, &backend).unwrap();
+    let client = Client::init(client_id, &backend).unwrap();
     let kpb = client.gen_keypackage(&backend).unwrap();
     kpb.key_package().tls_serialize(&mut io::stdout()).unwrap();
 }
@@ -21,11 +106,11 @@ fn public_key(backend: &MlsCryptoProvider, client_id: ClientId) {
 }
 
 fn group(backend: &MlsCryptoProvider, client_id: ClientId, group_id: &[u8]) {
-    let mut client = Client::init(client_id, &backend).unwrap();
+    let client = Client::init(client_id, &backend).unwrap();
     let group_id = GroupId::from_slice(group_id);
     let group_config = MlsConversationConfiguration::openmls_default_configuration();
     let kp_hash = client.keypackage_hash(&backend).unwrap();
-    let mut group = MlsGroup::new(backend, &group_config, group_id, &kp_hash).unwrap();
+    let mut group = MlsGroup::new(backend, &group_config, group_id, kp_hash.as_slice()).unwrap();
     group.save(&mut io::stdout()).unwrap();
 }
 
@@ -48,84 +133,4 @@ fn app_message(backend: &MlsCryptoProvider, group_data: &mut dyn Read, text: Str
     let mut group = MlsGroup::load(group_data).unwrap();
     let message = group.create_message(backend, text.as_bytes()).unwrap();
     message.tls_serialize(&mut io::stdout()).unwrap();
-}
-
-#[derive(Parser, Debug)]
-#[clap(name = "crypto-cli")]
-struct Cli {
-    #[clap(short, long)]
-    store: String,
-    #[clap(short, long)]
-    enc_key: String,
-    #[clap(subcommand)]
-    command: Command,
-}
-
-#[derive(Subcommand, Debug)]
-enum Command {
-    KeyPackage {
-        client_id: ClientId,
-    },
-    PublicKey {
-        client_id: ClientId,
-    },
-    Group {
-        client_id: ClientId,
-        group_id: String,
-    },
-    Member {
-        #[clap(subcommand)]
-        command: MemberCommand,
-    },
-    Message {
-        #[clap(short, long)]
-        group: String,
-        text: String,
-    },
-}
-
-#[derive(Subcommand, Debug)]
-enum MemberCommand {
-    Add {
-        #[clap(short, long)]
-        group: String,
-        key_package: String,
-        #[clap(short, long)]
-        welcome_out: Option<String>,
-    },
-}
-
-fn path_reader(path: String) -> io::Result<Box<dyn Read>> {
-    if path == "-" {
-        Ok(Box::new(io::stdin()))
-    } else {
-        Ok(Box::new(fs::File::open(path)?))
-    }
-}
-
-fn main() {
-    let cli = Cli::parse();
-    let backend = MlsCryptoProvider::try_new(&cli.store, &cli.enc_key).unwrap();
-    match cli.command {
-        Command::KeyPackage { client_id } => key_package(&backend, client_id),
-        Command::PublicKey { client_id } => public_key(&backend, client_id),
-        Command::Group { client_id, group_id } => group(&backend, client_id, group_id.as_bytes()),
-        Command::Member {
-            command:
-                MemberCommand::Add {
-                    group,
-                    key_package,
-                    welcome_out,
-                },
-        } => {
-            let mut group_data = path_reader(group).unwrap();
-            let mut kp_data = path_reader(key_package).unwrap();
-            let wel_data = welcome_out.map(|path| fs::File::create(path).unwrap());
-            add_member(&backend, &mut group_data, &mut kp_data, wel_data);
-        }
-        Command::Message { group, text } => {
-            let mut group_data = path_reader(group).unwrap();
-            app_message(&backend, &mut group_data, text);
-        }
-    }
 }
