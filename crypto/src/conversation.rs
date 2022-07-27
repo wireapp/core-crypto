cfdc6300564e23a780bc1c48625daba7fd448c77
Augusto <augusto.c.dias@wire.com>
2022-07-27 13:12:01 +0200

    adding more tests

--- crypto/src/conversation.rs
+++ crypto/src/conversation.rs
@@ -369,12 +369,12 @@ impl MlsConversation {
     fn calculate_delay(self_index: usize, epoch: u64, total_members: usize) -> CryptoResult<u64> {
         let self_index: u64 = self_index.try_into().map_err(CryptoError::from)?;
         let total_members: u64 = total_members.try_into().map_err(CryptoError::from)?;
-        let position = ((epoch + self_index) % total_members) + 1;
+        let position = ((epoch % total_members) + (self_index % total_members)) % total_members + 1;
         let result = match position {
             1 => 0,
             2 => 15,
             3 => 30,
-            _ => ((TryInto::<f32>::try_into(position).ln() * 120.0) as u64).saturating_sub(106),
+            _ => (((position as f32).ln() * 120.0) as u64).saturating_sub(106),
         };
         Ok(result)
     }
@@ -849,6 +849,36 @@ pub mod tests {
         }
 
         #[test]
+        pub fn test_calculate_delay_max() {
+            let (self_index, epoch, total_members) = (usize::MAX, u64::MAX, usize::MAX);
+            let delay = MlsConversation::calculate_delay(self_index, epoch, total_members).unwrap();
+            assert_eq!(delay, 0);
+        }
+
+        #[test]
+        pub fn test_calculate_delay_min() {
+            let (self_index, epoch, total_members) = (usize::MIN, u64::MIN, usize::MAX);
+            let delay = MlsConversation::calculate_delay(self_index, epoch, total_members).unwrap();
+            assert_eq!(delay, 0);
+        }
+
+        #[test]
+        #[should_panic]
+        pub fn test_calculate_delay_panic() {
+            let (self_index, epoch, total_members) = (0, 0, usize::MIN);
+            // total members can never be 0 as there's no group with 0 members and it will panic
+            // when trying to calculate the remainder of 0
+            MlsConversation::calculate_delay(self_index, epoch, total_members).unwrap();
+        }
+
+        #[test]
+        pub fn test_calculate_delay_min_max() {
+            let (self_index, epoch, total_members) = (usize::MIN, u64::MAX, usize::MAX);
+            let delay = MlsConversation::calculate_delay(self_index, epoch, total_members).unwrap();
+            assert_eq!(delay, 0);
+        }
+
+        #[test]
         pub fn test_calculate_delay_first() {
             let (self_index, epoch, total_members) = (9, 1, 10);
             let delay = MlsConversation::calculate_delay(self_index, epoch, total_members).unwrap();
@@ -858,8 +888,6 @@ pub mod tests {
         #[test]
         pub fn test_calculate_delay_second() {
             let (self_index, epoch, total_members) = (0, 1, 10);
-            let epoch = 1;
-            let total_members = 10;
             let delay = MlsConversation::calculate_delay(self_index, epoch, total_members).unwrap();
             assert_eq!(delay, 15);
         }
