6f01e5be7957073c954a37e4eb163c0d4958f0da
Augusto <augusto.c.dias@wire.com>
2022-08-31 16:28:52 +0200

    Adding possibility to generate multiple versions from rollup

--- crypto-ffi/bindings/js/rollup.config.js
+++ crypto-ffi/bindings/js/rollup.config.js
@@ -3,50 +3,83 @@ import { generateDtsBundle } from "dts-bundle-generator";
 import { resolve as pathResolve } from "path";
 import ts from "rollup-plugin-ts";
 import typescript from "typescript";
-const { sys: { writeFile } } = typescript;
+const {
+    sys: { writeFile },
+} = typescript;
 
 const generateDtsBundlePlugin = (entry, output) => ({
-  name: "dts-bundle-generator",
-  renderStart() {
-    const result = generateDtsBundle(
-      [
-        {
-          filePath: entry,
-          output: {
-            noBanner: true,
-            exportReferencedTypes: false,
-          },
-        },
-      ],
-      {
-        preferredConfigPath: pathResolve(__dirname, "./tsconfig.json"),
-      }
-    );
-    if (!result) {
-      throw new Error("Error in DTS bundle generation");
-    }
+    name: "dts-bundle-generator",
+    renderStart() {
+        const result = generateDtsBundle(
+            [
+                {
+                    filePath: entry,
+                    output: {
+                        noBanner: true,
+                        exportReferencedTypes: false,
+                    },
+                },
+            ],
+            {
+                preferredConfigPath: pathResolve(__dirname, "./tsconfig.json"),
+            }
+        );
+        if (!result) {
+            throw new Error("Error in DTS bundle generation");
+        }
 
-    const dtsFile = result[0];
-    writeFile(output, dtsFile);
-  },
+        const dtsFile = result[0];
+        writeFile(output, dtsFile);
+    },
 });
 
-const config = {
-  input: pathResolve(__dirname, "./CoreCrypto.ts"),
-  output: {
-    file: pathResolve(__dirname, "../../../platforms/web/corecrypto.js"),
-    format: "es",
-  },
-  plugins: [
-    rust(),
-    ts({
-      tsconfig: pathResolve(__dirname, "./tsconfig.json"),
-    }),
-    generateDtsBundlePlugin(
-      pathResolve(__dirname, "./CoreCrypto.ts"),
-      pathResolve(__dirname, "../../../platforms/web/corecrypto.d.ts")
-    ),
-  ],
+const getOutput = (format = "es2017") => {
+    return {
+        file: pathResolve(
+            __dirname,
+            "../../../platforms/web/" + format + "/corecrypto.js"
+        ),
+        format: "es",
+    };
 };
 
-export default config;
+const getPlugins = (format = "es2017") => {
+    return [
+        rust(),
+        ts({
+            tsconfig: {
+                fileName: pathResolve(__dirname, "./tsconfig.json"),
+                hook: (resolvedConfig) => ({
+                    ...resolvedConfig,
+                    target: format,
+                }),
+            },
+        }),
+        generateDtsBundlePlugin(
+            pathResolve(__dirname, "./CoreCrypto.ts"),
+            pathResolve(
+                __dirname,
+                "../../../platforms/web/" + format + "/corecrypto.d.ts"
+            )
+        ),
+    ];
+};
+
+const rollup = (_args) => {
+    const input = pathResolve(__dirname, "./CoreCrypto.ts");
+
+    return [
+        {
+            input,
+            output: getOutput("es2017"),
+            plugins: getPlugins("es2017"),
+        },
+        {
+            input,
+            output: getOutput("es2019"),
+            plugins: getPlugins("es2019"),
+        },
+    ];
+};
+
+export default rollup;
