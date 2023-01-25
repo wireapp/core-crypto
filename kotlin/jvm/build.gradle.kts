98de2701fe5d7356a53d4977b57276de73bd53f1
Mathieu Amiot <amiot.mathieu@gmail.com>
2023-01-25 12:09:41 +0100

    chore: Cut release 0.6.0-rc.5

--- kotlin/jvm/build.gradle.kts
+++ kotlin/jvm/build.gradle.kts
@@ -58,7 +58,7 @@ afterEvaluate {
             create<MavenPublication>("maven") {
                 groupId = "com.wire"
                 artifactId = "core-crypto-jvm"
-                version = "0.6.0-rc.4"
+                version = "0.6.0-rc.5"
 
                 from(components["java"])
 
--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -9,7 +9,6 @@ use crate::release::ReleaseCommands;
 
 #[derive(Debug, Parser)]
 #[clap(author, version, about, long_about = None)]
-#[clap(propagate_version = true)]
 struct Xtask {
     #[clap(subcommand)]
     command: Commands,
@@ -22,7 +21,6 @@ enum Commands {
     #[clap(subcommand)]
     Release(ReleaseCommands),
 }
-
 
 fn main() -> Result<()> {
     color_eyre::install()?;
@@ -37,7 +35,7 @@ fn main() -> Result<()> {
         },
         Commands::Release(release_command) => match release_command {
             ReleaseCommands::Bump { version, dry_run } => release::bump(version, dry_run)?,
-        }
+        },
     }
 
     Ok(())
