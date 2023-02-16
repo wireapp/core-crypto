37f2c2429b37efbb62cedf51abd6288f31c9e94b
Mathieu Amiot <amiot.mathieu@gmail.com>
2023-02-16 15:08:37 +0100

    chore: Release v0.6.2

--- kotlin/jvm/build.gradle.kts
+++ kotlin/jvm/build.gradle.kts
@@ -109,7 +109,7 @@ afterEvaluate {
             create<MavenPublication>("maven") {
                 groupId = "com.wire"
                 artifactId = "core-crypto-jvm"
-                version = "0.6.1"
+                version = "0.6.2"
 
                 from(components["java"])
 
