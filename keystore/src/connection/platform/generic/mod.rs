53fc155830e414677d5c6c2f01cb879fce4cc447
Mathieu Amiot <amiot.mathieu@gmail.com>
2023-04-11 16:05:54 +0200

    fix: Fixed iOS keychain handling with proper attributes

--- keystore/src/connection/platform/generic/mod.rs
+++ keystore/src/connection/platform/generic/mod.rs
@@ -98,21 +98,84 @@ impl SqlCipherConnection {
     #[cfg(feature = "ios-wal-compat")]
     fn handle_ios_wal_compat(conn: &rusqlite::Connection, path: &str) -> CryptoKeystoreResult<()> {
         const ERR_SEC_ITEM_NOT_FOUND: i32 = -25300;
+        const LEGACY_ACCT_NAME: &str = "keystore_salt";
+        const WIRE_SERVICE_NAME: &str = "wire.com";
         use security_framework::passwords as ios_keychain;
-
         use sha2::Digest as _;
+
+        #[allow(non_upper_case_globals)]
+        // This is to make sure that macOS/iOS keychain items that we create (see above for the *why*)
+        // are accessible in the background through a `kSecAttrAccessibleAfterFirstUnlock` attribute
+        // More on the topic: https://developer.apple.com/documentation/security/keychain_services/keychain_items/restricting_keychain_item_accessibility
+        // More here on the specific attribute: https://developer.apple.com/documentation/security/ksecattraccessibleafterfirstunlock?language=swift
+        fn mark_password_as_accessible(key: &str) -> security_framework::base::Result<()> {
+            use core_foundation::{
+                base::TCFType,
+                dictionary::CFDictionary,
+                string::{CFString, CFStringRef},
+            };
+            use security_framework::base::Error;
+            use security_framework_sys::{
+                base::errSecSuccess,
+                item::{kSecAttrAccount, kSecAttrService, kSecClass, kSecClassGenericPassword},
+            };
+
+            // Import raw symbols from CoreFoundation
+            extern "C" {
+                pub static kSecAttrAccessibleAfterFirstUnlock: CFStringRef;
+                pub static kSecAttrAccessible: CFStringRef;
+            }
+
+            // Create a query that matches a:
+            let query_params = CFDictionary::from_CFType_pairs(&[
+                // Class GenericPassword
+                (unsafe { CFString::wrap_under_get_rule(kSecClass) }, unsafe {
+                    CFString::wrap_under_get_rule(kSecClassGenericPassword).as_CFType()
+                }),
+                // with Service = "wire.com"
+                (
+                    unsafe { CFString::wrap_under_get_rule(kSecAttrService) },
+                    CFString::from(WIRE_SERVICE_NAME).as_CFType(),
+                ),
+                // Holding account name = `key` (in the following form: `keystore_salt_[sha256(file_path)]`)
+                (
+                    unsafe { CFString::wrap_under_get_rule(kSecAttrAccount) },
+                    CFString::from(key).as_CFType(),
+                ),
+            ]);
+
+            // And now we ask to update the following properties:
+            let payload_params = CFDictionary::from_CFType_pairs(&[(
+                // Keychain Accessibility setting
+                // See: https://developer.apple.com/documentation/security/ksecattraccessible
+                unsafe { CFString::wrap_under_get_rule(kSecAttrAccessible) },
+                // Set to AccessibleAfterFirstUnlock (i.e. is accessible after the first post-boot unlock)
+                unsafe { CFString::wrap_under_get_rule(kSecAttrAccessibleAfterFirstUnlock).as_CFType() },
+            )]);
+
+            // Update the item in the keychain
+            match unsafe {
+                security_framework_sys::keychain_item::SecItemUpdate(
+                    query_params.as_concrete_TypeRef(),
+                    payload_params.as_concrete_TypeRef(),
+                )
+            } {
+                errSecSuccess => Ok(()),
+                err => Err(Error::from_code(err)),
+            }
+        }
 
         let mut path_hash = sha2::Sha256::default();
         path_hash.update(path.as_bytes());
-        let keychain_key = format!("keystore_salt_{}", hex::encode(&path_hash.finalize()));
+        let keychain_key = format!("{LEGACY_ACCT_NAME}_{}", hex::encode(&path_hash.finalize()));
 
         // Old version compat fix
-        if let Ok(salt) = ios_keychain::get_generic_password("wire.com", "keystore_salt") {
-            ios_keychain::set_generic_password("wire.com", &keychain_key, salt.as_slice())?;
-            ios_keychain::delete_generic_password("wire.com", "keystore_salt")?;
+        if let Ok(salt) = ios_keychain::get_generic_password(WIRE_SERVICE_NAME, LEGACY_ACCT_NAME) {
+            ios_keychain::set_generic_password(WIRE_SERVICE_NAME, &keychain_key, salt.as_slice())?;
+            ios_keychain::delete_generic_password(WIRE_SERVICE_NAME, LEGACY_ACCT_NAME)?;
         }
 
-        match ios_keychain::get_generic_password("wire.com", &keychain_key) {
+        match ios_keychain::get_generic_password(WIRE_SERVICE_NAME, &keychain_key) {
             Ok(salt) => {
                 conn.pragma_update(None, "cipher_salt", format!("x'{}'", hex::encode(salt)))?;
             }
@@ -121,10 +184,14 @@ impl SqlCipherConnection {
                 let mut bytes = [0u8; 16];
                 hex::decode_to_slice(salt, &mut bytes)
                     .map_err(|e| crate::CryptoKeystoreError::HexSaltDecodeError(e))?;
-                ios_keychain::set_generic_password("wire.com", &keychain_key, &bytes)?;
+
+                ios_keychain::set_generic_password(WIRE_SERVICE_NAME, &keychain_key, &bytes)?;
             }
             Err(e) => return Err(e.into()),
         }
+
+        // We're doing it here to make sure we retroactively mark database salts as accessible
+        mark_password_as_accessible(&keychain_key)?;
 
         const CIPHER_PLAINTEXT_BYTES: u32 = 32;
         conn.pragma_update(None, "cipher_plaintext_header_size", CIPHER_PLAINTEXT_BYTES)?;
