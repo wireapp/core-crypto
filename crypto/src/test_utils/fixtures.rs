4dbfd5d1e419851475c140dd42e4fa058480dfb9
beltram <beltram.maldant@gmail.com>
2023-04-12 10:54:50 +0200

    chore: get rid of internal 'CredentialSupplier' test util

--- crypto/src/test_utils/fixtures.rs
+++ crypto/src/test_utils/fixtures.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see http://www.gnu.org/licenses/.
 
-pub use crate::mls::credential::CredentialSupplier;
 use crate::prelude::{MlsCiphersuite, MlsConversationConfiguration, MlsCustomConfiguration};
 use openmls_traits::types::SignatureScheme;
 pub use rstest::*;
