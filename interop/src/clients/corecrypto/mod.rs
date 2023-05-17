5cb463b5cc3deefc60823d1e4ed17d920551d0b2
Mathieu Amiot <amiot.mathieu@gmail.com>
2023-05-17 10:53:44 +0200

    fix: Size regression on FFI

--- interop/src/clients/corecrypto/mod.rs
+++ interop/src/clients/corecrypto/mod.rs
@@ -14,6 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see http://www.gnu.org/licenses/.
 
-pub mod ffi;
+// pub mod ffi;
 pub mod native;
 pub mod web;
