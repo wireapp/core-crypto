19fd4c01b8ecb2e789e680cb6a23353524751dc4
Jacob Persson <7156+typfel@users.noreply.github.com>
2023-06-02 11:18:25 +0200

    feat: generate XCFramework when releasing for Swift (#330)

---
+++ crypto-ffi/bindings/swift/build-xcframework.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+xcodebuild archive \
+    -project 'WireCoreCrypto/WireCoreCrypto.xcodeproj' \
+    -scheme 'WireCoreCrypto' \
+    -configuration Release \
+    -archivePath 'WireCoreCrypto-ios.xcarchive' \
+    -destination 'generic/platform=ios' \
+    SKIP_INSTALL=NO \
+    BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+
+xcodebuild archive \
+    -project 'WireCoreCrypto/WireCoreCrypto.xcodeproj' \
+    -scheme 'WireCoreCrypto' \
+    -configuration Release \
+    -archivePath 'WireCoreCrypto-simulator.xcarchive' \
+    -destination 'generic/platform=ios Simulator' \
+    SKIP_INSTALL=NO \
+    BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+
+xcodebuild -create-xcframework \
+    -archive 'WireCoreCrypto-ios.xcarchive' \
+    -framework 'WireCoreCrypto.framework' \
+    -archive 'WireCoreCrypto-simulator.xcarchive' \
+    -framework 'WireCoreCrypto.framework' \
+    -output 'WireCoreCrypto.xcframework'
+
+zip -r WireCoreCrypto.xcframework.zip WireCoreCrypto.xcframework
+
+rm -rf WireCoreCrypto.xcframework
+rm -rf WireCoreCrypto-ios.xcarchive
+rm -rf WireCoreCrypto-simulator.xcarchive
