1b68f7ec99ef4a83eb2a140f4ee4d8c4f8dced79
beltram <beltram.maldant@gmail.com>
2023-08-22 15:47:23 +0200

    test: fix wasm test hitting a limit. Just split them for now, waiting for a proper solution

---
+++ crypto-ffi/bindings/js/test/wasm-tests.sh
@@ -0,0 +1,4 @@
+cargo test --target wasm32-unknown-unknown -p core-crypto --lib e2e_identity
+cargo test --target wasm32-unknown-unknown -p core-crypto --lib mls
+cargo test --target wasm32-unknown-unknown -p core-crypto --lib group_store
+cargo test --target wasm32-unknown-unknown -p core-crypto --lib proteus
