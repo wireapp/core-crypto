0c34ef856683dbd357cec0dd7d61c01d3a80e874
beltram <beltram.maldant@gmail.com>
2023-09-19 17:46:54 +0200

    wip

--- crypto/src/mls/credential/test_trust_anchor.rs
+++ crypto/src/mls/credential/test_trust_anchor.rs
@@ -2,11 +2,15 @@
 
 use crate::test_utils::x509::{new_self_signed_certificate, CertificateParams};
 use openmls_traits::types::SignatureScheme;
+use rustls::client::ServerCertVerifier as _;
 use std::process::Command;
 use x509_cert::der::Encode;
 
 #[test]
 fn toy() {
+    let root1 = include_bytes!("root1.crt");
+    let intermediate = include_bytes!("root1-int1-ee_1-good.crt");
+
     let scheme = SignatureScheme::ECDSA_SECP256R1_SHA256;
     let cert = new_self_signed_certificate(CertificateParams::default(), scheme, true);
 
@@ -31,8 +35,8 @@ fn toy() {
     let verify = if verify.status.success() { "✅" } else { "❌" };
     println!("verify > {verify}");
 
-    use rustls::client::ServerCertVerifier as _;
     let verifier = rustls_platform_verifier::Verifier::new();
+    let verifier = rustls_platform_verifier::Verifier::new_with_fake_root(cert.to_der().unwrap().as_slice());
 
     let end_identity = rustls::Certificate(cert.to_der().unwrap());
     let server_name = rustls::ServerName::try_from("wire.com").unwrap();
@@ -50,6 +54,28 @@ fn toy() {
         std::time::SystemTime::now(),
     )
     .unwrap();*/
+}
+
+const ROOT1: &[u8] = include_bytes!("root1.crt");
+const ROOT1_INT1: &[u8] = include_bytes!("root1-int1.crt");
+const ROOT1_INT1_EXAMPLE_COM_GOOD: &[u8] = include_bytes!("root1-int1-ee_example.com-good.crt");
+
+#[test]
+fn toy_2() {
+    let verifier = rustls_platform_verifier::Verifier::new_with_fake_root(ROOT1);
+    let end_entity = rustls::Certificate(ROOT1_INT1_EXAMPLE_COM_GOOD.to_vec());
+    let intermediates = [rustls::Certificate(ROOT1_INT1.to_vec())];
+    let server_name = rustls::client::ServerName::try_from("example.com").unwrap();
+    verifier
+        .verify_server_cert(
+            &end_entity,
+            &intermediates,
+            &server_name,
+            &mut std::iter::empty(),
+            &[],
+            std::time::SystemTime::now(),
+        )
+        .unwrap();
 }
 
 pub fn rand_base64_str(size: usize) -> String {
