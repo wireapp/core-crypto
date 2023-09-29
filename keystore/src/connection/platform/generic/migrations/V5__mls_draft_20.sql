dc46478825e6373eeded56d7f0ec52ee76371fe0
beltram <beltram.maldant@gmail.com>
2023-09-29 11:36:31 +0200

    fix: backward incompatible database schemas. It only preserves Proteus compatibility when migrating from CC 0.11.0 -> 1.0.0. For anything MLS-related it is recommended to wipe all the groups

---
+++ keystore/src/connection/platform/generic/migrations/V5__mls_draft_20.sql
@@ -0,0 +1,35 @@
+-- Both those table are used only in MLS and we do not need to maintain backward compatibility there
+DROP TABLE mls_keys;
+DROP TABLE mls_identities;
+
+CREATE TABLE mls_signature_keypairs (
+    signature_scheme INT,
+    keypair BLOB,
+    pk BLOB,
+    credential_id BLOB
+);
+
+CREATE TABLE mls_credentials (
+    id BLOB,
+    credential BLOB
+);
+
+CREATE TABLE mls_hpke_private_keys (
+    pk BLOB,
+    sk BLOB
+);
+
+CREATE TABLE mls_encryption_keypairs (
+    pk BLOB,
+    sk BLOB
+);
+
+CREATE TABLE mls_psk_bundles (
+    psk_id BLOB,
+    psk    BLOB
+);
+
+CREATE TABLE mls_keypackages (
+    keypackage_ref BLOB,
+    keypackage     BLOB
+);
