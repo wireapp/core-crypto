b4ff60a38e8e0e9794246d3eaf625cb43ab62bc1
beltram <beltram.maldant@gmail.com>
2023-10-13 12:08:34 +0200

    build: release v1.0.0-pre.6+v1-schemafix-003

--- kotlin/android/build.gradle.kts
+++ kotlin/android/build.gradle.kts
@@ -112,7 +112,7 @@ afterEvaluate {
             create<MavenPublication>("maven") {
                 groupId = "com.wire"
                 artifactId = "core-crypto-android"
-                version = "1.0.0-pre.6+v1-schemafix-002"
+                version = "1.0.0-pre.6+v1-schemafix-003"
                 from(components["release"])
                 pom {
                     name.set("core-crypto-android")
