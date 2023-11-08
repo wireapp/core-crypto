bec0149fb6fac979bb9a8a5d2c5b92de173be814
Mathieu Amiot <amiot.mathieu@gmail.com>
2023-11-08 13:11:26 +0100

    build: Release v1.0.0-pre.6+v1-schemafix-007

--- kotlin/android/build.gradle.kts
+++ kotlin/android/build.gradle.kts
@@ -112,7 +112,7 @@ afterEvaluate {
             create<MavenPublication>("maven") {
                 groupId = "com.wire"
                 artifactId = "core-crypto-android"
-                version = "1.0.0-pre.6+v1-schemafix-006"
+                version = "1.0.0-pre.6+v1-schemafix-007"
                 from(components["release"])
                 pom {
                     name.set("core-crypto-android")
