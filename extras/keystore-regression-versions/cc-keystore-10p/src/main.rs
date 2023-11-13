b040f010fc12662550c7d13cbb11141f1fe6c4ec
Mathieu Amiot <amiot.mathieu@gmail.com>
2023-11-13 14:55:35 +0100

    test: Test keystore migration regressions

---
+++ extras/keystore-regression-versions/cc-keystore-10p/src/main.rs
@@ -0,0 +1,29 @@
+use cc_keystore_support::*;
+use core_crypto_keystore::Connection;
+pub struct CoreCryptoKeystore10p(Connection);
+
+#[async_trait::async_trait(?Send)]
+impl CoreCryptoKeystore for CoreCryptoKeystore10p {
+    async fn open(path: &str, key: &str) -> Result<Self>
+    where
+        Self: Sized,
+    {
+        Ok(Self(Connection::open_with_key(path, key).await?))
+    }
+    async fn close(self) -> Result<()> {
+        self.0.close().await?;
+        Ok(())
+    }
+    async fn wipe(self) -> Result<()> {
+        self.0.wipe().await?;
+        Ok(())
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    use clap::Parser as _;
+    let args = CliArgs::<CoreCryptoKeystore10p>::parse();
+    args.run().await?;
+    Ok(())
+}
