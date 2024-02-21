7e670e83f87dd0447469842c13f30d7262124ede
Mathieu Amiot <amiot.mathieu@gmail.com>
2024-02-21 13:31:45 +0100

    fix: Remove cached is_e2ei_capable flag

--- crypto/src/e2e_identity/rotate.rs
+++ crypto/src/e2e_identity/rotate.rs
@@ -693,15 +693,9 @@ pub mod tests {
                         .collect::<Vec<_>>();
                     assert_eq!(all_credentials.len(), 2);
 
-                    let client = Client::load(
-                        &alice_central.mls_central.mls_backend,
-                        &cid,
-                        all_credentials,
-                        scs,
-                        matches!(case.credential_type, MlsCredentialType::X509),
-                    )
-                    .await
-                    .unwrap();
+                    let client = Client::load(&alice_central.mls_central.mls_backend, &cid, all_credentials, scs)
+                        .await
+                        .unwrap();
                     alice_central.mls_central.mls_client = Some(client);
 
                     // Verify that Alice has the same credentials
