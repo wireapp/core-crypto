4c28750000fbd499587fc4f5ae8dfc3827aae217
Ivan Stanković <ivan.stankovic@wire.com>
2024-07-01 14:31:50 +0200

    chore: remove the entire sample-projects thing
    This hasn't seen updates in more than 2 years, contains a number of
    webp files and a few binary archives, it's unclear what purpose it
    serves... just drop it.

--- sample-projects/ios/Tests/MyLibraryTests/MyLibraryTests.swift
+++
@@ -1,8 +0,0 @@
-import XCTest
-@testable import MyLibrary
-
-final class MyLibraryTests: XCTestCase {
-    func testExample() throws {
-        XCTAssertEqual(MyLibrary.version(), "0.3.0")
-    }
-}
