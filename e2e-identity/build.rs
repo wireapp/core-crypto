2c6c5052587a478b2af2c844fb2ab4a8413b7185
SimonThormeyer <simonthormeyer@gmail.com>
2024-08-06 15:32:23 +0200

    style: address unexpected_cfgs lint

--- e2e-identity/build.rs
+++ e2e-identity/build.rs
@@ -1,4 +1,5 @@
 fn main() {
+    println!("cargo::rustc-check-cfg=cfg(ci)");
     // detects when running in Github action
     if let Ok("true") = std::env::var("CI").as_deref() {
         println!("cargo:rustc-cfg=ci")
