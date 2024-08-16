3ea51332cc66c8d70d126a2ddcc26edc8375837d
SimonThormeyer <simonthormeyer@gmail.com>
2024-08-16 17:15:25 +0200

    refactor: add unique id field to mls_epoch_encryption_keypairs table

--- keystore/src/connection/platform/generic/migrations/V12__unique_id_constraints.sql
+++ keystore/src/connection/platform/generic/migrations/V12__unique_id_constraints.sql
@@ -11,3 +11,17 @@ SELECT sha256_blob(pk), pk, sk FROM mls_encryption_keypairs;
 DROP TABLE mls_encryption_keypairs;
 
 ALTER TABLE mls_encryption_keypairs_new RENAME TO mls_encryption_keypairs;
+
+---- mls_epoch_encryption_keypairs ----
+CREATE TABLE mls_epoch_encryption_keypairs_new (
+    id_hex TEXT UNIQUE,
+    keypairs BLOB
+);
+
+-- We convert the output of hex() for consistency, because the rust encoder returns lowercase.
+INSERT INTO mls_epoch_encryption_keypairs_new (id_hex, keypairs)
+SELECT LOWER(hex(id)), keypairs FROM mls_epoch_encryption_keypairs;
+
+DROP TABLE mls_epoch_encryption_keypairs;
+
+ALTER TABLE mls_epoch_encryption_keypairs_new RENAME TO mls_epoch_encryption_keypairs;
