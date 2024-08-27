2c594a9d284f64a3bbc7fcc11e470bf291b96f5d
SimonThormeyer <simonthormeyer@gmail.com>
2024-08-27 10:40:44 +0200

    chore: migrate code to idb api

--- keystore/src/lib.rs
+++ keystore/src/lib.rs
@@ -122,7 +122,7 @@ pub mod dummy_entity {
 
 /// Used to calculate ID hashes for some MlsEntities' SQLite tables (not used on wasm).
 /// We only use sha256 on platforms where we use SQLite.
-/// On wasm, we use IndexedDB, a key-value store, via the rexie crate.
+/// On wasm, we use IndexedDB, a key-value store, via the idb crate.
 #[cfg(not(target_family = "wasm"))]
 pub(crate) fn sha256(data: &[u8]) -> String {
     let mut hasher = Sha256::new();
