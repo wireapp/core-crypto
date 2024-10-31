ac7f42d1e67def50446aafb11031fba093df90b2
SimonThormeyer <simonthormeyer@gmail.com>
2024-10-31 11:41:02 +0100

    chore(crypto): make symbols used across benches public
    Before, cargo check warnings were shown in this code.

--- crypto/benches/utils/proteus_bench.rs
+++ crypto/benches/utils/proteus_bench.rs
@@ -6,7 +6,7 @@ use proteus_wasm::{
     keys::{PreKey, PreKeyBundle},
 };
 
-pub(crate) async fn setup_proteus(in_memory: bool) -> CoreCrypto {
+pub async fn setup_proteus(in_memory: bool) -> CoreCrypto {
     let (core_crypto, _) = setup_mls(Default::default(), Default::default(), in_memory).await;
     core_crypto.proteus_init().await.unwrap();
     core_crypto
@@ -16,12 +16,12 @@ pub(crate) fn new_prekeys(size: usize) -> Vec<PreKeyBundle> {
     (0..size).map(|_| new_prekey()).collect()
 }
 
-pub(crate) fn new_prekey() -> PreKeyBundle {
+pub fn new_prekey() -> PreKeyBundle {
     let kp = keys::IdentityKeyPair::new();
     let prekey = PreKey::new(keys::PreKeyId::new(1));
     PreKeyBundle::new(kp.public_key, &prekey)
 }
 
-pub(crate) fn session_id() -> String {
+pub fn session_id() -> String {
     uuid::Uuid::new_v4().hyphenated().to_string()
 }
