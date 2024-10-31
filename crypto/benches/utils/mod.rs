ff0f68344f49a72a119f512d94c6721c48c41248
SimonThormeyer <simonthormeyer@gmail.com>
2024-10-31 11:41:02 +0100

    chore: make symbols used across benches public

--- crypto/benches/utils/mod.rs
+++ crypto/benches/utils/mod.rs
@@ -1,7 +1,7 @@
 #![allow(dead_code)]
 
 pub(crate) mod mls;
-pub(crate) mod proteus_bench;
+pub mod proteus_bench;
 
 use criterion::Criterion;
 pub(crate) use mls::*;
@@ -10,22 +10,22 @@ pub(crate) use mls::*;
 pub(crate) const SAMPLE_SIZE: usize = 10;
 
 // number of clients in a group
-pub(crate) const GROUP_RANGE: std::ops::Range<usize> = GROUP_MIN..GROUP_MAX;
-pub(crate) const GROUP_RANGE_PROTEUS: std::ops::Range<usize> = GROUP_MIN..GROUP_MAX_PROTEUS;
+pub const GROUP_RANGE: std::ops::Range<usize> = GROUP_MIN..GROUP_MAX;
+pub const GROUP_RANGE_PROTEUS: std::ops::Range<usize> = GROUP_MIN..GROUP_MAX_PROTEUS;
 
-pub(crate) const GROUP_MAX: usize = 1000 + GROUP_MIN + 1;
-pub(crate) const GROUP_MAX_PROTEUS: usize = 100 + GROUP_MIN + 1;
+pub const GROUP_MAX: usize = 1000 + GROUP_MIN + 1;
+pub const GROUP_MAX_PROTEUS: usize = 100 + GROUP_MIN + 1;
 
 pub(crate) const GROUP_MIN: usize = 1;
 
-pub(crate) const GROUP_STEP: usize = 200;
-pub(crate) const GROUP_STEP_PROTEUS: usize = 20;
+pub const GROUP_STEP: usize = 200;
+pub const GROUP_STEP_PROTEUS: usize = 20;
 
 // size (in bytes) of application messages
-pub(crate) const MSG_RANGE: std::ops::Range<usize> = MSG_MIN..MSG_MAX;
-pub(crate) const MSG_MAX: usize = 10_000 + MSG_MIN + 1;
+pub const MSG_RANGE: std::ops::Range<usize> = MSG_MIN..MSG_MAX;
+pub const MSG_MAX: usize = 10_000 + MSG_MIN + 1;
 pub(crate) const MSG_MIN: usize = 10;
-pub(crate) const MSG_STEP: usize = 2000;
+pub const MSG_STEP: usize = 2000;
 
 // pending proposal constants
 pub(crate) const PENDING_RANGE: std::ops::Range<usize> = PENDING_MIN..PENDING_MAX;
@@ -33,6 +33,6 @@ pub(crate) const PENDING_MAX: usize = 100 + PENDING_MIN + 1;
 pub(crate) const PENDING_MIN: usize = 1;
 pub(crate) const PENDING_STEP: usize = 20;
 
-pub(crate) fn criterion() -> Criterion {
+pub fn criterion() -> Criterion {
     Criterion::default().sample_size(SAMPLE_SIZE).without_plots()
 }
