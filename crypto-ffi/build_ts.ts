4b0d79e7e7d62c1d88302315ee6e9c8b1ab1fd2b
SimonThormeyer <simonthormeyer@gmail.com>
2024-11-18 13:28:16 +0100

    chore(ffi/web): expect ts compile errors in build file
    This is a cosmetic change, the errors didn't make the build fail, but just showed as warnings when the build file itself is compiled with typescript, which some IDEs may do when opening the file.

--- crypto-ffi/build_ts.ts
+++ crypto-ffi/build_ts.ts
@@ -12,6 +12,7 @@ const cp = async (from: string, to: string, move: boolean = false) => {
     }
 }
 
+// @ts-expect-error S1378: Top-level await expressions are only allowed when the module option is set to es2022, esnext, system, node16, nodenext, or preserve, and the target option is set to es2017 or higher.
 await Bun.build({
     entrypoints: [`${baseDir}/CoreCrypto.ts`],
     outdir: destDir,
@@ -30,7 +31,10 @@ await Bun.build({
     ],
 });
 
+// @ts-expect-error S1378: Top-level await expressions are only allowed when the module option is set to es2022, esnext, system, node16, nodenext, or preserve, and the target option is set to es2017 or higher.
 await cp(`${baseDir}/wasm/core-crypto-ffi_bg.wasm`, `${destDir}/core-crypto-ffi_bg.wasm`);
+// @ts-expect-error S1378: Top-level await expressions are only allowed when the module option is set to es2022, esnext, system, node16, nodenext, or preserve, and the target option is set to es2017 or higher.
 await rename(`${destDir}/CoreCrypto.js`, `${destDir}/corecrypto.js`);
+// @ts-expect-error S1378: Top-level await expressions are only allowed when the module option is set to es2022, esnext, system, node16, nodenext, or preserve, and the target option is set to es2017 or higher.
 await rename(`${destDir}/CoreCrypto.d.ts`, `${destDir}/corecrypto.d.ts`);
 
