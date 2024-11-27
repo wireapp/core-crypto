1a94b00dbefa1883720bfee1e57ef6e8120fa3f7
SimonThormeyer <simonthormeyer@gmail.com>
2024-11-27 13:35:43 +0100

    chore: remove keystore_v_1_0_0 as dependency, add as submodule
    We have to fix error exposure for indexed db errors, this is more straightforward when we have the old keystore implementation directly in this repository.

---
+++ keystore/src/connection/platform/wasm/migrations/keystore_v_1_0_0/mls.rs
@@ -0,0 +1,167 @@
+// Wire
+// Copyright (C) 2022 Wire Swiss GmbH
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+
+use openmls_basic_credential::SignatureKeyPair;
+use openmls_traits::key_store::{MlsEntity, MlsEntityId};
+
+use super::entities::MlsEpochEncryptionKeyPair;
+use crate::keystore_v_1_0_0::{
+    entities::{
+        MlsEncryptionKeyPair, MlsHpkePrivateKey, MlsKeyPackage, MlsPskBundle, MlsSignatureKeyPair, PersistedMlsGroup,
+    },
+    CryptoKeystoreError,
+};
+
+#[inline(always)]
+pub fn deser<T: MlsEntity>(bytes: &[u8]) -> Result<T, CryptoKeystoreError> {
+    Ok(postcard::from_bytes(bytes)?)
+}
+
+#[inline(always)]
+pub fn ser<T: MlsEntity>(value: &T) -> Result<Vec<u8>, CryptoKeystoreError> {
+    Ok(postcard::to_stdvec(value)?)
+}
+
+#[cfg_attr(target_family = "wasm", async_trait::async_trait(?Send))]
+#[cfg_attr(not(target_family = "wasm"), async_trait::async_trait)]
+impl openmls_traits::key_store::OpenMlsKeyStore for crate::keystore_v_1_0_0::connection::Connection {
+    type Error = CryptoKeystoreError;
+
+    async fn store<V: MlsEntity + Sync>(&self, k: &[u8], v: &V) -> Result<(), Self::Error>
+    where
+        Self: Sized,
+    {
+        if k.is_empty() {
+            return Err(CryptoKeystoreError::MlsKeyStoreError(
+                "The provided key is empty".into(),
+            ));
+        }
+
+        let data = ser(v)?;
+
+        match V::ID {
+            MlsEntityId::GroupState => {
+                return Err(CryptoKeystoreError::IncorrectApiUsage(
+                    "Groups must not be saved using OpenMLS's APIs. You should use the keystore's provided methods",
+                ));
+            }
+            MlsEntityId::SignatureKeyPair => {
+                let concrete_signature_keypair: &SignatureKeyPair = v
+                    .downcast()
+                    .expect("There's an implementation issue in OpenMLS. This shouln't be happening.");
+
+                // Having an empty credential id seems tolerable, since the SignatureKeyPair type is retrieved from the key store via its public key.
+                let credential_id = vec![];
+                let kp = MlsSignatureKeyPair::new(
+                    concrete_signature_keypair.signature_scheme(),
+                    k.into(),
+                    data,
+                    credential_id,
+                );
+                self.save(kp).await?;
+            }
+            MlsEntityId::KeyPackage => {
+                let kp = MlsKeyPackage {
+                    keypackage_ref: k.into(),
+                    keypackage: data,
+                };
+                self.save(kp).await?;
+            }
+            MlsEntityId::HpkePrivateKey => {
+                let kp = MlsHpkePrivateKey { pk: k.into(), sk: data };
+                self.save(kp).await?;
+            }
+            MlsEntityId::PskBundle => {
+                let kp = MlsPskBundle {
+                    psk_id: k.into(),
+                    psk: data,
+                };
+                self.save(kp).await?;
+            }
+            MlsEntityId::EncryptionKeyPair => {
+                let kp = MlsEncryptionKeyPair { pk: k.into(), sk: data };
+                self.save(kp).await?;
+            }
+            MlsEntityId::EpochEncryptionKeyPair => {
+                let kp = MlsEpochEncryptionKeyPair {
+                    id: k.into(),
+                    keypairs: data,
+                };
+                self.save(kp).await?;
+            }
+        }
+
+        Ok(())
+    }
+
+    async fn read<V: MlsEntity>(&self, k: &[u8]) -> Option<V>
+    where
+        Self: Sized,
+    {
+        if k.is_empty() {
+            return None;
+        }
+
+        match V::ID {
+            MlsEntityId::GroupState => {
+                let group: PersistedMlsGroup = self.find(k).await.ok().flatten()?;
+                deser(&group.state).ok()
+            }
+            MlsEntityId::SignatureKeyPair => {
+                let sig: MlsSignatureKeyPair = self.find(k).await.ok().flatten()?;
+                deser(&sig.keypair).ok()
+            }
+            MlsEntityId::KeyPackage => {
+                let kp: MlsKeyPackage = self.find(k).await.ok().flatten()?;
+                deser(&kp.keypackage).ok()
+            }
+            MlsEntityId::HpkePrivateKey => {
+                let hpke_pk: MlsHpkePrivateKey = self.find(k).await.ok().flatten()?;
+                deser(&hpke_pk.sk).ok()
+            }
+            MlsEntityId::PskBundle => {
+                let psk_bundle: MlsPskBundle = self.find(k).await.ok().flatten()?;
+                deser(&psk_bundle.psk).ok()
+            }
+            MlsEntityId::EncryptionKeyPair => {
+                let kp: MlsEncryptionKeyPair = self.find(k).await.ok().flatten()?;
+                deser(&kp.sk).ok()
+            }
+            MlsEntityId::EpochEncryptionKeyPair => {
+                let kp: MlsEpochEncryptionKeyPair = self.find(k).await.ok().flatten()?;
+                deser(&kp.keypairs).ok()
+            }
+        }
+    }
+
+    async fn delete<V: MlsEntity>(&self, k: &[u8]) -> Result<(), Self::Error> {
+        if k.is_empty() {
+            return Ok(());
+        }
+
+        match V::ID {
+            MlsEntityId::GroupState => self.remove::<PersistedMlsGroup, _>(k).await?,
+            MlsEntityId::SignatureKeyPair => self.remove::<MlsSignatureKeyPair, _>(k).await?,
+            MlsEntityId::HpkePrivateKey => self.remove::<MlsHpkePrivateKey, _>(k).await?,
+            MlsEntityId::KeyPackage => self.remove::<MlsKeyPackage, _>(k).await?,
+            MlsEntityId::PskBundle => self.remove::<MlsPskBundle, _>(k).await?,
+            MlsEntityId::EncryptionKeyPair => self.remove::<MlsEncryptionKeyPair, _>(k).await?,
+            MlsEntityId::EpochEncryptionKeyPair => self.remove::<MlsEpochEncryptionKeyPair, _>(k).await?,
+        }
+
+        Ok(())
+    }
+}
