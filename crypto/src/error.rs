8a65f3acaa29311587db39facaeebea606b06177
SimonThormeyer <simonthormeyer@gmail.com>
2024-12-12 08:57:34 +0100

    error

--- crypto/src/error.rs
+++ crypto/src/error.rs
@@ -113,6 +113,18 @@ pub enum CryptoError {
     /// Standard I/O Error
     #[error(transparent)]
     IoError(#[from] std::io::Error),
+    /// Mls Transport Callbacks were not provided
+    #[error("The mls transport callbacks needed for CoreCrypto to operate were not set")]
+    MlsTransportNotProvided,
+    /// Any error that occurs during mls transport.
+    #[error("Error during mls transport: {0}")]
+    ErrorDuringMlsTransport(String),
+    /// Message rejected by the delivery service
+    #[error("Message rejected by the delivery service. Reason: {reason}")]
+    MessageRejected {
+        /// Why was the message rejected by the delivery service?
+        reason: String,
+    },
     /// A supplied [`openmls::ciphersuite::hash_ref::HashReference`] is not of the expected size: 16
     #[error("A supplied reference is not of the expected size: 16")]
     InvalidHashReference,
