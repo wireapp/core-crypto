9d0dc599513073a83003860df2197d3075a7c8d6
SimonThormeyer <simon.thormeyer@wire.com>
2025-01-17 13:10:47 +0100

    fix(web): broken error type mapping in try/catch patterns.
    Apparently the `CoreCryptoError.fromStdError()` function does not work as expected.
    
    We can easily fix error type mapping by doing an early return at least for the cases
    where this function is called with an error that is already an instance of `CoreCryptoError`.
    
    Now calling this function at least doesn't make things worse.

--- crypto-ffi/bindings/js/src/CoreCryptoError.ts
+++ crypto-ffi/bindings/js/src/CoreCryptoError.ts
@@ -55,6 +55,9 @@ export class CoreCryptoError extends Error {
     }
 
     static fromStdError(e: Error): CoreCryptoError | Error {
+        if (e instanceof CoreCryptoError) {
+            return e;
+        }
         const opts = {
             cause: e.cause || undefined,
             stack: e.stack || undefined,
