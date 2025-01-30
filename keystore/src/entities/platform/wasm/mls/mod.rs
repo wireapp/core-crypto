1afca7dbff944c3d805dc510940a74fb1e16d139
SimonThormeyer <simon.thormeyer@wire.com>
2025-01-30 12:17:39 +0100

    refactor: use `Entity` derive macro for tables with hex ids

--- keystore/src/entities/platform/wasm/mls/mod.rs
+++ keystore/src/entities/platform/wasm/mls/mod.rs
@@ -17,10 +17,8 @@
 pub mod credential;
 pub mod e2ei_acme_ca;
 pub mod encryption_keypair;
-pub mod epoch_encryption_keypair;
 pub mod group;
 pub mod hpke_private_key;
-pub mod keypackage;
 pub mod pending_message;
 pub mod psk_bundle;
 pub mod refresh_token;
