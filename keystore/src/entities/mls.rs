9dacd241f6f3761ab14854708f6f039c11ece7bc
Peter Goodspeed-Niklaus <peter.goodspeed-niklaus@wire.com>
2025-02-06 15:24:54 +0100

    chore: add `MlsBufferedCommit` to keystore entities list

--- keystore/src/entities/mls.rs
+++ keystore/src/entities/mls.rs
@@ -91,6 +91,48 @@ pub struct MlsPendingMessage {
     pub message: Vec<u8>,
 }
 
+/// Entity representing a buffered commit.
+///
+/// There should always exist either 0 or 1 of these in the store per conversation.
+/// Commits are buffered if not all proposals they reference have yet been received.
+///
+/// We don't automatically zeroize on drop because the commit data is still encrypted at this point;
+/// it is not risky to leave it in memory.
+#[derive(Debug, Clone, PartialEq, Eq, Zeroize, core_crypto_macros::Entity)]
+#[cfg_attr(
+    any(target_family = "wasm", feature = "serde"),
+    derive(serde::Serialize, serde::Deserialize)
+)]
+pub struct MlsBufferedCommit {
+    // we'd ideally just call this field `conversation_id`, but as of right now the
+    // Entity macro does not yet support id columns not named `id`
+    #[id(hex, column = "conversation_id_hex")]
+    conversation_id: Vec<u8>,
+    commit_data: Vec<u8>,
+}
+
+impl MlsBufferedCommit {
+    /// Create a new `Self` from conversation id and the commit data.
+    pub fn new(conversation_id: Vec<u8>, commit_data: Vec<u8>) -> Self {
+        Self {
+            conversation_id,
+            commit_data,
+        }
+    }
+
+    pub fn conversation_id(&self) -> &[u8] {
+        &self.conversation_id
+    }
+
+    pub fn commit_data(&self) -> &[u8] {
+        &self.commit_data
+    }
+
+    pub fn into_commit_data(self) -> Vec<u8> {
+        self.commit_data
+    }
+}
+
 /// Entity representing a persisted `Credential`
 #[derive(Debug, Clone, PartialEq, Eq, Zeroize)]
 #[zeroize(drop)]
