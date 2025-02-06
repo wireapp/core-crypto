c8c9937f480e03d096bf509f29d601695a96e5fb
Peter Goodspeed-Niklaus <peter.goodspeed-niklaus@wire.com>
2025-02-06 15:24:54 +0100

    chore: add `MlsBufferedCommit` to keystore entities list

--- keystore/src/error.rs
+++ keystore/src/error.rs
@@ -35,6 +35,8 @@ pub enum MissingKeyErrorKind {
     MlsPskBundle,
     #[error("MLS CredentialBundle")]
     MlsCredential,
+    #[error("MLS Buffered Commit")]
+    MlsBufferedCommit,
     #[error("MLS Persisted Group")]
     MlsGroup,
     #[error("MLS Persisted Pending Group")]
