4420b8d7fac9ebb93dff78ad5042f0a9913fde18
coriolinus <coriolinus@users.noreply.github.com>
2025-02-12 16:49:01 +0000

    deploy: 681d50911d3333d2c96b2de7363c47b7ee8e0104

---
+++ interop/build/web/wasm/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":["build_wasm","spawn_http_server"]};
---
+++ trait.impl/clap_builder/derive/trait.Subcommand.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["decode",[["impl Subcommand for <a class=\"enum\" href=\"decode/enum.Command.html\" title=\"enum decode::Command\">Command</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[131]}
