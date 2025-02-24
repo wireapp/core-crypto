185f78f14af87076073d3d942ac2b7ee709cd768
Peter Goodspeed-Niklaus <peter.goodspeed-niklaus@wire.com>
2025-02-24 11:41:15 +0100

    chore: do wasm tests in a proper matrix instead of a shell script
    This gives us both better visibility, in case some or all of them
    fail, and also better speed via parallelism.
    
    Also unify the various wasm test scripts; no reason not to do them
    all as part of one big matrix.

--- crypto-ffi/bindings/js/test/wasm-tests.sh
+++
@@ -1,4 +0,0 @@
-wasm-pack test --headless --chrome ./crypto -- e2e_identity
-wasm-pack test --headless --chrome ./crypto -- mls
-wasm-pack test --headless --chrome ./crypto -- group_store
-wasm-pack test --headless --chrome ./crypto -- proteus
