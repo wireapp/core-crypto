0c78efee8689b726f2eceba37330d2b40be41cd9
Ivan Stanković <ivan.stankovic@wire.com>
2025-02-28 11:14:07 +0100

    fix!: crypto-ffi: use DatabaseKey instead of string for the database key

--- crypto-ffi/src/wasm/mod.rs
+++ crypto-ffi/src/wasm/mod.rs
@@ -1455,7 +1455,7 @@ impl CoreCrypto {
     /// see [core_crypto::mls::MlsCentral::try_new]
     pub async fn _internal_new(
         path: String,
-        key: String,
+        key: DatabaseKey,
         client_id: FfiClientId,
         ciphersuites: Box<[u16]>,
         entropy_seed: Option<Box<[u8]>>,
@@ -1469,7 +1469,7 @@ impl CoreCrypto {
             .expect("we never run corecrypto on systems with architectures narrower than 32 bits");
         let configuration = MlsCentralConfiguration::try_new(
             path,
-            key,
+            key.0,
             Some(client_id.into()),
             ciphersuites,
             entropy_seed,
@@ -1488,11 +1488,11 @@ impl CoreCrypto {
     /// see [core_crypto::mls::MlsCentral::try_new]
     pub async fn deferred_init(
         path: String,
-        key: String,
+        key: DatabaseKey,
         entropy_seed: Option<Box<[u8]>>,
     ) -> WasmCryptoResult<CoreCrypto> {
         let entropy_seed = entropy_seed.map(|s| s.to_vec());
-        let configuration = MlsCentralConfiguration::try_new(path, key, None, vec![], entropy_seed, None)
+        let configuration = MlsCentralConfiguration::try_new(path, key.0, None, vec![], entropy_seed, None)
             .map_err(CoreCryptoError::from)?;
 
         let central = MlsCentral::try_new(configuration)
@@ -2373,5 +2373,18 @@ impl From<core_crypto::e2e_identity::CrlRegistration> for CrlRegistration {
             dirty: value.dirty,
             expiration: value.expiration,
         }
+    }
+}
+
+#[wasm_bindgen]
+#[derive(Debug)]
+pub struct DatabaseKey(core_crypto_keystore::DatabaseKey);
+
+#[wasm_bindgen]
+impl DatabaseKey {
+    #[wasm_bindgen(constructor)]
+    pub fn new(buf: &[u8]) -> Result<DatabaseKey, wasm_bindgen::JsError> {
+        let key = core_crypto_keystore::DatabaseKey::try_from(buf).map_err(wasm_bindgen::JsError::new)?;
+        Ok(DatabaseKey(key))
     }
 }
