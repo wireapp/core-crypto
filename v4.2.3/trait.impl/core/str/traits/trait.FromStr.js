b54021c37f4b5876b67f43a77189ac9e80bc453f
typfel <typfel@users.noreply.github.com>
2025-03-14 16:38:26 +0000

    deploy: 6c1867211d55d009f7ad42e70a01707283fd766b

---
+++ v4.2.3/trait.impl/core/str/traits/trait.FromStr.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["core_crypto",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/str/traits/trait.FromStr.html\" title=\"trait core::str::traits::FromStr\">FromStr</a> for <a class=\"struct\" href=\"core_crypto/prelude/struct.ClientId.html\" title=\"struct core_crypto::prelude::ClientId\">ClientId</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[312]}
