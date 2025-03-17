accb116431fc57e1cddc65ad07e58093ac8baec9
Peter Goodspeed-Niklaus <peter.goodspeed-niklaus@wire.com>
2025-03-17 11:10:23 +0100

    refactor: move client_id to a unified module

---
+++ crypto-ffi/src/client_id.rs
@@ -0,0 +1,10 @@
+#[derive(Debug, Clone, Eq, Hash, PartialEq, derive_more::From)]
+pub struct ClientId(pub(crate) core_crypto::prelude::ClientId);
+
+#[cfg(not(target_family = "wasm"))]
+uniffi::custom_type!(ClientId, Vec<u8>, {
+    lower: |id| id.0.to_vec(),
+    try_lift: |vec| Ok(Self(core_crypto::prelude::ClientId::from(vec)))
+});
+
+pub type FfiClientId = Box<[u8]>;
