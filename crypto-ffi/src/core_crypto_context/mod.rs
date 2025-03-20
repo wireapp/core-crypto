2f81a68a85047712a7252a2c8aeb60cf3f83d19e
Peter Goodspeed-Niklaus <peter.goodspeed-niklaus@wire.com>
2025-03-20 10:22:50 +0100

    refactor: unify `CoreCryptoContext::(set|get)_data` impls

--- crypto-ffi/src/core_crypto_context/mod.rs
+++ crypto-ffi/src/core_crypto_context/mod.rs
@@ -5,6 +5,8 @@ use wasm_bindgen::prelude::*;
 
 use core_crypto::context::CentralContext;
 
+use crate::CoreCryptoResult;
+
 #[derive(Debug)]
 #[cfg_attr(target_family = "wasm", wasm_bindgen)]
 #[cfg_attr(not(target_family = "wasm"), derive(uniffi::Object))]
@@ -17,5 +19,19 @@ impl Deref for CoreCryptoContext {
 
     fn deref(&self) -> &Self::Target {
         self.inner.deref()
+    }
+}
+
+#[cfg_attr(target_family = "wasm", wasm_bindgen)]
+#[cfg_attr(not(target_family = "wasm"), uniffi::export)]
+impl CoreCryptoContext {
+    /// See [core_crypto::context::CentralContext::set_data].
+    pub async fn set_data(&self, data: Vec<u8>) -> CoreCryptoResult<()> {
+        self.inner.set_data(data).await.map_err(Into::into)
+    }
+
+    /// See [core_crypto::context::CentralContext::get_data].
+    pub async fn get_data(&self) -> CoreCryptoResult<Option<Vec<u8>>> {
+        self.inner.get_data().await.map_err(Into::into)
     }
 }
