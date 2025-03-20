baf9a4707565d65f80eb708ada9f449faac10eb2
Peter Goodspeed-Niklaus <peter.goodspeed-niklaus@wire.com>
2025-03-20 13:24:12 +0100

    refactor: unify the CoreCryptoContext proteus impls

--- crypto-ffi/src/proteus.rs
+++ crypto-ffi/src/proteus.rs
@@ -3,14 +3,11 @@
 /// The `macro_export` macro ensures this shows up at the crate root in all cases.
 #[macro_export]
 macro_rules! proteus_impl {
-    ($body:block or throw $result:ty) => {{
+    ($body:block) => {{
         if cfg!(feature = "proteus") {
             $body
         } else {
-            <$result>::Err(core_crypto::Error::FeatureDisabled("proteus").into())
+            Err(core_crypto::Error::FeatureDisabled("proteus").into())
         }
     }};
-    ($body:block) => {
-        proteus_impl!($body or throw std::result::Result::<_, _>)
-    }
 }
--- crypto-ffi/src/wasm/context/mod.rs
+++ crypto-ffi/src/wasm/context/mod.rs
@@ -1,4 +1,3 @@
 use crate::CoreCryptoContext;
 
 pub mod e2ei;
-pub mod proteus;
