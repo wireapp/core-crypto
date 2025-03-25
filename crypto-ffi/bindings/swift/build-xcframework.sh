6dfc4b15969bd241970f8c7b59bc8a228b6b98c2
Jacob Persson <7156+typfel@users.noreply.github.com>
2025-03-25 14:53:44 +0100

    fix: broken swift bindings by publishing uniffi framework separately

--- crypto-ffi/bindings/swift/build-xcframework.sh
+++ crypto-ffi/bindings/swift/build-xcframework.sh
@@ -3,6 +3,31 @@
 set -e
 
 xcodebuild archive \
+    -project 'WireCoreCryptoUniffi/WireCoreCryptoUniffi.xcodeproj' \
+    -scheme 'WireCoreCryptoUniffi' \
+    -configuration Release \
+    -archivePath 'WireCoreCryptoUniffi-ios.xcarchive' \
+    -destination 'generic/platform=ios' \
+    SKIP_INSTALL=NO \
+    BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+
+xcodebuild archive \
+    -project 'WireCoreCryptoUniffi/WireCoreCryptoUniffi.xcodeproj' \
+    -scheme 'WireCoreCryptoUniffi' \
+    -configuration Release \
+    -archivePath 'WireCoreCryptoUniffi-simulator.xcarchive' \
+    -destination 'generic/platform=ios Simulator' \
+    SKIP_INSTALL=NO \
+    BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+
+xcodebuild -create-xcframework \
+    -archive 'WireCoreCryptoUniffi-ios.xcarchive' \
+    -framework 'WireCoreCryptoUniffi.framework' \
+    -archive 'WireCoreCryptoUniffi-simulator.xcarchive' \
+    -framework 'WireCoreCryptoUniffi.framework' \
+    -output 'WireCoreCryptoUniffi.xcframework'
+
+xcodebuild archive \
     -project 'WireCoreCrypto/WireCoreCrypto.xcodeproj' \
     -scheme 'WireCoreCrypto' \
     -configuration Release \
@@ -28,7 +53,11 @@ xcodebuild -create-xcframework \
     -output 'WireCoreCrypto.xcframework'
 
 zip -r WireCoreCrypto.xcframework.zip WireCoreCrypto.xcframework
+zip -r WireCoreCryptoUniffi.xcframework.zip WireCoreCryptoUniffi.xcframework
 
+rm -rf WireCoreCryptoUniffi.xcframework
+rm -rf WireCoreCryptoUniffi-ios.xcarchive
+rm -rf WireCoreCryptoUniffi-simulator.xcarchive
 rm -rf WireCoreCrypto.xcframework
 rm -rf WireCoreCrypto-ios.xcarchive
 rm -rf WireCoreCrypto-simulator.xcarchive
