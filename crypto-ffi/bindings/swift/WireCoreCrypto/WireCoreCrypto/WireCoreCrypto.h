2b950cc215c7f4d40e3bb5515224f7dcc721d2c5
Jacob Persson <7156+typfel@users.noreply.github.com>
2025-03-25 14:53:44 +0100

    fix: broken swift bindings by publishing uniffi framework separately

--- crypto-ffi/bindings/swift/WireCoreCrypto/WireCoreCrypto/WireCoreCrypto.h
+++ crypto-ffi/bindings/swift/WireCoreCrypto/WireCoreCrypto/WireCoreCrypto.h
@@ -18,12 +18,12 @@
 
 #import <Foundation/Foundation.h>
 
-//! Project version number for WireCoreCryptoWrapper.
+//! Project version number for WireCoreCrypto.
 FOUNDATION_EXPORT double WireCoreCryptoVersionNumber;
 
-//! Project version string for WireCoreCryptoWrapper.
+//! Project version string for WireCoreCrypto.
 FOUNDATION_EXPORT const unsigned char WireCoreCryptoVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <WireCoreCryptoWrapper/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import <WireCoreCrypto/PublicHeader.h>
 
 
