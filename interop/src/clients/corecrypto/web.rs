dbd9f981ca86fca4979403e17d7e541ede2852fe
Ivan Stanković <ivan.stankovic@wire.com>
2025-03-26 16:33:44 +0100

    chore: remove unnecessary license and copyright notices [WPB-14642]
    Licensing terms and copyright information is already present in other
    places, such as the LICENSE file and README.

--- interop/src/clients/corecrypto/web.rs
+++ interop/src/clients/corecrypto/web.rs
@@ -1,19 +1,3 @@
-// Wire
-// Copyright (C) 2022 Wire Swiss GmbH
-
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
-
-// You should have received a copy of the GNU General Public License
-// along with this program. If not, see http://www.gnu.org/licenses/.
-
 use crate::{
     CIPHERSUITE_IN_USE,
     clients::{EmulatedClient, EmulatedClientProtocol, EmulatedClientType, EmulatedMlsClient},
@@ -129,7 +113,7 @@ impl EmulatedMlsClient for CoreCryptoWebClient {
             .execute_async(
                 r#"
 const [ciphersuite, callback] = arguments;
-window.cc.transaction((ctx) => 
+window.cc.transaction((ctx) =>
     ctx.clientKeypackages(ciphersuite, window.credentialType, 1)
 ).then(([kp]) => callback(kp));"#,
                 vec![serde_json::json!(ciphersuite)],
