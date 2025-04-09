adce1a81fefe16cd56ae183ba853eba50ac80c39
SimonThormeyer <simon.thormeyer@wire.com>
2025-04-09 17:08:38 +0200

    test: port tests of static functions to bun
    Currently, we cannot instantiate core crypto, so for now we can only
    port tests that use static functions.

---
+++ crypto-ffi/bindings/js/testBun/metadata.test.ts
@@ -0,0 +1,28 @@
+import { setup, teardown } from "./utils";
+import { afterEach, test, beforeEach, describe, expect } from "bun:test";
+import { buildMetadata, version } from "../src/CoreCrypto";
+
+beforeEach(async () => {
+    await setup();
+});
+
+afterEach(async () => {
+    await teardown();
+});
+
+describe("metadata", () => {
+    test("metadata can be retrieved and contains key 'gitDescribe'", async () => {
+        expect(buildMetadata().toJSON()).toMatchObject({
+            gitDescribe: expect.anything(),
+        });
+    });
+
+    test("version can be retrieved and is a semantic version number", async () => {
+        expect(version()).toMatch(
+            RegExp(
+                // Regex for matching semantic versions from https://semver.org
+                "^(0|[1-9]\\d*)\\.(0|[1-9]\\d*)\\.(0|[1-9]\\d*)(?:-((?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\\.(?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\\+([0-9a-zA-Z-]+(?:\\.[0-9a-zA-Z-]+)*))?$"
+            )
+        );
+    });
+});
