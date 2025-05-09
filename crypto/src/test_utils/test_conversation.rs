b332e7998cadccfa11ebe17cb95a6f5def5734da
Peter Goodspeed-Niklaus <peter.goodspeed-niklaus@wire.com>
2025-05-09 13:18:42 +0200

    feat(test): add `TestConversation::external_join`
    This returns a `ExternalJoinGuard` which can be used to notify the
    existing members, or which contains sufficient information to
    do stuff for more complicated test cases if desired.

--- crypto/src/test_utils/test_conversation.rs
+++ crypto/src/test_utils/test_conversation.rs
@@ -1,3 +1,5 @@
+use openmls::prelude::MlsMessageOut;
+
 use crate::prelude::ConversationId;
 
 use super::{SessionContext, TestContext, conversation_id};
@@ -46,6 +48,73 @@ impl<'a> TestConversation<'a> {
             )
             .await
             .expect("all invitations succeeded");
+    }
+
+    /// All members of this conversation.
+    ///
+    /// The creator is always the first member returned.
+    pub fn members(&self) -> impl Iterator<Item = &SessionContext> {
+        std::iter::once(self.creator).chain(self.joiners.iter().copied())
+    }
+}
+
+/// This struct encapsulates the result of a join by external commit.
+///
+/// To notify all existing members of the conversation, call [`Self::notify_existing_members`].
+/// Otherwise, use the struct members to do things manually.
+pub struct ExternalJoinGuard<'a> {
+    conversation_id: &'a ConversationId,
+    pub(crate) previous_conversation_members: Vec<&'a SessionContext>,
+    // this is dead code for now, but we expect to use it in the relatively near future
+    // once we start using this in tests. At that point, remove the annotation.
+    #[expect(dead_code)]
+    pub(crate) joiner: &'a SessionContext,
+    pub(crate) join_commit: MlsMessageOut,
+}
+
+impl ExternalJoinGuard<'_> {
+    pub async fn notify_existing_members(self) {
+        let message_bytes = self.join_commit.to_bytes().unwrap();
+        for member in self.previous_conversation_members {
+            member
+                .transaction
+                .conversation(self.conversation_id)
+                .await
+                .unwrap()
+                .decrypt_message(&message_bytes)
+                .await
+                .unwrap();
+        }
+    }
+}
+
+impl<'a> TestConversation<'a> {
+    /// The supplied session joins this conversation by external commit.
+    ///
+    /// This does _not_ distribute the external commit to the existing members. To do that,
+    /// use the [`notify_existing_members` method][ExternalJoinGuard::notify_existing_members] of
+    /// the returned item.
+    pub async fn external_join(&'a mut self, joiner: &'a SessionContext) -> ExternalJoinGuard<'a> {
+        let group_info = self.creator.get_group_info(&self.id).await;
+        joiner
+            .transaction
+            .join_by_external_commit(group_info, self.case.custom_cfg(), self.case.credential_type)
+            .await
+            .unwrap();
+        let join_commit = joiner.mls_transport.latest_commit().await;
+
+        let mut previous_conversation_members = Vec::with_capacity(self.joiners.len() + 1);
+        previous_conversation_members.push(self.creator);
+        previous_conversation_members.extend(self.joiners.iter().copied());
+
+        self.joiners.push(joiner);
+
+        ExternalJoinGuard {
+            conversation_id: &self.id,
+            previous_conversation_members,
+            joiner,
+            join_commit,
+        }
     }
 }
 
