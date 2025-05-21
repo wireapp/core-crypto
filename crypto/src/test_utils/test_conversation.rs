c7907bceb74b30c89810d0dd5a66e21df8b5ec4a
SimonThormeyer <simon.thormeyer@wire.com>
2025-05-21 14:11:04 +0200

    refactor(test conversation): move extension of member list to `CommitGuard::notify_members()`
    The `TestConversation` should only be aware that new members were added
    once the commit was distributed. This affects
    `TestConversation::invite()` and `TestConversation::external_join`, the
    only operations so far that produce a `CommitGuard`.

--- crypto/src/test_utils/test_conversation.rs
+++ crypto/src/test_utils/test_conversation.rs
@@ -14,8 +14,7 @@ pub struct TestConversation<'a> {
     pub(crate) case: &'a TestContext,
     #[as_ref]
     pub(crate) id: ConversationId,
-    pub(crate) creator: &'a SessionContext,
-    pub(crate) joiners: Vec<&'a SessionContext>,
+    pub(crate) members: Vec<&'a SessionContext>,
 }
 
 impl<'a> TestConversation<'a> {
@@ -30,8 +29,7 @@ impl<'a> TestConversation<'a> {
         Self {
             case,
             id,
-            creator,
-            joiners: Vec::new(),
+            members: vec![creator],
         }
     }
 
@@ -46,50 +44,45 @@ impl<'a> TestConversation<'a> {
     }
 
     pub async fn invite(&'a mut self, sessions: impl IntoIterator<Item = &'a SessionContext>) -> CommitGuard<'a> {
-        let idx_of_first_new_member = self.joiners.len();
-        let sessions = sessions.into_iter();
-        let (lower_bound, _) = sessions.size_hint();
-        self.joiners.reserve(lower_bound);
-        self.joiners.extend(sessions);
-        let new_members = &self.joiners[idx_of_first_new_member..];
-        let new_members_iter = new_members.iter();
-        let (lower_bound, _) = new_members_iter.size_hint();
+        let new_members = sessions.into_iter().collect::<Vec<_>>();
 
-        let mut key_packages = Vec::with_capacity(lower_bound);
-        for cc in new_members_iter {
-            let kp = cc.rand_key_package(self.case).await;
-            key_packages.push(kp);
-        }
+        let key_packages =
+            futures_util::future::join_all(new_members.iter().map(|cc| cc.rand_key_package(self.case))).await;
         self.guard().await.add_members(key_packages).await.unwrap();
         let welcome = self.transport().latest_commit_bundle().await.welcome.unwrap();
         let commit = self.transport().latest_commit_bundle().await.commit;
-        let existing_members = self.joiners[..idx_of_first_new_member].to_vec();
         CommitGuard {
             conversation: self,
-            members_to_notify: existing_members,
-            committer: self.creator,
+            committed_operation: CommittedOperation::Add(AddGuard {
+                committer_index: 0,
+                new_members,
+                welcome_message: welcome,
+            }),
             commit,
-            invited_members: Some((new_members.to_vec(), welcome)),
         }
+    }
+
+    pub fn creator(&self) -> &SessionContext {
+        self.members[0]
     }
 
     /// All members of this conversation.
     ///
-    /// The creator is always the first member returned.
+    /// The creator is always the first member returned (if they're still a member).
     pub fn members(&self) -> impl Iterator<Item = &SessionContext> {
-        std::iter::once(self.creator).chain(self.joiners.iter().copied())
+        self.members.iter().copied()
     }
 
     /// Convenience function to get the mls transport of the creator.
     pub fn transport(&self) -> Arc<dyn MlsTransportTestExt> {
-        self.creator.mls_transport.clone()
+        self.creator().mls_transport.clone()
     }
 
     /// Convenience function to get the conversation guard of this conversation.
     ///
     /// The guard belongs to the creator of the conversation.
     pub async fn guard(&self) -> ConversationGuard {
-        self.creator.transaction.conversation(&self.id).await.unwrap()
+        self.creator().transaction.conversation(&self.id).await.unwrap()
     }
 
     /// Remove this member from this conversation.
@@ -99,9 +92,14 @@ impl<'a> TestConversation<'a> {
     /// Panics if you try to remove the conversation creator; use a different abstraction if you are testing that case.
     /// Panics if you try to remove someone who is not a current member.
     pub async fn remove(&mut self, member_id: &ClientId) -> &'a SessionContext {
+        assert_ne!(
+            &self.creator().session.id().await.unwrap(),
+            member_id,
+            "cannot remove the creator via this API because we're acting on the creators behalf."
+        );
         // can't use `Iterator::position` because getting the id is async
         let mut joiner_idx = None;
-        for (idx, joiner) in self.joiners.iter().enumerate() {
+        for (idx, joiner) in self.members.iter().enumerate() {
             let joiner_id = joiner.session.id().await.unwrap();
             if joiner_id == *member_id {
                 joiner_idx = Some(idx);
@@ -111,7 +109,7 @@ impl<'a> TestConversation<'a> {
 
         // if we didn't find it, return early instead of trying to apply that removal to the conversation
         let removed = joiner_idx
-            .map(|idx| self.joiners.swap_remove(idx))
+            .map(|idx| self.members.swap_remove(idx))
             .expect("could find the member to remove among the joiners of this conversation");
 
         // removing the member here removes it from the creator and also produces a commit
@@ -123,7 +121,7 @@ impl<'a> TestConversation<'a> {
         let commit = self.transport().latest_commit().await.to_bytes().unwrap();
 
         // we already removed the member from the joined members of our conversation, so chain it in
-        for joiner in std::iter::once(removed).chain(self.joiners.iter().copied()) {
+        for joiner in std::iter::once(removed).chain(self.members.iter().copied()) {
             joiner
                 .transaction
                 .conversation(&self.id)
@@ -144,21 +142,52 @@ impl<'a> TestConversation<'a> {
 /// To notify all existing members of the conversation, call [`Self::notify_existing_members`].
 /// Otherwise, use the struct members to do things manually.
 pub struct CommitGuard<'a> {
-    conversation: &'a TestConversation<'a>,
-    pub(crate) members_to_notify: Vec<&'a SessionContext>,
-    // this is dead code for now, but we expect to use it in the relatively near future
-    // once we start using this in tests. At that point, remove the annotation.
-    #[expect(dead_code)]
-    pub(crate) committer: &'a SessionContext,
+    conversation: &'a mut TestConversation<'a>,
+    /// The member at this index won't be included in the list of [Self::members_to_notify]
+    committed_operation: CommittedOperation<'a>,
     pub(crate) commit: MlsMessageOut,
-    /// In case someone was invited to this group, there will be a welcome message.
-    pub(crate) invited_members: Option<(Vec<&'a SessionContext>, MlsMessageOut)>,
 }
 
-impl CommitGuard<'_> {
+struct AddGuard<'a> {
+    committer_index: usize,
+    new_members: Vec<&'a SessionContext>,
+    welcome_message: MlsMessageOut,
+}
+
+/// Keeps state about the comitted operation that will be used when the
+/// corresponding [CommitGuard] is used to (notify members)[CommitGuard::notify_members].
+#[expect(clippy::large_enum_variant)]
+enum CommittedOperation<'a> {
+    /// The member with the provided index won't be notified, new members will be
+    /// added to the member list of the test conversation
+    Add(AddGuard<'a>),
+    /// All existing members will be notified, the new joiner will be added to the
+    /// member list of the conversation.
+    ExternalJoin(&'a SessionContext),
+}
+
+impl<'a> CommitGuard<'a> {
+    fn members_to_notify(&self) -> Box<dyn Iterator<Item = &'a SessionContext> + '_> {
+        if let CommittedOperation::Add(AddGuard {
+            committer_index: skipped_index,
+            ..
+        }) = self.committed_operation
+        {
+            Box::new(
+                self.conversation
+                    .members
+                    .iter()
+                    .enumerate()
+                    .filter_map(move |(idx, member)| (idx != skipped_index).then_some(*member)),
+            )
+        } else {
+            Box::new(self.conversation.members.iter().copied())
+        }
+    }
+
     pub async fn notify_members(self) {
         let message_bytes = self.commit.to_bytes().unwrap();
-        for member in self.members_to_notify {
+        for member in self.members_to_notify() {
             member
                 .transaction
                 .conversation(&self.conversation.id)
@@ -169,12 +198,28 @@ impl CommitGuard<'_> {
                 .unwrap();
         }
 
-        if let Some((invited_members, welcome_message)) = self.invited_members {
-            for new_member in invited_members {
-                new_member
-                    .transaction
-                    .process_welcome_message(welcome_message.clone().into(), self.conversation.case.custom_cfg())
-                    .await;
+        // In case of an external join or an add operation, update the member list
+        // of the test conversation.
+        match self.committed_operation {
+            CommittedOperation::ExternalJoin(joiner) => {
+                self.conversation.members.push(joiner);
+            }
+            CommittedOperation::Add(AddGuard {
+                new_members: invited_members,
+                welcome_message,
+                ..
+            }) => {
+                // Process welcome message on receiver side
+                for new_member in invited_members.iter() {
+                    new_member
+                        .transaction
+                        .process_welcome_message(welcome_message.clone().into(), self.conversation.case.custom_cfg())
+                        .await
+                        .unwrap();
+                }
+                // Then update the member list
+                self.conversation.members.reserve(invited_members.len());
+                self.conversation.members.extend(invited_members);
             }
         }
     }
@@ -184,10 +229,10 @@ impl<'a> TestConversation<'a> {
     /// The supplied session joins this conversation by external commit.
     ///
     /// This does _not_ distribute the external commit to the existing members. To do that,
-    /// use the [`notify_existing_members` method][ExternalJoinGuard::notify_existing_members] of
+    /// use the [`notify_existing_members` method][CommitGuard::notify_members] of
     /// the returned item.
     pub async fn external_join(&'a mut self, joiner: &'a SessionContext) -> CommitGuard<'a> {
-        let group_info = self.creator.get_group_info(&self.id).await;
+        let group_info = self.creator().get_group_info(&self.id).await;
         joiner
             .transaction
             .join_by_external_commit(group_info, self.case.custom_cfg(), self.case.credential_type)
@@ -195,18 +240,10 @@ impl<'a> TestConversation<'a> {
             .unwrap();
         let join_commit = joiner.mls_transport.latest_commit().await;
 
-        let mut previous_conversation_members = Vec::with_capacity(self.joiners.len() + 1);
-        previous_conversation_members.push(self.creator);
-        previous_conversation_members.extend(self.joiners.iter().copied());
-
-        self.joiners.push(joiner);
-
         CommitGuard {
             conversation: self,
-            members_to_notify: previous_conversation_members,
-            committer: joiner,
+            committed_operation: CommittedOperation::ExternalJoin(joiner),
             commit: join_commit,
-            invited_members: None,
         }
     }
 }
