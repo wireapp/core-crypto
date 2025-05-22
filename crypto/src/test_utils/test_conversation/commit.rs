d7aa0aac8ce1b9d73b7eab5fd17c2aa534cd7426
SimonThormeyer <simon.thormeyer@wire.com>
2025-05-22 12:36:46 +0200

    chore(test conversation): modularize test conversation components

---
+++ crypto/src/test_utils/test_conversation/commit.rs
@@ -0,0 +1,160 @@
+use openmls::prelude::group_info::VerifiableGroupInfo;
+use std::marker::PhantomData;
+
+use super::super::SessionContext;
+use super::TestConversation;
+use super::operation_guard::AddGuard;
+use super::operation_guard::Commit;
+use super::operation_guard::OperationGuard;
+use super::operation_guard::TestOperation;
+
+impl<'a> TestConversation<'a> {
+    /// Invite all sessions into this conversation and notify all members, old and new.
+    pub async fn invite(self, sessions: impl IntoIterator<Item = &'a SessionContext>) -> TestConversation<'a> {
+        let commit_guard = self.invite_guarded(sessions).await;
+        commit_guard.notify_members().await
+    }
+
+    /// Invites all sessions into this conversation. Call [CommitGuard::notify_members] to notify other members.
+    pub async fn invite_guarded(
+        self,
+        sessions: impl IntoIterator<Item = &'a SessionContext>,
+    ) -> OperationGuard<'a, Commit> {
+        let new_members = sessions.into_iter().collect::<Vec<_>>();
+
+        let key_packages =
+            futures_util::future::join_all(new_members.iter().map(|cc| cc.rand_key_package(self.case))).await;
+        self.guard().await.add_members(key_packages).await.unwrap();
+        let commit = self.transport().await.latest_commit_bundle().await.commit;
+        let actor_index = self.actor_index();
+        OperationGuard {
+            conversation: self,
+            operation: TestOperation::Add(AddGuard {
+                committer_index: actor_index,
+                new_members,
+            }),
+            message: commit,
+            _message_type: PhantomData,
+        }
+    }
+
+    /// Advance the epoch (by updating the creator's key material) and notify all members.
+    pub async fn advance_epoch(self) -> TestConversation<'a> {
+        self.update().await
+    }
+
+    pub async fn update(self) -> TestConversation<'a> {
+        self.update_guarded().await.notify_members().await
+    }
+
+    pub async fn update_guarded(self) -> OperationGuard<'a, Commit> {
+        self.guard().await.update_key_material().await.unwrap();
+        let commit = self.transport().await.latest_commit_bundle().await.commit;
+        let committer_index = self.actor_index();
+        OperationGuard {
+            conversation: self,
+            operation: TestOperation::Update(committer_index),
+            message: commit,
+            _message_type: PhantomData,
+        }
+    }
+
+    pub async fn commit_pending_proposals(self) -> TestConversation<'a> {
+        self.commit_pending_proposals_guarded().await.notify_members().await
+    }
+
+    pub async fn commit_pending_proposals_guarded(self) -> OperationGuard<'a, Commit> {
+        self.guard().await.commit_pending_proposals().await.unwrap();
+        let commit = self.transport().await.latest_commit().await;
+        let actor_index = self.actor_index();
+        OperationGuard {
+            conversation: self,
+            // Comitting pending proposals is equivalent to an update
+            operation: TestOperation::Update(actor_index),
+            message: commit,
+            _message_type: PhantomData,
+        }
+    }
+
+    /// See [Self::remove_guarded].
+    pub async fn remove(self, member: &'a SessionContext) -> TestConversation<'a> {
+        self.remove_guarded(member).await.notify_members().await
+    }
+
+    /// Remove this member from this conversation.
+    ///
+    /// Applies the removal to all members of the conversation.
+    ///
+    /// Panics if you try to remove the current actor (by default, the conversation creator);
+    /// Panics if you try to remove someone who is not a current member.
+    pub async fn remove_guarded(self, member: &'a SessionContext) -> OperationGuard<'a, Commit> {
+        let member_id = member.session.id().await.unwrap();
+        assert_ne!(
+            member_id,
+            self.actor().session.id().await.unwrap(),
+            "cannot remove the actor because we're acting on the actor's behalf."
+        );
+
+        // removing the member here removes it from the creator and also produces a commit
+        self.guard().await.remove_members(&[member_id]).await.unwrap();
+        let commit = self.transport().await.latest_commit().await;
+        let actor_index = self.actor_index();
+
+        OperationGuard {
+            conversation: self,
+            operation: TestOperation::Remove(actor_index, member),
+            message: commit,
+            _message_type: PhantomData,
+        }
+    }
+    /// See [Self::external_join_guarded]
+    pub async fn external_join(self, joiner: &'a SessionContext) -> TestConversation<'a> {
+        self.external_join_guarded(joiner).await.notify_members().await
+    }
+
+    pub async fn external_join_via_group_info(
+        self,
+        joiner: &'a SessionContext,
+        group_info: VerifiableGroupInfo,
+    ) -> TestConversation<'a> {
+        self.external_join_via_group_info_guarded(joiner, group_info)
+            .await
+            .notify_members()
+            .await
+    }
+
+    /// The supplied session joins this conversation by external commit.
+    ///
+    /// This does _not_ distribute the external commit to the existing members. To do that,
+    /// use the [`notify_existing_members` method][CommitGuard::notify_members] of
+    /// the returned item.
+    pub async fn external_join_guarded(self, joiner: &'a SessionContext) -> OperationGuard<'a, Commit> {
+        let group_info = self.actor().get_group_info(&self.id).await;
+        self.external_join_via_group_info_guarded(joiner, group_info).await
+    }
+
+    /// The supplied session joins this conversation by external commit. The group info is taken from the latest
+    ///
+    /// This does _not_ distribute the external commit to the existing members. To do that,
+    /// use the [`notify_existing_members` method][CommitGuard::notify_members] of
+    /// the returned item.
+    pub async fn external_join_via_group_info_guarded(
+        self,
+        joiner: &'a SessionContext,
+        group_info: VerifiableGroupInfo,
+    ) -> OperationGuard<'a, Commit> {
+        joiner
+            .transaction
+            .join_by_external_commit(group_info, self.case.custom_cfg(), self.case.credential_type)
+            .await
+            .unwrap();
+        let join_commit = joiner.mls_transport().await.latest_commit().await;
+
+        OperationGuard {
+            conversation: self,
+            operation: TestOperation::ExternalJoin(joiner),
+            message: join_commit,
+            _message_type: PhantomData,
+        }
+    }
+}
---
+++ crypto/src/test_utils/test_conversation/proposal.rs
@@ -0,0 +1,75 @@
+use std::marker::PhantomData;
+
+use super::super::SessionContext;
+use super::super::TestConversation;
+use super::operation_guard::{AddGuard, OperationGuard, Proposal, TestOperation};
+
+impl<'a> TestConversation<'a> {
+    pub async fn invite_proposal(self, session: &'a SessionContext) -> TestConversation<'a> {
+        self.invite_proposal_guarded(session).await.notify_members().await
+    }
+
+    pub async fn invite_proposal_guarded(self, new_member: &'a SessionContext) -> OperationGuard<'a, Proposal> {
+        let proposer = self.actor();
+        let key_package = new_member.rand_key_package(self.case).await;
+        let proposal = proposer
+            .transaction
+            .new_add_proposal(self.id(), key_package.into())
+            .await
+            .unwrap()
+            .proposal;
+        let proposer_index = self.member_index(proposer).await;
+        OperationGuard {
+            conversation: self,
+            operation: TestOperation::Add(AddGuard {
+                committer_index: proposer_index,
+                new_members: vec![new_member],
+            }),
+            message: proposal,
+            _message_type: PhantomData,
+        }
+    }
+
+    pub async fn update_proposal(self) -> TestConversation<'a> {
+        self.update_proposal_guarded().await.notify_members().await
+    }
+
+    pub async fn update_proposal_guarded(self) -> OperationGuard<'a, Proposal> {
+        let proposer = self.actor();
+        let proposal = proposer
+            .transaction
+            .new_update_proposal(self.id())
+            .await
+            .unwrap()
+            .proposal;
+        let proposer_index = self.actor_index();
+        OperationGuard {
+            conversation: self,
+            operation: TestOperation::Update(proposer_index),
+            message: proposal,
+            _message_type: PhantomData,
+        }
+    }
+
+    pub async fn remove_proposal(self, member: &'a SessionContext) -> TestConversation<'a> {
+        self.remove_proposal_guarded(member).await.notify_members().await
+    }
+
+    pub async fn remove_proposal_guarded(self, member: &'a SessionContext) -> OperationGuard<'a, Proposal> {
+        let proposer = self.actor();
+        let member_id = member.session.id().await.unwrap();
+        let proposal = proposer
+            .transaction
+            .new_remove_proposal(self.id(), member_id)
+            .await
+            .unwrap()
+            .proposal;
+        let proposer_index = self.actor_index();
+        OperationGuard {
+            conversation: self,
+            operation: TestOperation::Remove(proposer_index, member),
+            message: proposal,
+            _message_type: PhantomData,
+        }
+    }
+}
