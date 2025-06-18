e3397212e58472c5be75d6f43c89900b5d07409c
SimonThormeyer <simon.thormeyer@wire.com>
2025-06-18 17:41:43 +0200

    chore(swift): fix swift tests following breaking change
    We replaced existing error tuple structs with real structs. This
    requires adding argument labels in swift.

--- crypto-ffi/bindings/swift/WireCoreCrypto/WireCoreCryptoTests/WireCoreCryptoTests.swift
+++ crypto-ffi/bindings/swift/WireCoreCrypto/WireCoreCryptoTests/WireCoreCryptoTests.swift
@@ -314,10 +314,9 @@ final class WireCoreCryptoTests: XCTestCase {
         }
 
         let expectedError = CoreCryptoError.Mls(
-            MlsError.ConversationAlreadyExists(
-                conversationId.copyBytes()
-            )
-        )
+            mlsError: MlsError.ConversationAlreadyExists(
+                conversationId: conversationId.copyBytes()
+            ))
 
         try await alice.transaction { ctx in
             await self.XCTAssertThrowsErrorAsync(expectedError) {
@@ -476,7 +475,7 @@ final class WireCoreCryptoTests: XCTestCase {
         XCTAssertEqual(plaintext, message)
 
         try await bob.transaction { context in
-            await self.XCTAssertThrowsErrorAsync(CoreCryptoError.Mls(.DuplicateMessage)) {
+            await self.XCTAssertThrowsErrorAsync(CoreCryptoError.Mls(mlsError: .DuplicateMessage)) {
                 _ = try await context.decryptMessage(
                     conversationId: conversationId, payload: ciphertext)
             }
