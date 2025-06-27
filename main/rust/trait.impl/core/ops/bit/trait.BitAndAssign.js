06ad67cdcb97cd2b9c36028b5e28de13d2f85370
coriolinus <coriolinus@users.noreply.github.com>
2025-06-27 07:55:52 +0000

    deploy: ff13613ad270a31b2c4b4cee8738ef8cc730ca91

--- main/rust/trait.impl/core/ops/bit/trait.BitAndAssign.js
+++ main/rust/trait.impl/core/ops/bit/trait.BitAndAssign.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["interop",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/ops/bit/trait.BitAndAssign.html\" title=\"trait core::ops::bit::BitAndAssign\">BitAndAssign</a> for <a class=\"struct\" href=\"interop/clients/struct.EmulatedClientProtocol.html\" title=\"struct interop::clients::EmulatedClientProtocol\">EmulatedClientProtocol</a>"]]]]);
+    var implementors = Object.fromEntries([["interop",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/ops/bit/trait.BitAndAssign.html\" title=\"trait core::ops::bit::BitAndAssign\">BitAndAssign</a> for <a class=\"struct\" href=\"interop/clients/struct.EmulatedClientProtocol.html\" title=\"struct interop::clients::EmulatedClientProtocol\">EmulatedClientProtocol</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- main/rust/trait.impl/core/ops/bit/trait.Not.js
+++ main/rust/trait.impl/core/ops/bit/trait.Not.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["interop",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/ops/bit/trait.Not.html\" title=\"trait core::ops::bit::Not\">Not</a> for <a class=\"struct\" href=\"interop/clients/struct.EmulatedClientProtocol.html\" title=\"struct interop::clients::EmulatedClientProtocol\">EmulatedClientProtocol</a>"]]]]);
+    var implementors = Object.fromEntries([["interop",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/ops/bit/trait.Not.html\" title=\"trait core::ops::bit::Not\">Not</a> for <a class=\"struct\" href=\"interop/clients/struct.EmulatedClientProtocol.html\" title=\"struct interop::clients::EmulatedClientProtocol\">EmulatedClientProtocol</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
