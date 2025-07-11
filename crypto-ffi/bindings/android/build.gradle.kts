efea21f1bde6750ef096d39598830f5b9e458e9c
Peter Goodspeed-Niklaus <peter.goodspeed-niklaus@wire.com>
2025-07-11 11:47:59 +0200

    refactor!(jvm): Revert "refactor!: move uniffi generated code into a separate library"
    This reverts commit 38649b9d310f3f0171f91ef7f6007011c3f809b9.
    
    At this point we want the Android/JVM code to be thin enough that
    it makes more sense to have a single library. So let's build it like that
    and then fix whatever fails in consequence.

--- crypto-ffi/bindings/android/build.gradle.kts
+++ crypto-ffi/bindings/android/build.gradle.kts
@@ -13,11 +13,21 @@ val dokkaHtmlJar = tasks.register<Jar>("dokkaHtmlJar") {
     archiveClassifier.set("html-docs")
 }
 
+val generatedDir = buildDir.resolve("generated").resolve("uniffi")
+
+val copyBindings by tasks.register<Copy>("copyBindings") {
+    group = "uniffi"
+    from(kotlinSources)
+    include("**/*")
+    exclude("**/CoreCrypto.kt", "**/core_crypto.kt")
+    into(generatedDir)
+}
+
 dependencies {
-    implementation(project(":uniffi-android"))
     implementation(platform(kotlin("bom")))
     implementation(platform(libs.coroutines.bom))
     implementation(kotlin("stdlib-jdk7"))
+    implementation("${libs.jna.get()}@aar")
     implementation(libs.appCompat)
     implementation(libs.ktx.core)
     implementation(libs.coroutines.core)
@@ -74,6 +84,56 @@ android {
             proguardFiles(file("proguard-android-optimize.txt"), file("proguard-rules.pro"))
         }
     }
+}
+
+val processedResourcesDir = buildDir.resolve("processedResources")
+
+fun registerCopyJvmBinaryTask(target: String, jniTarget: String, include: String = "*.so"): TaskProvider<Copy> =
+    tasks.register<Copy>("copy-${target}") {
+        group = "uniffi"
+        from(projectDir.resolve("../../../target/${target}/release"))
+        include(include)
+        into(processedResourcesDir.resolve(jniTarget))
+    }
+
+val copyBinariesTasks = listOf(
+    registerCopyJvmBinaryTask("aarch64-linux-android", "arm64-v8a"),
+    registerCopyJvmBinaryTask("armv7-linux-androideabi", "armeabi-v7a"),
+    registerCopyJvmBinaryTask("x86_64-linux-android", "x86_64")
+)
+
+project.afterEvaluate {
+    tasks.getByName("mergeReleaseJniLibFolders") { dependsOn(copyBinariesTasks) }
+    tasks.getByName("mergeDebugJniLibFolders") { dependsOn(copyBinariesTasks) }
+}
+
+tasks.withType<ProcessResources> {
+    dependsOn(copyBinariesTasks)
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    dependsOn(copyBindings)
+}
+
+tasks.withType<Jar> {
+    dependsOn(copyBindings)
+}
+
+tasks.withType<Test> {
+    enabled = false // FIXME: find a way to do this at some point
+    dependsOn(copyBinariesTasks)
+}
+
+kotlin.sourceSets.getByName("main").apply {
+    kotlin.srcDir(generatedDir.resolve("main"))
+}
+
+kotlin.sourceSets.getByName("androidTest").apply {
+    kotlin.srcDir(generatedDir.resolve("test"))
+}
+
+android.sourceSets.getByName("main").apply {
+    jniLibs.srcDir(processedResourcesDir)
 }
 
 // Allows skipping signing jars published to 'MavenLocal' repository
