aab8d6005f98d2684203423bd5a9034590dcb7e8
SimonThormeyer <simon.thormeyer@wire.com>
2025-07-15 12:34:15 +0200

    test(web): error builds correctly when coming from cc or `wasm_bindgen`

--- crypto-ffi/bindings/js/test/wdio/errors.test.ts
+++ crypto-ffi/bindings/js/test/wdio/errors.test.ts
@@ -9,6 +9,7 @@ import {
 } from "./utils";
 import { afterEach, beforeEach, describe } from "mocha";
 import {
+    ConversationId,
     CoreCryptoError,
     type CoreCryptoRichError,
 } from "../../src/CoreCrypto";
@@ -22,7 +23,7 @@ afterEach(async () => {
 });
 
 describe("core crypto errors", () => {
-    it("should build correctly", async () => {
+    it("should build correctly when constructed manually", async () => {
         await ccInit(ALICE_ID);
         const result = await browser.execute(async () => {
             const CoreCryptoError = window.ccModule.CoreCryptoError;
@@ -52,6 +53,94 @@ describe("core crypto errors", () => {
         expect(result.isCorrectInstance).toBe(true);
         expect(result.proteusErrorCodeIsCorrect).toBe(true);
     });
+
+    it("should build correctly when constructed by cc", async () => {
+        await ccInit(ALICE_ID);
+        await createConversation(ALICE_ID, CONV_ID);
+
+        const result = await browser.execute(
+            async (clientName, convId) => {
+                const cc = window.ensureCcDefined(clientName);
+                const conversationId = new window.ccModule.ConversationId(
+                    new TextEncoder().encode(convId)
+                );
+                const CoreCryptoError = window.ccModule.CoreCryptoError;
+
+                try {
+                    await cc.transaction(async (cx) => {
+                        await cx.createConversation(
+                            conversationId,
+                            window.ccModule.CredentialType.Basic
+                        );
+                    });
+                    return {
+                        errorWasThrown: false,
+                    };
+                } catch (err) {
+                    const ccErr = CoreCryptoError.fromStdError(err as Error);
+
+                    const errorSerialized = JSON.stringify(err);
+                    const standardError = new Error(errorSerialized);
+                    const errorDeserialized =
+                        CoreCryptoError.fromStdError(standardError);
+                    return {
+                        errorWasThrown: true,
+                        isCorrectInstance: ccErr instanceof CoreCryptoError,
+                        errorTypeSurvivesSerialization:
+                            errorDeserialized instanceof CoreCryptoError,
+                    };
+                }
+            },
+            ALICE_ID,
+            CONV_ID
+        );
+
+        expect(result.errorWasThrown).toBe(true);
+        expect(result.isCorrectInstance).toBe(true);
+        expect(result.errorTypeSurvivesSerialization).toBe(true);
+    });
+});
+
+it("should build correctly when constructed by wasm bindgen", async () => {
+    await ccInit(ALICE_ID);
+    const result = await browser.execute(
+        async (clientName, convId) => {
+            const cc = window.ensureCcDefined(clientName);
+            const CoreCryptoError = window.ccModule.CoreCryptoError;
+
+            try {
+                await cc.transaction(async (cx) => {
+                    await cx.createConversation(
+                        // pass in a string argument instead of a `ConversationId` instance
+                        convId as unknown as ConversationId,
+                        window.ccModule.CredentialType.Basic
+                    );
+                });
+                return {
+                    errorWasThrown: false,
+                };
+            } catch (err) {
+                const ccErr = CoreCryptoError.fromStdError(err as Error);
+
+                const errorSerialized = JSON.stringify(err);
+                const standardError = new Error(errorSerialized);
+                const errorDeserialized =
+                    CoreCryptoError.fromStdError(standardError);
+                return {
+                    errorWasThrown: true,
+                    isCorrectInstance: ccErr instanceof CoreCryptoError,
+                    errorTypeSurvivesSerialization:
+                        errorDeserialized instanceof CoreCryptoError,
+                };
+            }
+        },
+        ALICE_ID,
+        CONV_ID
+    );
+
+    expect(result.errorWasThrown).toBe(true);
+    expect(result.isCorrectInstance).toBe(true);
+    expect(result.errorTypeSurvivesSerialization).toBe(true);
 });
 
 describe("Error type mapping", () => {
