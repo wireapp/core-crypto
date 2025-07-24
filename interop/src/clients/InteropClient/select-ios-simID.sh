8e1be51a80a9c63525ccdc949afb5fcf8bd0223b
Felix Werner <felix.werner@wire.com>
2025-07-24 15:45:43 +0200

    ci: add script to rerun xbuild with latest iOS version on error
    Xbuild sometimes does not detect the correct latest os version. To
    handle these cases we detect the latest available iphone 16 device and
    set the destination.

---
+++ interop/src/clients/InteropClient/select-ios-simID.sh
@@ -0,0 +1,19 @@
+
+# Get the latest available iOS runtime version
+LATEST_IOS_RUNTIME=$(xcrun simctl list runtimes | \
+  grep 'iOS' | grep -v unavailable | \
+  sed -E 's/.*iOS ([0-9\.]+).*/\1/' | \
+  sort -Vr | head -n 1)
+
+# Extract iPhone 16 Simulator ID for that runtime
+SIMULATOR_ID=$(xcrun simctl list devices available | \
+sed -n '/-- iOS 18.5 --/,/^--/p' | \
+grep -E '^ *iPhone 16 \(' | \
+sed -E 's/^.*\(([A-F0-9-]{36})\).*/\1/')
+
+if [ -z "$SIM_ID" ]; then
+  echo "No iPhone 16 simulator found for iOS $RUNTIME"
+  exit 1
+else
+
+echo $SIMULATOR_ID
