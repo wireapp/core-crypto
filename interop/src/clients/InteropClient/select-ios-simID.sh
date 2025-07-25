0254eed73f15c43e4aa6176b17462cf34c045c89
Felix Werner <felix.werner@wire.com>
2025-07-25 10:43:22 +0200

    added console log

--- interop/src/clients/InteropClient/select-ios-simID.sh
+++ interop/src/clients/InteropClient/select-ios-simID.sh
@@ -1,4 +1,16 @@
 
+
+
+# BUILD_OUTPUT='Ineligible destinations for the "InteropClient" scheme:
+#     { platform:iOS, error:iOS 18.2 is not installed. To use with Xcode, first download and install the platform }'
+
+
+# MISSING_VERSION=$(echo "$BUILD_OUTPUT" | \
+#   grep -oE 'iOS [0-9]+\.[0-9]+ is not installed' | \
+#   sed -E 's/iOS ([0-9]+\.[0-9]+) is not installed/\1/')
+
+echo "Detecting Simulator failed. Trying to find latest Simulator version and rerun."
+
 # Get the latest available iOS runtime version
 LATEST_IOS_RUNTIME=$(xcrun simctl list runtimes | \
   grep 'iOS' | grep -v unavailable | \
