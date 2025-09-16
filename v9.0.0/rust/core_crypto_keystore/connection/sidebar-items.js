d5892e14dd7b2dd113d9cf8fc0a9c8a4edb56357
SimonThormeyer <SimonThormeyer@users.noreply.github.com>
2025-09-16 11:09:47 +0000

    deploy: c25d305b37189cdf0a094f7e96d289e556cd5902

---
+++ v9.0.0/rust/core_crypto_keystore/connection/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"constant":["MAX_BLOB_LEN"],"enum":["ConnectionType"],"mod":["platform"],"struct":["Connection","DatabaseKey"],"trait":["DatabaseConnection","DatabaseConnectionRequirements","FetchFromDatabase"]};
---
+++ v9.0.0/rust/trait.impl/proteus_traits/trait.PreKeyStore.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["core_crypto_keystore",[["impl PreKeyStore for <a class=\"struct\" href=\"core_crypto_keystore/connection/struct.Connection.html\" title=\"struct core_crypto_keystore::connection::Connection\">Connection</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[212]}
