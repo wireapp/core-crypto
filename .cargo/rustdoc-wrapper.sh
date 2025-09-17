a1729dbbf2d498d62bb2509090d63a6709628806
Peter Goodspeed-Niklaus <peter.goodspeed-niklaus@wire.com>
2025-09-17 12:51:12 +0200

    build: adjust rustdoc-wrapper.sh to better strip GFM
    Previously it stripped GFM admonitions written in lowercase.
    However, strictly, they're supposed to be in uppercase, and there
    is a somewhat larger set than were previously supported:
    
    <https://github.com/orgs/community/discussions/16925>
    
    As we now have a markdown format checker in CI, we know that these
    admonitions will be rendered in uppercase, so we had to adapt the
    sed expression. We updated the set of admonitions en passant.

--- .cargo/rustdoc-wrapper.sh
+++ .cargo/rustdoc-wrapper.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 # The README file in repo root contains some github-flavored Markdown constructs
 # that are not understood by rustdoc. Remove them so rustdoc doesn't complain.
-if [ $CARGO_CRATE_NAME = "core_crypto" ]; then
+if [ "$CARGO_CRATE_NAME" = "core_crypto" ]; then
     tempfile=$(mktemp)
+    # we do not want to expand the contained backticks as expressions here;
+    # they are code fences
+    #shellcheck disable=SC2016
     sed -E \
-        -e '/\[!(note|warning|important)\]/d' \
+        -e '/\[!(NOTE|TIP|IMPORTANT|WARNING|CAUTION)\]/d' \
         -e 's/```ignore/```text/g' \
-        $CARGO_MANIFEST_DIR/../README.md > $tempfile
-    export STRIPPED_README_PATH=$tempfile
+        "$CARGO_MANIFEST_DIR/../README.md" > "$tempfile"
+    export STRIPPED_README_PATH="$tempfile"
 fi
 rustdoc "$@"
