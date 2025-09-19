431dc15bed76878bba4b3bbebcd35ed495899f50
SimonThormeyer <simon.thormeyer@wire.com>
2025-09-19 16:47:33 +0200

    feat(ffi): add new `Database` type [WPB-19568]

--- crypto-ffi/src/database/mod.rs
+++ crypto-ffi/src/database/mod.rs
@@ -2,5 +2,26 @@
 
 mod key;
 
+#[cfg(target_family = "wasm")]
+use wasm_bindgen::prelude::*;
+
+use crate::{CoreCryptoError, CoreCryptoResult};
+
 pub(super) use key::*;
 pub use key::{DatabaseKey, migrate_database_key_type_to_bytes, update_database_key};
+
+/// The database acting as a core crypto keystore.
+#[derive(Debug, derive_more::From, derive_more::Into)]
+#[cfg_attr(target_family = "wasm", wasm_bindgen)]
+#[cfg_attr(not(target_family = "wasm"), derive(Clone, derive_more::Deref, uniffi::Object))]
+pub struct Database(core_crypto_keystore::Database);
+
+/// Open or create a [Database].
+#[cfg_attr(not(target_family = "wasm"), uniffi::export)]
+#[cfg_attr(target_family = "wasm", wasm_bindgen(js_name = openDatabase))]
+pub async fn open_database(name: &str, key: DatabaseKeyMaybeArc) -> CoreCryptoResult<Database> {
+    core_crypto_keystore::Database::open(core_crypto_keystore::ConnectionType::Persistent(name), &key.to_cc())
+        .await
+        .map(Database)
+        .map_err(CoreCryptoError::generic())
+}
