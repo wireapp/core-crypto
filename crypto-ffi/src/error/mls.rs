eb2760b425e84752568ac0ef57b8d31b98aea635
SimonThormeyer <simon.thormeyer@wire.com>
2025-09-23 17:57:45 +0200

    feat(wasm)!: structural wasm errors

--- crypto-ffi/src/error/mls.rs
+++ crypto-ffi/src/error/mls.rs
@@ -1,4 +1,12 @@
+#[cfg(target_family = "wasm")]
+use js_sys::Object;
+#[cfg(target_family = "wasm")]
+use wasm_bindgen::JsValue;
+
 use core_crypto::InnermostErrorMessage as _;
+
+#[cfg(target_family = "wasm")]
+use super::wasm::{JsErrorContext, JsValueMutationExt as _};
 
 /// MLS produces these kinds of error
 #[derive(Debug, thiserror::Error)]
@@ -56,5 +64,42 @@ impl From<core_crypto::MlsError> for MlsError {
         Self::Other {
             msg: e.innermost_error_message(),
         }
+    }
+}
+
+#[cfg(target_family = "wasm")]
+impl JsErrorContext for MlsError {
+    fn get_context(&self) -> JsValue {
+        let context = Object::new();
+        let inner_context = Object::new();
+        match &self {
+            e @ MlsError::ConversationAlreadyExists { conversation_id } => {
+                context.set_field("type", e.as_ref());
+                inner_context.set_field(
+                    "conversationId",
+                    serde_wasm_bindgen::to_value(conversation_id).expect("constructing json array"),
+                );
+            }
+            e @ (MlsError::DuplicateMessage
+            | MlsError::BufferedFutureMessage
+            | MlsError::WrongEpoch
+            | MlsError::BufferedCommit
+            | MlsError::MessageEpochTooOld
+            | MlsError::SelfCommitIgnored
+            | MlsError::UnmergedPendingGroup
+            | MlsError::StaleProposal
+            | MlsError::StaleCommit
+            | MlsError::OrphanWelcome) => context.set_field("type", e.as_ref()),
+            e @ MlsError::MessageRejected { reason } => {
+                context.set_field("type", e.as_ref());
+                inner_context.set_field("reason", reason);
+            }
+            e @ MlsError::Other { msg } => {
+                context.set_field("type", e.as_ref());
+                inner_context.set_field("msg", msg);
+            }
+        }
+        context.set_field("context", inner_context);
+        context.into()
     }
 }
