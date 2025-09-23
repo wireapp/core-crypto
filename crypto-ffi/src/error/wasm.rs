ddfa47c4135487f1c555a51ed61a3ce907843690
SimonThormeyer <simon.thormeyer@wire.com>
2025-09-23 17:57:45 +0200

    feat(wasm)!: structural wasm errors

--- crypto-ffi/src/error/wasm.rs
+++ crypto-ffi/src/error/wasm.rs
@@ -1,6 +1,9 @@
 //! Uniffi uses `CoreCryptoError` directly. Wasm needs to wrap it in order to get proper error handling.
 //! This module contains that Wasm special handling.
 
+use js_sys::{Object, Reflect};
+use wasm_bindgen::JsValue;
+
 type InternalError = super::core_crypto::CoreCryptoError;
 
 #[derive(Debug, thiserror::Error)]
@@ -8,18 +11,18 @@ pub struct CoreCryptoError(#[source] InternalError);
 
 impl std::fmt::Display for CoreCryptoError {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        let proteus_error_code = match &self.0 {
-            InternalError::Proteus {
-                exception: crate::ProteusError::Other { error_code },
-            } => Some(*error_code),
-            _ => None,
-        };
+        let (error_type, error_context) = self.get_type_and_context();
+        let context_string = js_sys::JSON::stringify(&error_context)
+            .expect("serializing error context")
+            .as_string()
+            .expect("parsing js string into rust string");
 
         let json = serde_json::to_string(&serde_json::json!({
             "message": self.0.to_string(),
             "error_name": self.0.variant_name(),
             "error_stack": self.0.stack(),
-            "proteus_error_code": proteus_error_code,
+            "type": error_type.to_string(),
+            "context": serde_json::from_str::<serde_json::Value>(&context_string).expect("parsing json string")
         }))
         .map_err(|_| std::fmt::Error)?;
 
@@ -49,10 +52,31 @@ impl From<CoreCryptoError> for wasm_bindgen::JsValue {
 
         let stacked_error = construct_error_stack(&val);
         stacked_error.set_name(&val.0.variant_name());
+        let (error_type, error_context) = val.get_type_and_context();
+        stacked_error.set_field("type", error_type);
+        stacked_error.set_field("context", &error_context);
 
         stacked_error.into()
     }
 }
+
+pub(super) trait JsErrorContext {
+    fn get_context(&self) -> wasm_bindgen::JsValue;
+}
+
+pub(super) trait JsValueMutationExt: AsRef<JsValue> + From<JsValue> + wasm_bindgen::JsCast {
+    fn new_with_property(property_key: &str, value: impl Into<JsValue>) -> Self {
+        let target = Object::new().into();
+        Reflect::set(&target, &property_key.into(), &value.into()).expect("mutating newly created js value");
+        target.into()
+    }
+
+    fn set_field(&self, property: &str, value: impl Into<JsValue>) {
+        Reflect::set(self.as_ref(), &property.into(), &value.into()).expect("mutating js value");
+    }
+}
+
+impl<T> JsValueMutationExt for T where T: AsRef<JsValue> + From<JsValue> + wasm_bindgen::JsCast {}
 
 impl CoreCryptoError {
     pub(crate) fn generic<E>() -> impl FnOnce(E) -> Self
@@ -64,5 +88,23 @@ impl CoreCryptoError {
 
     pub(crate) fn ad_hoc(err: impl ToString) -> Self {
         Self(InternalError::ad_hoc(err))
+    }
+
+    fn get_type_and_context(&self) -> (&str, wasm_bindgen::JsValue) {
+        match &self.0 {
+            e @ InternalError::Mls { mls_error } => (e.as_ref(), mls_error.get_context()),
+            e @ InternalError::Proteus {
+                exception: proteus_error,
+            } => (e.as_ref(), proteus_error.get_context()),
+            e @ InternalError::E2ei { e2ei_error } => {
+                (e.as_ref(), JsValue::new_with_property("e2ei_error", e2ei_error))
+            }
+
+            e @ InternalError::SerializationError(_) => (e.as_ref(), JsValue::new_with_property("msg", e.to_string())),
+
+            e @ InternalError::UnknownCiphersuite => (e.as_ref(), JsValue::new_with_property("msg", e.to_string())),
+            e @ InternalError::TransactionFailed { error } => (e.as_ref(), JsValue::new_with_property("error", error)),
+            e @ InternalError::Other { msg } => (e.as_ref(), JsValue::new_with_property("msg", msg)),
+        }
     }
 }
