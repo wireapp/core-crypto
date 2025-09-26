47d3a2f5a6cd1c3b079ccf8e3483957fd52d653a
SimonThormeyer <simon.thormeyer@wire.com>
2025-09-26 09:59:31 +0200

    chore: fix typo

--- crypto-ffi/src/error/mls.rs
+++ crypto-ffi/src/error/mls.rs
@@ -44,7 +44,7 @@ pub enum MlsError {
     StaleProposal,
     #[error("The received commit is deemed stale and is from an older epoch.")]
     StaleCommit,
-    /// This happens when the DS cannot flag KeyPackages as claimed or not. It this scenario, a client
+    /// This happens when the DS cannot flag KeyPackages as claimed or not. In this scenario, a client
     /// requests their old KeyPackages to be deleted but one has already been claimed by another client to create a Welcome.
     /// In that case the only solution is that the client receiving such a Welcome tries to join the group
     /// with an External Commit instead
