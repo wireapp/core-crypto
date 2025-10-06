356e3fcc991d1e4336f495202a6839f591de46d4
Felix Werner <felix.werner@wire.com>
2025-10-06 17:21:04 +0200

    feat: ffi: ts: adjust ts wrapper to uniffi

--- crypto-ffi/bindings/js/test/wdio/utils.ts
+++ crypto-ffi/bindings/js/test/wdio/utils.ts
@@ -165,7 +165,7 @@ export async function ccInit(
             const cipherSuite = window.defaultCipherSuite;
             const encoder = new TextEncoder();
             const clientId = new window.ccModule.ClientId(
-                encoder.encode(clientName)
+                encoder.encode(clientName).buffer
             );
 
             const key = new Uint8Array(32);
@@ -173,7 +173,7 @@ export async function ccInit(
 
             const database = await window.ccModule.openDatabase(
                 clientName,
-                new window.ccModule.DatabaseKey(key)
+                new window.ccModule.DatabaseKey(key.buffer)
             );
 
             const instance = await window.ccModule.CoreCrypto.init(database);
