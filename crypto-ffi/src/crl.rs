817c6d79e2b6c1318dc42bd9f40c66350038288e
Felix Werner <felix.werner@wire.com>
2025-10-07 16:36:50 +0200

    feat: ffi: remove wasm_bindgen from crl

--- crypto-ffi/src/crl.rs
+++ crypto-ffi/src/crl.rs
@@ -1,11 +1,6 @@
-#[cfg(target_family = "wasm")]
-use wasm_bindgen::prelude::*;
-
 pub(crate) type NewCrlDistributionPoints = Option<Vec<String>>;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[cfg_attr(target_family = "wasm", wasm_bindgen, derive(serde::Serialize, serde::Deserialize))]
-#[cfg_attr(not(target_family = "wasm"), derive(uniffi::Record))]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, uniffi::Record)]
 /// Supporting struct for CRL registration result
 pub struct CrlRegistration {
     /// Whether this CRL modifies the old CRL (i.e. has a different revocated cert list)
@@ -20,15 +15,5 @@ impl From<core_crypto::e2e_identity::CrlRegistration> for CrlRegistration {
             dirty: value.dirty,
             expiration: value.expiration,
         }
-    }
-}
-
-#[cfg(target_family = "wasm")]
-#[wasm_bindgen]
-impl CrlRegistration {
-    /// Contstruct a CRL registration from its fields
-    #[wasm_bindgen(constructor)]
-    pub fn new(dirty: bool, expiration: Option<u64>) -> Self {
-        Self { dirty, expiration }
     }
 }
