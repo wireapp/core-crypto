1fecf11c46a1fdc192e69b230a7638a5d8468d1b
Felix Werner <felix.werner@wire.com>
2025-10-07 09:17:46 +0200

    feat: ffi: remove wasm from history client

--- crypto-ffi/src/ephemeral.rs
+++ crypto-ffi/src/ephemeral.rs
@@ -1,34 +1,15 @@
 use core_crypto::{CoreCrypto, HistorySecret as CoreCryptoHistorySecret};
-#[cfg(target_family = "wasm")]
-use wasm_bindgen::prelude::*;
 
 use crate::{ClientId, CoreCryptoError, CoreCryptoFfi, CoreCryptoResult, client_id::ClientIdMaybeArc};
 
 /// A `HistorySecret` encodes sufficient client state that it can be used to instantiate an
 /// ephemeral client.
-#[derive(core_crypto_macros::Debug, Clone, PartialEq, Eq)]
-#[cfg_attr(target_family = "wasm", wasm_bindgen(getter_with_clone))]
-#[cfg_attr(not(target_family = "wasm"), derive(uniffi::Record))]
+#[derive(core_crypto_macros::Debug, Clone, PartialEq, Eq, uniffi::Record)]
 pub struct HistorySecret {
     /// Client id of the associated history client
-    #[cfg_attr(target_family = "wasm", wasm_bindgen(readonly, js_name = clientId))]
     pub client_id: ClientIdMaybeArc,
     /// Opaque secret data sufficient to reconstruct a history client.
-    #[cfg_attr(target_family = "wasm", wasm_bindgen(readonly))]
     pub data: Vec<u8>,
-}
-
-#[cfg(target_family = "wasm")]
-#[wasm_bindgen]
-impl HistorySecret {
-    /// Construct a history secret from client id and raw data
-    #[wasm_bindgen(constructor)]
-    pub fn new(client_id: ClientIdMaybeArc, data: &[u8]) -> Result<HistorySecret, wasm_bindgen::JsError> {
-        Ok(HistorySecret {
-            client_id,
-            data: data.into(),
-        })
-    }
 }
 
 impl TryFrom<&CoreCryptoHistorySecret> for HistorySecret {
