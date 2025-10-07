a2aa809b21d553695cc237e0eaeacf3502410dc8
Peter Goodspeed-Niklaus <peter.goodspeed-niklaus@wire.com>
2025-10-07 17:44:21 +0200

    feat: add the ability to search for credentials per the current impl
    The bit of our current implementation which handles finding credentials
    is split across two different functions in `Session`; it's hard to follow.
    But I am fairly confident that this replicates the current logic.
    
    The bit where keypairs get distributed across signing schemes and saved
    credentials seems very weird to me. I'd like to refactor that bit of
    the database at some point and make it make more sense.

---
+++ crypto/src/mls/credential/keypairs.rs
@@ -0,0 +1,79 @@
+use core_crypto_keystore::{
+    connection::FetchFromDatabase as _,
+    entities::{EntityFindParams, StoredSignatureKeypair},
+};
+use mls_crypto_provider::Database;
+use openmls::prelude::{OpenMlsCrypto, SignatureScheme};
+use openmls_basic_credential::SignatureKeyPair;
+use tls_codec::{Deserialize as _, Serialize as _};
+
+use super::{Error, Result};
+use crate::{ClientId, KeystoreError, MlsError};
+
+/// Load all stored keypairs from the keystore
+pub(super) async fn load_all(database: &Database) -> Result<Vec<StoredSignatureKeypair>> {
+    database
+        .find_all::<StoredSignatureKeypair>(EntityFindParams::default())
+        .await
+        .map_err(KeystoreError::wrap("finding all mls signature keypairs"))
+        .map_err(Into::into)
+}
+
+/// Generate a new keypair in-memory with the specificed signature scheme
+pub(super) fn generate(crypto: impl OpenMlsCrypto, signature_scheme: SignatureScheme) -> Result<SignatureKeyPair> {
+    let (private_key, public_key) = crypto
+        .signature_key_gen(signature_scheme)
+        .map_err(MlsError::wrap("generating signature key"))?;
+    Ok(SignatureKeyPair::from_raw(signature_scheme, private_key, public_key))
+}
+
+/// Store a keypair in the keystore, attached to a particular client id
+pub(super) async fn store(database: &Database, id: &ClientId, keypair: &SignatureKeyPair) -> Result<()> {
+    let data = keypair
+        .tls_serialize_detached()
+        .map_err(Error::tls_serialize("keypair"))?;
+
+    debug_assert!(
+        {
+            let deserialized =
+                SignatureKeyPair::tls_deserialize_exact(&data).expect("keypair deserializes without error");
+            deserialized.signature_scheme() == keypair.signature_scheme()
+                && deserialized.public() == keypair.public()
+                && deserialized.private() == keypair.private()
+        },
+        "serialized keypair data must deserialize correctly"
+    );
+
+    let store_keypair = StoredSignatureKeypair::new(
+        keypair.signature_scheme(),
+        keypair.public().to_owned(),
+        data,
+        id.as_slice().into(),
+    );
+    database
+        .save(store_keypair)
+        .await
+        .map_err(KeystoreError::wrap("storing keypairs in keystore"))?;
+
+    Ok(())
+}
+
+/// Deserialize a [`StoredSignatureKeypair`] into a [`SignatureKeyPair`]
+pub(super) fn deserialize(stored: &StoredSignatureKeypair) -> Result<SignatureKeyPair> {
+    SignatureKeyPair::tls_deserialize_exact(&stored.keypair)
+        .map_err(KeystoreError::wrap("deserializing keypair from keystore"))
+        .map_err(Into::into)
+}
+
+/// Retrieve the first keypair from the list which matches the provided signature scheme and client id
+pub(super) async fn find_matching(
+    keypairs: &[StoredSignatureKeypair],
+    client_id: impl AsRef<[u8]>,
+    signature_scheme: SignatureScheme,
+) -> Result<Option<SignatureKeyPair>> {
+    keypairs
+        .iter()
+        .find(|stored| stored.credential_id == client_id.as_ref() && stored.signature_scheme == signature_scheme as u16)
+        .map(deserialize)
+        .transpose()
+}
