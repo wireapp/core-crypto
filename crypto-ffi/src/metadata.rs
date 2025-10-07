839a246a2a481558d64178e699a95c92c5af21fc
Felix Werner <felix.werner@wire.com>
2025-10-07 16:36:29 +0200

    feat: ffi: remove wasm_bindgen from metadata

--- crypto-ffi/src/metadata.rs
+++ crypto-ffi/src/metadata.rs
@@ -1,11 +1,7 @@
-#[cfg(target_family = "wasm")]
-use wasm_bindgen::prelude::*;
-
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 /// The version of `core-crypto`.
-#[cfg_attr(not(target_family = "wasm"), uniffi::export)]
-#[cfg_attr(target_family = "wasm", wasm_bindgen)]
+#[uniffi::export]
 pub fn version() -> String {
     VERSION.to_string()
 }
@@ -15,7 +11,6 @@ pub fn version() -> String {
 // the `wasm_bindgen` attribute on the struct members is not compatible
 // with the `#[cfg_attr]` attribute.
 
-#[cfg(not(target_family = "wasm"))]
 #[derive(uniffi::Record)]
 /// Metadata describing the conditions of the build of this software.
 pub struct BuildMetadata {
@@ -39,74 +34,18 @@ pub struct BuildMetadata {
     pub git_dirty: String,
 }
 
-/// Metadata describing the conditions of the build of this software.
-#[cfg(target_family = "wasm")]
-#[wasm_bindgen(inspectable)]
-pub struct BuildMetadata {
-    /// Build Timestamp
-    #[wasm_bindgen(readonly)]
-    pub timestamp: &'static str,
-    /// Whether this build was in Debug mode (true) or Release mode (false)
-    #[wasm_bindgen(readonly, js_name = "cargoDebug")]
-    pub cargo_debug: &'static str,
-    /// Features enabled for this build
-    #[wasm_bindgen(readonly, js_name = "cargoFeatures")]
-    pub cargo_features: &'static str,
-    /// Optimization level
-    #[wasm_bindgen(readonly, js_name = "optLevel")]
-    pub opt_level: &'static str,
-    /// Build target triple
-    #[wasm_bindgen(readonly, js_name = "targetTriple")]
-    pub target_triple: &'static str,
-    /// Git branch
-    #[wasm_bindgen(readonly, js_name = "gitBranch")]
-    pub git_branch: &'static str,
-    /// Output of `git describe`
-    #[wasm_bindgen(readonly, js_name = "gitDescribe")]
-    pub git_describe: &'static str,
-    /// Hash of current git commit
-    #[wasm_bindgen(readonly, js_name = "gitSha")]
-    pub git_sha: &'static str,
-    /// `true` when the source code differed from the commit at the most recent git hash
-    #[wasm_bindgen(readonly, js_name = "gitDirty")]
-    pub git_dirty: &'static str,
-}
-
 /// Returns build data for CoreCrypto
-#[cfg_attr(not(target_family = "wasm"), uniffi::export)]
-#[cfg_attr(target_family = "wasm", wasm_bindgen)]
+#[uniffi::export]
 pub fn build_metadata() -> BuildMetadata {
     BuildMetadata {
-        #[cfg_attr(target_family = "wasm", expect(clippy::useless_conversion))]
         timestamp: core_crypto::BUILD_METADATA.timestamp.into(),
-        #[cfg_attr(target_family = "wasm", expect(clippy::useless_conversion))]
         cargo_debug: core_crypto::BUILD_METADATA.cargo_debug.into(),
-        #[cfg_attr(target_family = "wasm", expect(clippy::useless_conversion))]
         cargo_features: core_crypto::BUILD_METADATA.cargo_features.into(),
-        #[cfg_attr(target_family = "wasm", expect(clippy::useless_conversion))]
         opt_level: core_crypto::BUILD_METADATA.opt_level.into(),
-        #[cfg_attr(target_family = "wasm", expect(clippy::useless_conversion))]
         target_triple: core_crypto::BUILD_METADATA.target_triple.into(),
-        #[cfg_attr(target_family = "wasm", expect(clippy::useless_conversion))]
         git_branch: core_crypto::BUILD_METADATA.git_branch.into(),
-        #[cfg_attr(target_family = "wasm", expect(clippy::useless_conversion))]
         git_describe: core_crypto::BUILD_METADATA.git_describe.into(),
-        #[cfg_attr(target_family = "wasm", expect(clippy::useless_conversion))]
         git_sha: core_crypto::BUILD_METADATA.git_sha.into(),
-        #[cfg_attr(target_family = "wasm", expect(clippy::useless_conversion))]
         git_dirty: core_crypto::BUILD_METADATA.git_dirty.into(),
-    }
-}
-
-#[cfg(target_family = "wasm")]
-impl crate::CoreCryptoFfi {
-    /// Returns the current version of CoreCrypto
-    pub fn version() -> String {
-        version()
-    }
-
-    /// Returns build data for CoreCrypto
-    pub fn build_metadata() -> BuildMetadata {
-        build_metadata()
     }
 }
