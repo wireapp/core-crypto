674c191901c035dcb70b59a092bea601542a3713
Felix Werner <felix.werner@wire.com>
2025-10-08 10:47:27 +0200

    build: ffi: add "wasm-unstable-single-threaded" feature
    mozilla.github.io/uniffi-rs/latest/wasm/configuration.html

--- crypto-ffi/src/core_crypto/epoch_observer.rs
+++ crypto-ffi/src/core_crypto/epoch_observer.rs
@@ -15,7 +15,8 @@ pub enum EpochChangedReportingError {
 
 /// An `EpochObserver` is notified whenever a conversation's epoch changes.
 #[uniffi::export(with_foreign)]
-#[async_trait]
+#[cfg_attr(target_family = "wasm", async_trait(?Send))]
+#[cfg_attr(not(target_family = "wasm"), async_trait)]
 pub trait EpochObserver: Send + Sync {
     /// This function will be called every time a conversation's epoch changes.l    ///
     /// The `epoch` parameter is the new epoch.
@@ -44,8 +45,8 @@ pub trait EpochObserver: Send + Sync {
 /// together directly, so this is the straightforward way to accomplish that.
 struct ObserverShim(Arc<dyn EpochObserver>);
 
-#[cfg_attr(target_family = "wasm", async_trait(?Send))]
-#[cfg_attr(not(target_family = "wasm"), async_trait)]
+#[cfg_attr(target_family = "wasm", async_trait::async_trait(?Send))]
+#[cfg_attr(not(target_family = "wasm"), async_trait::async_trait)]
 impl core_crypto::mls::EpochObserver for ObserverShim {
     async fn epoch_changed(&self, conversation_id: ConversationId, epoch: u64) {
         if let Err(err) = self
