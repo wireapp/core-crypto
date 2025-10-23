b9f20601bff211b63166f1b77f16dde02fb43474
Peter Goodspeed-Niklaus <peter.goodspeed-niklaus@wire.com>
2025-10-23 14:30:55 +0200

    chore: explicitly save the created credential in `e2ei_mls_init_only`
    E2ei is hideously complicated and at least some of that complication
    is because credentials used to be _very_ finicky. I'm intentionally
    trying not to mess with any of this more than I have to.
    
    This is a case where I had to. IDK how the credential used to be
    persisted, but these days, once there is a credential it is easy enough
    to explicitly persist it into the keystore.

--- crypto/src/transaction_context/e2e_identity/mod.rs
+++ crypto/src/transaction_context/e2e_identity/mod.rs
@@ -15,7 +15,7 @@ use wire_e2e_identity::prelude::x509::extract_crl_uris;
 
 use super::TransactionContext;
 use crate::{
-    CertificateBundle, Ciphersuite, ClientId, ClientIdentifier, E2eiEnrollment, RecursiveError,
+    CertificateBundle, Ciphersuite, ClientId, ClientIdentifier, Credential, E2eiEnrollment, RecursiveError,
     e2e_identity::NewCrlDistributionPoints,
     mls::credential::{crl::get_new_crl_distribution_points, x509::CertificatePrivateKey},
 };
@@ -64,6 +64,11 @@ impl TransactionContext {
         enrollment: &mut E2eiEnrollment,
         certificate_chain: String,
     ) -> Result<NewCrlDistributionPoints> {
+        let mls_provider = self
+            .mls_provider()
+            .await
+            .map_err(RecursiveError::transaction("getting mls provider"))?;
+
         let sk = enrollment
             .get_sign_key_for_mls()
             .map_err(RecursiveError::e2e_identity("creating new enrollment"))?;
@@ -71,9 +76,7 @@ impl TransactionContext {
         let certificate_chain = enrollment
             .certificate_response(
                 certificate_chain,
-                self.mls_provider()
-                    .await
-                    .map_err(RecursiveError::transaction("getting mls provider"))?
+                mls_provider
                     .authentication_service()
                     .borrow()
                     .await
@@ -94,6 +97,18 @@ impl TransactionContext {
             certificate_chain,
             private_key,
         };
+
+        let mut credential = Credential::x509(cert_bundle.clone()).map_err(RecursiveError::mls_credential(
+            "creating credential from certificate bundle in e2ei_mls_init_only",
+        ))?;
+        // we don't need to keep the credential ref; this credential will be loaded in mls_init later on
+        credential
+            .save(&mls_provider.keystore())
+            .await
+            .map_err(RecursiveError::mls_credential(
+                "saving credential in e2ei_mls_init_only",
+            ))?;
+
         let identifier = ClientIdentifier::X509(HashMap::from([(cs.signature_algorithm(), cert_bundle)]));
         self.mls_init(identifier, &[cs])
             .await
