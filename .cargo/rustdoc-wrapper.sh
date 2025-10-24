01593f5e4aed24dca49f8bf8464ec866794751cd
Felix Werner <felix.werner@wire.com>
2025-10-24 14:13:31 +0200

    chore: replace sed with perl

--- .cargo/rustdoc-wrapper.sh
+++ .cargo/rustdoc-wrapper.sh
@@ -6,10 +6,10 @@ if [ "$CARGO_CRATE_NAME" = "core_crypto" ]; then
     # we do not want to expand the contained backticks as expressions here;
     # they are code fences
     #shellcheck disable=SC2016
-    sed -E \
-        -e '/\[!(NOTE|TIP|IMPORTANT|WARNING|CAUTION)\]/d' \
-        -e 's/```ignore/```text/g' \
-        "$CARGO_MANIFEST_DIR/../README.md" > "$tempfile"
+     perl -ne '
+         s/```ignore/```text/g;
+         print unless /\[!(NOTE|TIP|IMPORTANT|WARNING|CAUTION)\]/
+     ' "$CARGO_MANIFEST_DIR/../README.md" > "$tempfile"
     export STRIPPED_README_PATH="$tempfile"
 fi
 rustdoc "$@"
