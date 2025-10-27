f2f04c9667237885ec2c1a4389f4b930cf4191eb
Peter Goodspeed-Niklaus <peter.goodspeed-niklaus@wire.com>
2025-10-27 09:59:18 +0100

    chore: cause doc test to build

--- crypto/src/mls/credential/credential_ref/find.rs
+++ crypto/src/mls/credential/credential_ref/find.rs
@@ -16,19 +16,21 @@ use crate::{ClientId, Credential, CredentialRef, KeystoreError, RecursiveError, 
 /// ## Example
 ///
 /// ```no_run
-/// # use crypto::src::mls::{Credential, FindFilters};
+/// # use core_crypto::{CredentialRef, CredentialFindFilters, ClientIdRef};
 /// # use openmls::prelude::CredentialType;
 /// # let database = todo!();
-/// # let client_id = todo!();
+/// # let client_id = ClientIdRef::new(&[]);
+/// # smol::block_on(async {
 /// // get all basic credentials for a client
-/// let credentials = Credential::find(
+/// let credentials = CredentialRef::find(
 ///     &database,
-///     FindFilters::builder()
+///     CredentialFindFilters::builder()
 ///         .client_id(&client_id)
 ///         .credential_type(CredentialType::Basic)
 ///         .build()
-/// )?;
+/// ).await.unwrap();
 /// # println!("{credentials:?}");
+/// # });
 /// ```
 #[derive(Debug, Default, typed_builder::TypedBuilder)]
 pub struct FindFilters<'a> {
