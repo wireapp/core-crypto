050fae8d27687a73548b6971efb428e80fd38bd8
Peter Goodspeed-Niklaus <peter.goodspeed-niklaus@wire.com>
2025-10-28 12:46:24 +0100

    test(js): add test that credential can be saved

--- crypto-ffi/bindings/js/test/bun/credential.test.ts
+++ crypto-ffi/bindings/js/test/bun/credential.test.ts
@@ -1,4 +1,4 @@
-import { setup, teardown } from "./utils";
+import { ccInit, setup, teardown } from "./utils";
 import { afterEach, test, beforeEach, describe, expect } from "bun:test";
 import {
     ciphersuiteDefault,
@@ -24,4 +24,24 @@ describe("credentials", () => {
         expect(credential.type()).toEqual(CredentialType.Basic);
         expect(credential.earliest_validity()).toEqual(0n);
     });
+
+    test("credential can be saved", async () => {
+        const credential = Credential.basic(
+            ciphersuiteDefault(),
+            new ClientId(Buffer.from("any random client id here"))
+        );
+
+        // create a CC instance and init so that we get access to the transaction interface
+        // only necessary until WPB-21396
+        let { cc, db } = await ccInit("alice");
+
+        const ref = await cc.transaction(async (_ctx) => {
+            return await credential.save(db);
+        });
+
+        expect(ref).toBeDefined();
+        expect(ref.type()).toEqual(CredentialType.Basic);
+        // saving causes the earliest validity to be updated
+        expect(ref.earliest_validity()).not.toEqual(0n);
+    })
 });
