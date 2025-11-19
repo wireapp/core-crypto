b0559ee0d5e8f8404cdb68797d0d9907b1c31c0a
Jacob Persson <7156+typfel@users.noreply.github.com>
2025-11-19 13:20:19 +0100

    chore: add scaffolding for android interop project

--- crypto-ffi/bindings/build.gradle.kts
+++ crypto-ffi/bindings/build.gradle.kts
@@ -12,6 +12,7 @@ buildscript {
     }
 }
 
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id(libs.plugins.vanniktech.publish.get().pluginId) version libs.versions.vanniktech.publish
     id(libs.plugins.dokka.get().pluginId) version libs.versions.dokka
---
+++ interop/src/clients/android-interop/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "com.wire.androidinterop"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.wire.androidinterop"
+        minSdk = 30
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    implementation("core-crypto-kotlin:android")
+    implementation(libs.ktxSerialization)
+    implementation(libs.androidx.activity.compose)
+}
