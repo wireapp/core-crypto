7c94503d93534f213e7575234c5df9c0205dbdf6
Peter Goodspeed-Niklaus <peter.goodspeed-niklaus@wire.com>
2025-11-24 11:11:30 +0100

    feat: add utility methods to `KeyPackageExt`
    These methods will be particularly useful later on, when we want to
    implement the trait on a fatter ref type.

--- crypto/src/mls/key_package.rs
+++ crypto/src/mls/key_package.rs
@@ -10,7 +10,7 @@ use std::sync::LazyLock;
 use mls_crypto_provider::RustCrypto;
 use openmls::prelude::{KeyPackage, KeyPackageRef};
 
-use crate::MlsError;
+use crate::{Ciphersuite, CredentialType, MlsError};
 
 static CRYPTO: LazyLock<RustCrypto> = LazyLock::new(RustCrypto::default);
 
@@ -18,11 +18,40 @@ static CRYPTO: LazyLock<RustCrypto> = LazyLock::new(RustCrypto::default);
 pub trait KeyPackageExt {
     /// Delegates to [`KeyPackage::hash_ref`] with a static crypto instance.
     fn make_ref(&self) -> Result<KeyPackageRef, MlsError>;
+
+    /// Returns the ciphersuite associated this this key package.
+    fn ciphersuite(&self) -> Ciphersuite;
+
+    /// Returns the credential type associated with this key package.
+    fn credential_type(&self) -> CredentialType;
+
+    /// Determines whether a keypackage is valid in the sense of the former `client_valid_keypackages_count` method.
+    ///
+    /// In practice, this just checks whether its lifetime (if present) has expired or not.
+    fn is_valid(&self) -> bool;
 }
 
 impl KeyPackageExt for KeyPackage {
     fn make_ref(&self) -> Result<KeyPackageRef, MlsError> {
         self.hash_ref(&*CRYPTO)
             .map_err(MlsError::wrap("computing keypackage hash ref"))
+    }
+
+    fn ciphersuite(&self) -> Ciphersuite {
+        <KeyPackage>::ciphersuite(&self).into()
+    }
+
+    fn credential_type(&self) -> CredentialType {
+        self.leaf_node()
+            .credential()
+            .credential_type()
+            .try_into()
+            .expect("we should only ever have a key package from a credential that this instance of CC understands")
+    }
+
+    fn is_valid(&self) -> bool {
+        self.leaf_node()
+            .life_time()
+            .is_none_or(|lifetime| lifetime.has_acceptable_range() && lifetime.is_valid())
     }
 }
