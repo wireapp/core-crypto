04c1c964d7e635bf1d0bb9cdcc69920eddc276ac
Felix Werner <felix.werner@wire.com>
2025-11-26 14:56:05 +0100

    feat(ffi): add PkiEnvironmentHooks

---
+++ crypto-ffi/src/pki_environment.rs
@@ -0,0 +1,111 @@
+use std::{fmt, sync::Arc};
+
+use crate::{CoreCryptoFfi, CoreCryptoResult, Database};
+
+/// HttpMethod used for pki hooks
+#[derive(uniffi::Enum)]
+pub enum HttpMethod {
+    /// GET
+    Get,
+    /// POST
+    Post,
+    /// PUT
+    Put,
+    /// DELETE
+    Delete,
+    /// PATCH
+    Patch,
+    /// HEAD
+    Head,
+}
+
+impl From<core_crypto::e2e_identity::pki_env::HttpMethod> for HttpMethod {
+    fn from(inner: core_crypto::e2e_identity::pki_env::HttpMethod) -> Self {
+        match inner {
+            core_crypto::e2e_identity::pki_env::HttpMethod::Get => Self::Get,
+            core_crypto::e2e_identity::pki_env::HttpMethod::Post => Self::Post,
+            core_crypto::e2e_identity::pki_env::HttpMethod::Put => Self::Put,
+            core_crypto::e2e_identity::pki_env::HttpMethod::Delete => Self::Delete,
+            core_crypto::e2e_identity::pki_env::HttpMethod::Patch => Self::Patch,
+            core_crypto::e2e_identity::pki_env::HttpMethod::Head => Self::Head,
+        }
+    }
+}
+
+/// An HttpHeader used for pki hooks
+#[derive(uniffi::Record)]
+pub struct HttpHeader {
+    /// header name
+    pub name: String,
+    /// header value
+    pub value: String,
+}
+
+impl From<core_crypto::e2e_identity::pki_env::HttpHeader> for HttpHeader {
+    fn from(inner: core_crypto::e2e_identity::pki_env::HttpHeader) -> Self {
+        Self {
+            name: inner.name,
+            value: inner.value,
+        }
+    }
+}
+
+impl From<HttpHeader> for core_crypto::e2e_identity::pki_env::HttpHeader {
+    fn from(ffi: HttpHeader) -> Self {
+        Self {
+            name: ffi.name,
+            value: ffi.value,
+        }
+    }
+}
+
+/// An HttpResponse used for pki hooks
+#[derive(uniffi::Record)]
+pub struct HttpResponse {
+    /// http status code
+    pub status: u16,
+    /// List of header fields
+    pub headers: Vec<HttpHeader>,
+    /// http body
+    pub body: Vec<u8>,
+}
+
+}
+
+/// An OAuthResponse used for pki hooks
+#[derive(uniffi::Record)]
+pub struct OAuthResponse {
+    /// OAuth Access Token
+    pub access_token: String,
+    /// OAuth Id Token
+    pub id_token: Option<String>,
+    /// The Token Type
+    pub token_type: Option<String>,
+    /// Expiration
+    pub expires_in: Option<u64>,
+    /// OAuth Scope
+    pub scope: Option<String>,
+    /// OAuth Refresh Token
+    pub refresh_token: Option<String>,
+}
+
+/// Callbacks that CoreCrypto uses during E2e flow.
+#[uniffi::export(with_foreign)]
+#[cfg_attr(target_family = "wasm", async_trait::async_trait(?Send))]
+#[cfg_attr(not(target_family = "wasm"), async_trait::async_trait)]
+pub trait PkiEnvironmentHooks: Send + Sync {
+    /// Used for making HTTP requests to ACME servers, CRL distributors etc.
+    async fn http_request(
+        &self,
+        method: HttpMethod,
+        url: String,
+        headers: Vec<HttpHeader>,
+        body: Vec<u8>,
+    ) -> HttpResponse;
+
+    /// only used to authenticate with the user's identity provider
+    async fn authenticate(&self, idp: String, key_auth: String, acme_aud: String) -> OAuthResponse;
+
+    /// this one is only used for DPoP challenge
+    async fn fetch_backend_access_token(&self, dpop: String) -> String;
+}
