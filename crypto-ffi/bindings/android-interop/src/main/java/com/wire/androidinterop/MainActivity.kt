d9cc2914bc4b751b49f9b1702174c05cf8b9daeb
Jacob Persson <7156+typfel@users.noreply.github.com>
2025-11-27 16:41:00 +0100

    feat: add android interop application
    The application follows the same structure as the iOS interop application. Interop commands
    are sent via intents and the result is printed to stdout / logcat.

--- crypto-ffi/bindings/android-interop/src/main/java/com/wire/androidinterop/MainActivity.kt
+++ crypto-ffi/bindings/android-interop/src/main/java/com/wire/androidinterop/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.wire.androidinterop
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,6 +13,11 @@ import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.json.Json
 
 class MainActivity : ComponentActivity() {
+
+    val actionHandler = runBlocking {
+        InteropActionHandler(InteropActionHandler.defaultCoreCryptoClient())
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -20,6 +26,27 @@ class MainActivity : ComponentActivity() {
                 modifier = Modifier.fillMaxSize().padding(16.dp)
             )
         }
+        println("Ready")
     }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+
+        if (intent == null || intent.action?.compareTo(Intent.ACTION_RUN) != 0) {
+            return
+        }
+
+        try {
+            val action = InteropAction.fromIntent(intent)
+            runBlocking {
+                actionHandler.handleAction(action)
+                    .onSuccess { println(Json.encodeToString(InteropResponse.serializer(), InteropResponse.Success(it))) }
+                    .onFailure { println(Json.encodeToString(InteropResponse.serializer(),InteropResponse.Failure(it.message ?: "Unknown error"))) }
+            }
+        } catch (e: Throwable) {
+            return println(Json.encodeToString(InteropResponse.serializer(), InteropResponse.Failure(e.message ?: "Unknown error")))
+        }
+    }
+
 }
 
