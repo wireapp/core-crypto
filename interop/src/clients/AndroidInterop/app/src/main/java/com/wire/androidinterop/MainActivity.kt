05dec8adcd69d0c2a2eba39319989f59f5ce70e7
Jacob Persson <7156+typfel@users.noreply.github.com>
2025-11-27 16:41:00 +0100

    feat: add android interop application
    The application follows the same structure as the iOS interop application. Interop commands
    are sent via intents and the result is printed to stdout / logcat.

--- interop/src/clients/AndroidInterop/app/src/main/java/com/wire/androidinterop/MainActivity.kt
+++ interop/src/clients/AndroidInterop/app/src/main/java/com/wire/androidinterop/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.wire.androidinterop
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,36 +13,58 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.wire.androidinterop.ui.theme.AndroidInteropTheme
+import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.json.Json
 
 class MainActivity : ComponentActivity() {
+
+    val actionHandler = runBlocking {
+        InteropActionHandler(InteropActionHandler.defaultCoreCryptoClient())
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             AndroidInteropTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    InteropLabel(
+                        modifier =  Modifier.padding(innerPadding)
                     )
                 }
             }
         }
+        println("Ready")
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+
+        if (intent == null || intent.action?.compareTo(Intent.ACTION_RUN) != 0) {
+            return
+        }
+
+        try {
+            val action = InteropAction.fromIntent(intent)
+            runBlocking {
+                actionHandler.handleAction(action)
+                    .onSuccess { println(Json.encodeToString(InteropResponse.serializer(), InteropResponse.Success(it))) }
+                    .onFailure { println(Json.encodeToString(InteropResponse.serializer(),InteropResponse.Failure(it.message ?: "Unknown error"))) }
+            }
+        } catch (e: Throwable) {
+            return println(Json.encodeToString(InteropResponse.serializer(), InteropResponse.Failure(e.message ?: "Unknown error")))
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun InteropLabel(modifier: Modifier) {
     AndroidInteropTheme {
-        Greeting("Android")
+        Text(
+            text = "Android Interop",
+            modifier = modifier
+        )
     }
 }
