a6d7b1b50b91a044f07d6c30d50a46729d72ef4d
Jacob Persson <7156+typfel@users.noreply.github.com>
2025-11-27 16:41:00 +0100

    feat: add android interop application
    The application follows the same structure as the iOS interop application. Interop commands
    are sent via intents and the result is printed to stdout / logcat.

--- crypto-ffi/bindings/android-interop/build.gradle.kts
+++ crypto-ffi/bindings/android-interop/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialisation)
 }
 
 android {
@@ -37,7 +38,8 @@ android {
 }
 
 dependencies {
-
+    implementation(project(":android"))
+    implementation(libs.ktxSerialization)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
