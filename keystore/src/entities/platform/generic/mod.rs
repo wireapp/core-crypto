f7dbabb3a7fdbe96a5599e018ee3e0f458f97097
Peter Goodspeed-Niklaus <peter.goodspeed-niklaus@wire.com>
2025-12-02 13:25:20 +0100

    chore: add helper functions `count_helper_tx`, `delete_helper`

--- keystore/src/entities/platform/generic/mod.rs
+++ keystore/src/entities/platform/generic/mod.rs
@@ -8,7 +8,10 @@ use rusqlite::{OptionalExtension, Row, ToSql};
 pub use self::mls::*;
 #[cfg(feature = "proteus-keystore")]
 pub use self::proteus::*;
-use crate::{CryptoKeystoreResult, Entity, connection::KeystoreDatabaseConnection};
+use crate::{
+    CryptoKeystoreResult, Entity,
+    connection::{KeystoreDatabaseConnection, TransactionWrapper},
+};
 
 /// Helper to perform an SQL query to get an entity by its primary key
 ///
@@ -53,6 +56,17 @@ async fn count_helper<E: Entity>(conn: &KeystoreDatabaseConnection) -> CryptoKey
     statement.query_one([], |row| row.get(0)).map_err(Into::into)
 }
 
+/// Helper to perform an SQL query to count these entities in the database.
+///
+/// This function prepares and caches a statement of the form `SELECT count(*) FROM collection_name`.
+async fn count_helper_tx<E: Entity>(tx: &TransactionWrapper<'_>) -> CryptoKeystoreResult<u32> {
+    let mut statement = tx.prepare_cached(&format!(
+        "SELECT count(*) FROM {collection_name}",
+        collection_name = E::COLLECTION_NAME
+    ))?;
+    statement.query_one([], |row| row.get(0)).map_err(Into::into)
+}
+
 /// Helper to perform an SQL query to load all entities from the database.
 ///
 /// This function prepares and caches a statement of the form `SELECT * FROM collection_name`.
@@ -79,4 +93,23 @@ where
         .query_map([], from_row)?
         .collect::<Result<_, _>>()
         .map_err(Into::into)
+}
+
+/// Helper to perform an SQL query to delete an entity from the database.
+///
+/// This function prepares and caches a statement of the form `DELETE FROM collection_name WHERE id = ?`.
+/// You need to provide the primary key's column name and the actual primary key.
+///
+/// Returns `true` if at least one entity was deleted, or `false` if the id was not found in the database.
+async fn delete_helper<E: Entity>(
+    tx: &TransactionWrapper<'_>,
+    primary_key_column_name: &str,
+    primary_key: impl ToSql,
+) -> CryptoKeystoreResult<bool> {
+    let mut statement = tx.prepare_cached(&format!(
+        "DELETE FROM {collection_name} WHERE {primary_key_column_name} = ?",
+        collection_name = E::COLLECTION_NAME,
+    ))?;
+    let updated = statement.execute([primary_key])?;
+    Ok(updated > 0)
 }
