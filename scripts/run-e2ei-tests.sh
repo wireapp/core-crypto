8f50271a2990c08d7784e62030cd654223e89b38
Ivan Stanković <ivan.stankovic@wire.com>
2025-12-05 11:10:18 +0100

    chore: scripts: add a script for running E2EI tests
    This is simply copied from rusty-jwt-tools.

---
+++ scripts/run-e2ei-tests.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+#
+# This script is used to start test-wire-server before the test suite is run,
+# because starting Keycloak requires that the redirect URL, which points to the
+# test Wire server, is known.
+
+case "$TEST_IDP" in
+    authelia | keycloak)
+        echo Using $TEST_IDP
+        ;;
+
+    *)
+        echo "You need to set TEST_IDP variable to 'authelia' or 'keycloak'."
+        exit 1
+        ;;
+esac
+
+tmpfile=$(mktemp)
+rm ${tmpfile}
+mkfifo ${tmpfile}
+cargo run --locked test-wire-server > ${tmpfile} &
+test_wire_server_pid=$!
+
+# The test suite needs this environment variable in order to set up the test
+# environment.
+read TEST_WIRE_SERVER_ADDR < ${tmpfile}
+export TEST_WIRE_SERVER_ADDR
+
+echo -e \\nRunning nextest with arguments \"$@\"
+cargo nextest run --locked "$@"
+test_exit_code="$?"
+
+# Clean up.
+case "$TEST_IDP" in
+    authelia)
+        (docker kill authelia.local && docker rm authelia.local) > /dev/null
+        ;;
+
+    keycloak)
+        (docker kill keycloak && docker rm keycloak) > /dev/null
+        ;;
+esac
+
+kill ${test_wire_server_pid}
+rm ${tmpfile}
+
+exit "$test_exit_code"
