376a595f1a1b6c81c33881b3a364725ac432f6f7
SimonThormeyer <simon.thormeyer@wire.com>
2025-12-05 15:04:14 +0100

    feat(keystore/generic): update credentials schema

---
+++ keystore/src/connection/platform/generic/migrations/V20__credentials_new_primary_key.sql
@@ -0,0 +1,31 @@
+CREATE TABLE mls_credentials_new (
+    public_key_sha256 TEXT UNIQUE NOT NULL,
+    public_key BLOB NOT NULL,
+    id BLOB NOT NULL,
+    credential BLOB NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    ciphersuite INT NOT NULL,
+    secret_key BLOB NOT NULL
+);
+
+INSERT INTO mls_credentials_new (
+    public_key_sha256,
+    public_key,
+    id,
+    credential,
+    created_at,
+    ciphersuite,
+    secret_key
+)
+SELECT sha256_blob(public_key),
+    public_key,
+    id,
+    credential,
+    created_at,
+    ciphersuite,
+    secret_key
+FROM mls_credentials;
+
+DROP TABLE mls_credentials;
+
+ALTER TABLE mls_credentials_new RENAME TO mls_credentials;
