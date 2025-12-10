c60d5db81cc0c0dfc150edd77f241733b2451835
SimonThormeyer <simon.thormeyer@wire.com>
2025-12-10 17:35:21 +0100

    test(wasm/keystore): with the old schema, it was impossible to create more than one credential per session

--- keystore/src/connection/platform/wasm/migrations/mod.rs
+++ keystore/src/connection/platform/wasm/migrations/mod.rs
@@ -87,3 +87,229 @@ async fn do_migration_step(from: u32, name: &str, key: &DatabaseKey) -> CryptoKe
         _ => Err(CryptoKeystoreError::MigrationNotSupported(from)),
     }
 }
+#[cfg(test)]
+mod tests {
+    use std::sync::LazyLock;
+
+    use idb::builder::{DatabaseBuilder, ObjectStoreBuilder};
+    use rand::Rng as _;
+    use serde::Serialize as _;
+    use wasm_bindgen_test::wasm_bindgen_test;
+
+    use super::*;
+    use crate::{
+        connection::{platform::wasm::WasmStorageTransaction, storage::WasmStorageWrapper},
+        entities::{Entity, EntityBase, EntityTransactionExt, StoredCredential},
+    };
+
+    pub(crate) static TEST_ENCRYPTION_KEY: LazyLock<DatabaseKey> = LazyLock::new(DatabaseKey::generate);
+
+    wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
+
+    #[wasm_bindgen_test]
+    pub async fn can_run_migrations() {
+        let name = "test";
+
+        let test_builder = |version| -> DatabaseBuilder {
+            v3::get_builder(name)
+                .add_object_store(ObjectStoreBuilder::new("regression_check").auto_increment(false))
+                .version(version)
+        };
+
+        let idb = test_builder(DB_VERSION_3).build().await.expect("DB VERSION 3");
+
+        assert!(idb.store_names().contains(&"regression_check".into()));
+        idb.close();
+
+        crate::Database::migrate_db_key_type_to_bytes(name, "test1234", &TEST_ENCRYPTION_KEY)
+            .await
+            .unwrap();
+
+        let mut conn = crate::Database::migration_connection(test_builder(DB_VERSION_4), &TEST_ENCRYPTION_KEY)
+            .await
+            .expect("DB_VERSION_4");
+
+        let WasmStorageWrapper::Persistent(db) = conn.storage_mut().wrapper() else {
+            panic!("Storage isn't persistent");
+        };
+
+        let store_names = db.store_names();
+
+        assert!(store_names.contains(&"regression_check".into()));
+
+        assert!(store_names.contains(&"mls_psk_bundles".into()));
+        assert!(store_names.contains(&"mls_hpke_private_keys".into()));
+        assert!(store_names.contains(&"mls_encryption_keypairs".into()));
+        assert!(store_names.contains(&"mls_keypackages".into()));
+        assert!(store_names.contains(&"mls_credentials".into()));
+        assert!(store_names.contains(&"mls_groups".into()));
+        assert!(store_names.contains(&"mls_pending_groups".into()));
+        assert!(store_names.contains(&"proteus_prekeys".into()));
+        assert!(store_names.contains(&"proteus_identities".into()));
+        assert!(store_names.contains(&"proteus_sessions".into()));
+        conn.close().await.expect("closing connection");
+
+        let migrated_db = crate::Database::open(crate::ConnectionType::Persistent(name), &TEST_ENCRYPTION_KEY)
+            .await
+            .expect("completing migrations");
+
+        migrated_db.close().await.expect("closing connection");
+        let factory = Factory::new().expect("factory");
+        factory.delete(name).expect("delete request").await.expect("wiping db");
+    }
+
+    #[wasm_bindgen_test]
+    pub async fn v5_schema_allows_1_cred_per_session() {
+        let name = "test";
+
+        let test_builder = |version| -> DatabaseBuilder {
+            v3::get_builder(name)
+                .add_object_store(ObjectStoreBuilder::new("regression_check").auto_increment(false))
+                .version(version)
+        };
+
+        let idb = test_builder(DB_VERSION_3).build().await.expect("DB VERSION 3");
+        idb.close();
+
+        crate::Database::migrate_db_key_type_to_bytes(name, "test1234", &TEST_ENCRYPTION_KEY)
+            .await
+            .unwrap();
+
+        let builder = v5::get_builder(name);
+        let conn = crate::Database::migration_connection(builder, &TEST_ENCRYPTION_KEY)
+            .await
+            .expect("DB_VERSION_6");
+
+        const LEN_RANGE: std::ops::Range<usize> = 1024..8192;
+
+        let mut rng = rand::thread_rng();
+
+        crate::Database::migration_transaction(conn, async |tx| {
+            use openmls::prelude::Ciphersuite;
+
+            use crate::entities::StoredCredential;
+
+            let mut random_vec = || {
+                let len = rng.gen_range(LEN_RANGE);
+                let v: Vec<u8> = (0..len).map(|_| rng.r#gen()).collect();
+                v
+            };
+
+            let cred_a = StoredCredential {
+                session_id: random_vec(),
+                credential: random_vec(),
+                created_at: 2025,
+                ciphersuite: Ciphersuite::MLS_128_DHKEMX25519_AES128GCM_SHA256_Ed25519 as u16,
+                public_key: random_vec(),
+                private_key: random_vec(),
+            };
+
+            // Try creating a duplicate of this credential
+            let mut cred_b = cred_a.clone();
+            cred_b.credential = random_vec();
+            cred_b.ciphersuite = Ciphersuite::MLS_128_DHKEMX25519_CHACHA20POLY1305_SHA256_Ed25519 as u16;
+
+            let credentials = [cred_a, cred_b];
+            match tx {
+                WasmStorageTransaction::Persistent { tx, cipher } => {
+                    let serializer = serde_wasm_bindgen::Serializer::json_compatible();
+                    let store = tx.object_store(StoredCredential::COLLECTION_NAME)?;
+                    for mut credential in credentials {
+                        // Save credentials with session id as key
+                        let key = &js_sys::Uint8Array::from(credential.session_id.as_slice()).into();
+                        credential.encrypt(cipher)?;
+                        let js_value = credential.serialize(&serializer)?;
+                        store.put(&js_value, Some(key))?.await?;
+                    }
+                }
+                WasmStorageTransaction::InMemory { .. } => {}
+            }
+
+            Ok(())
+        })
+        .await
+        .expect("inserting test data");
+
+        let builder = v6::get_builder(name);
+        let mut conn = crate::Database::migration_connection(builder, &TEST_ENCRYPTION_KEY)
+            .await
+            .expect("DB_VERSION_6");
+
+        let count = StoredCredential::count(&mut conn).await.expect("credential count");
+
+        assert_eq!(
+            count, 1,
+            "saving two different credentials by the same session id will result in a single credential"
+        );
+
+        let migrated_db = crate::Database::open(crate::ConnectionType::Persistent(name), &TEST_ENCRYPTION_KEY)
+            .await
+            .expect("completing migrations");
+        migrated_db.close().await.expect("closing connection");
+        let factory = Factory::new().expect("factory");
+        factory.delete(name).expect("delete request").await.expect("wiping db");
+    }
+
+    #[wasm_bindgen_test]
+    pub async fn v9_schema_allows_multiple_creds_per_session() {
+        let name = "test";
+        const LEN_RANGE: std::ops::Range<usize> = 1024..8192;
+        let mut rng = rand::thread_rng();
+
+        let builder = v9::get_builder(name);
+        let conn = crate::Database::migration_connection(builder, &TEST_ENCRYPTION_KEY)
+            .await
+            .expect("DB_VERSION_9");
+
+        crate::Database::migration_transaction(conn, async |tx| {
+            use openmls::prelude::Ciphersuite;
+
+            use crate::entities::StoredCredential;
+
+            let mut random_vec = || {
+                let len = rng.gen_range(LEN_RANGE);
+                let v: Vec<u8> = (0..len).map(|_| rng.r#gen()).collect();
+                v
+            };
+
+            let cred_a = StoredCredential {
+                session_id: random_vec(),
+                credential: random_vec(),
+                created_at: 2025,
+                ciphersuite: Ciphersuite::MLS_128_DHKEMX25519_AES128GCM_SHA256_Ed25519 as u16,
+                public_key: random_vec(),
+                private_key: random_vec(),
+            };
+
+            // Insert a second credential
+            let mut cred_b = cred_a.clone();
+            cred_b.public_key = random_vec();
+
+            let credentials = [cred_a, cred_b];
+            for credential in credentials {
+                credential.save(tx).await?;
+            }
+
+            Ok(())
+        })
+        .await
+        .expect("inserting test data");
+
+        let builder = v9::get_builder(name);
+        let mut conn = crate::Database::migration_connection(builder, &TEST_ENCRYPTION_KEY)
+            .await
+            .expect("DB_VERSION_9");
+
+        let count = StoredCredential::count(&mut conn).await.expect("credential count");
+
+        assert_eq!(count, 2);
+
+        let migrated_db = crate::Database::open(crate::ConnectionType::Persistent(name), &TEST_ENCRYPTION_KEY)
+            .await
+            .expect("completing migrations");
+
+        migrated_db.close().await.expect("closing connection");
+        let factory = Factory::new().expect("factory");
+        factory.delete(name).expect("delete request").await.expect("wiping db");
+    }
+}
