b0893e7891c401489a41addb1285c1727ac2b708
Peter Goodspeed-Niklaus <peter.goodspeed-niklaus@wire.com>
2025-12-15 14:07:35 +0100

    chore(keystore): impl new entity traits for `PersistedMlsPendingGroup`

--- keystore/src/entities/platform/wasm/mls/mod.rs
+++ keystore/src/entities/platform/wasm/mls/mod.rs
@@ -3,6 +3,7 @@ pub mod e2ei_acme_ca;
 pub mod encryption_keypair;
 pub mod group;
 pub mod hpke_private_key;
+pub mod pending_group;
 pub mod pending_message;
 pub mod psk_bundle;
 pub mod refresh_token;
