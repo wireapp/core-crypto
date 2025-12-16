be6219cfba81febcc2eac313e35cb17c3ea8f12e
Peter Goodspeed-Niklaus <peter.goodspeed-niklaus@wire.com>
2025-12-16 10:54:25 +0100

    feat(keystore): implement new methods on wasm storage, transaction
    These new methods are specialized for the new entity traits and provide
    a more efficient API. In WPB-22196 we're going to need to remove all the
    old methods and rename these to take over their names.

--- keystore/src/connection/platform/wasm/storage/storage.rs
+++ keystore/src/connection/platform/wasm/storage/storage.rs
@@ -1,6 +1,7 @@
 use aes_gcm::KeyInit as _;
 use idb::TransactionMode;
 use js_sys::Uint8Array;
+use serde::de::DeserializeOwned;
 use sha2::Digest as _;
 use wasm_bindgen::JsValue;
 
@@ -9,6 +10,7 @@ use crate::{
     CryptoKeystoreResult,
     connection::DatabaseKey,
     entities::{Entity, EntityFindParams},
+    traits::{Decryptable, Decrypting, Entity as NewEntity},
 };
 
 pub struct WasmEncryptedStorage {
@@ -238,5 +240,124 @@ impl WasmEncryptedStorage {
         }
 
         Ok(())
+    }
+
+    /// Count the number of entities in this storage.
+    pub async fn new_count<E>(&self) -> CryptoKeystoreResult<u32>
+    where
+        E: NewEntity<ConnectionType = WasmConnection>,
+    {
+        match &self.storage {
+            WasmStorageWrapper::Persistent(idb) => {
+                let transaction = idb.transaction(&[E::COLLECTION_NAME], TransactionMode::ReadOnly)?;
+                let store = transaction.object_store(E::COLLECTION_NAME)?;
+                let request = store.count(None)?;
+                let data = request.await?;
+
+                Ok(data)
+            }
+            WasmStorageWrapper::InMemory(map) => Ok(map
+                .borrow()
+                .get(E::COLLECTION_NAME)
+                .map(|v| v.values().len() as _)
+                .unwrap_or_default()),
+        }
+    }
+
+    /// Get the instance of `E` whose primary key serializes to `key`.
+    ///
+    /// Note that `key`'s type is not directly tied to `E`. We don't know if `E` implements
+    /// `BorrowPrimaryKey` or not, and without specialization, we can't just do the right thing
+    /// and accept the more general form. But we do know the primary key and its borrowed form
+    /// both implement `KeyType`, so it's always safe to accept a byte reference.
+    ///
+    /// We're forced to require that `E`'s decryptable form is `DeserializeOwned`, not just `Deserialize`.
+    /// This is an annoying limitation, because it obviates the work done to ensure that a decrypting instance
+    /// could borrow from serialized data without necessarily copying everything.
+    /// We can't really get around it, though: it stems from the `T: DeserializeOwned` bound in
+    /// [`serde_wasm_bindgen::from_value`].
+    //
+    // After putting some thought into it, I'd prefer not to redesign the `Decrypting` trait, though.
+    // There's always the chance that `serde_wasm_bindgen` will relax that restriction, at which point
+    // we can just relax this bound and all will be good.
+    pub async fn new_get<'a, E>(&self, key: &[u8]) -> CryptoKeystoreResult<Option<E>>
+    where
+        E: NewEntity<ConnectionType = WasmConnection> + Decryptable<'a>,
+        <E as Decryptable<'a>>::DecryptableFrom: DeserializeOwned,
+    {
+        let js_value = match &self.storage {
+            WasmStorageWrapper::Persistent(idb) => {
+                let transaction = idb.transaction(&[E::COLLECTION_NAME], TransactionMode::ReadOnly)?;
+                let store = transaction.object_store(E::COLLECTION_NAME)?;
+                let id = Uint8Array::from(key);
+                let Some(js_value) = store.get(JsValue::from(id))?.await? else {
+                    return Ok(None);
+                };
+                js_value
+            }
+            WasmStorageWrapper::InMemory(map) => {
+                let map = map.borrow();
+                let Some(store) = map.get(E::COLLECTION_NAME) else {
+                    return Ok(None);
+                };
+                let Some(js_value) = store.get(key).cloned() else {
+                    return Ok(None);
+                };
+                js_value
+            }
+        };
+
+        let decryptable_entity = serde_wasm_bindgen::from_value::<E::DecryptableFrom>(js_value)?;
+        let entity = decryptable_entity.decrypt(&self.cipher)?;
+
+        Ok(Some(entity))
+    }
+
+    /// Get all instances of `E` from the database.
+    pub async fn new_get_all<'a, E>(&self) -> CryptoKeystoreResult<Vec<E>>
+    where
+        E: NewEntity<ConnectionType = WasmConnection> + Decryptable<'a>,
+        <E as Decryptable<'a>>::DecryptableFrom: DeserializeOwned,
+    {
+        self.new_get_all_with_query(None).await
+    }
+
+    /// Get all instance of `E` from the database, limiting to the key or key range specified in `query`.
+    ///
+    /// This has the same `DeserializeOwned` limitation that [`Self::new_get`] does; see documentation
+    /// there for details.
+    pub async fn new_get_all_with_query<'a, E>(&self, query: Option<idb::Query>) -> CryptoKeystoreResult<Vec<E>>
+    where
+        E: NewEntity<ConnectionType = WasmConnection> + Decryptable<'a>,
+        <E as Decryptable<'a>>::DecryptableFrom: DeserializeOwned,
+    {
+        let js_values = match &self.storage {
+            WasmStorageWrapper::Persistent(idb) => {
+                let transaction = idb.transaction(&[E::COLLECTION_NAME], TransactionMode::ReadOnly)?;
+                let store = transaction.object_store(E::COLLECTION_NAME)?;
+                store.get_all(query, None)?.await?
+            }
+            WasmStorageWrapper::InMemory(map) => {
+                let map = map.borrow();
+                map.get(E::COLLECTION_NAME)
+                    .map(|v| {
+                        v.values().cloned()
+                        // Annoying that we have to allocate a vector of the collected values instead of just
+                        // iterating over borrowed values, but that's the simplest way to keep compat with
+                        // the idb version
+                        .collect()
+                    })
+                    .unwrap_or_default()
+            }
+        };
+
+        js_values
+            .into_iter()
+            .map(|js_value| {
+                serde_wasm_bindgen::from_value::<E::DecryptableFrom>(js_value)
+                    .map_err(Into::into)
+                    .and_then(|decryptable| decryptable.decrypt(&self.cipher))
+            })
+            .collect()
     }
 }
--- keystore/src/connection/platform/wasm/storage/transaction.rs
+++ keystore/src/connection/platform/wasm/storage/transaction.rs
@@ -1,10 +1,15 @@
 use std::{cell::RefCell, rc::Rc};
 
 use js_sys::Uint8Array;
+use serde::Serialize;
 use wasm_bindgen::JsValue;
 
 use super::{super::WasmConnection, InMemoryDB};
-use crate::{CryptoKeystoreError, CryptoKeystoreResult, entities::Entity};
+use crate::{
+    CryptoKeystoreError, CryptoKeystoreResult,
+    entities::Entity,
+    traits::{Encrypting, Entity as NewEntity, KeyType},
+};
 
 // The lifetime is to comply with the sqlite implementation.
 pub enum WasmStorageTransaction<'a> {
@@ -19,6 +24,13 @@ pub enum WasmStorageTransaction<'a> {
 }
 
 impl WasmStorageTransaction<'_> {
+    fn cipher(&self) -> &aes_gcm::Aes256Gcm {
+        match self {
+            WasmStorageTransaction::Persistent { cipher, .. } => cipher,
+            WasmStorageTransaction::InMemory { cipher, .. } => cipher,
+        }
+    }
+
     pub(crate) async fn commit_tx(self) -> CryptoKeystoreResult<()> {
         let Self::Persistent {
             tx: transaction,
@@ -81,9 +93,11 @@ impl WasmStorageTransaction<'_> {
         Ok(())
     }
 
-    // This will start seeing use in WPB-22194
-    #[expect(dead_code)]
-    pub(crate) async fn count<E: Entity<ConnectionType = WasmConnection>>(&self) -> CryptoKeystoreResult<u32> {
+    /// Count the number of entities in this transaction.
+    pub(crate) async fn new_count<E>(&self) -> CryptoKeystoreResult<u32>
+    where
+        E: NewEntity<ConnectionType = WasmConnection>,
+    {
         match self {
             WasmStorageTransaction::Persistent { tx, .. } => {
                 let object_store = tx.object_store(E::COLLECTION_NAME)?;
@@ -96,6 +110,65 @@ impl WasmStorageTransaction<'_> {
                     .get(E::COLLECTION_NAME)
                     .map(|collection| collection.len())
                     .unwrap_or_default() as _)
+            }
+        }
+    }
+
+    /// Save an entity instance into the transaction.
+    pub(crate) async fn new_save<'a, E>(&self, entity: &'a E) -> CryptoKeystoreResult<()>
+    where
+        E: NewEntity<ConnectionType = WasmConnection> + Encrypting<'a>,
+    {
+        let serializer = serde_wasm_bindgen::Serializer::json_compatible();
+        let encrypted = entity.encrypt(self.cipher())?;
+        let js_value = encrypted.serialize(&serializer)?;
+
+        let key = JsValue::from(Uint8Array::from(entity.primary_key().bytes().as_ref()));
+
+        match self {
+            WasmStorageTransaction::Persistent { tx, .. } => {
+                let store = tx.object_store(E::COLLECTION_NAME)?;
+                store.put(&js_value, Some(&key))?.await?;
+            }
+            WasmStorageTransaction::InMemory { db, .. } => {
+                let mut map = db.borrow_mut();
+                let entry = map.entry(E::COLLECTION_NAME.into()).or_default();
+                let id = key
+                    .as_string()
+                    .map(|s| CryptoKeystoreResult::Ok(s.as_bytes().into()))
+                    .unwrap_or_else(|| Ok(serde_wasm_bindgen::from_value(key)?))?;
+                entry.insert(id, js_value);
+            }
+        }
+
+        Ok(())
+    }
+
+    /// Remove an entity by key from the transaction.
+    ///
+    /// Note that `key`'s type is not directly tied to `E`. We don't know if `E` implements
+    /// `BorrowPrimaryKey` or not, and without specialization, we can't just do the right thing
+    /// and accept the more general form. But we do know the primary key and its borrowed form
+    /// both implement `KeyType`, so it's always safe to accept a byte reference.
+    pub(crate) async fn new_delete<E>(&self, key: &[u8]) -> CryptoKeystoreResult<bool>
+    where
+        E: NewEntity<ConnectionType = WasmConnection>,
+    {
+        match self {
+            WasmStorageTransaction::Persistent { tx, .. } => {
+                let query = JsValue::from(Uint8Array::from(key));
+
+                let store = tx.object_store(E::COLLECTION_NAME)?;
+                let existed = store.count(Some(query.clone().into()))?.await?;
+                store.delete(query)?.await?;
+
+                Ok(existed > 0)
+            }
+            WasmStorageTransaction::InMemory { db, .. } => {
+                let mut store = db.borrow_mut();
+                let store = store.entry(E::COLLECTION_NAME.into()).or_default();
+                let removed = store.remove(key);
+                Ok(removed.is_some())
             }
         }
     }
