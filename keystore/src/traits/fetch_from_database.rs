80a0aca608736b2f66f8ef5ca82a3890824abeef
Peter Goodspeed-Niklaus <peter.goodspeed-niklaus@wire.com>
2025-12-19 11:44:56 +0100

    chore(keystore): update `trait FetchFromDatabase` for coherency
    The trait used to be a bit loosely specified, since nobody was implementing it.
    Now we know precisely what it needs and how to implement it.

--- keystore/src/traits/fetch_from_database.rs
+++ keystore/src/traits/fetch_from_database.rs
@@ -1,9 +1,11 @@
+use std::borrow::Borrow;
+
 use async_trait::async_trait;
 
 use crate::{
     CryptoKeystoreResult,
     connection::KeystoreDatabaseConnection,
-    traits::{BorrowPrimaryKey, Entity, UniqueEntity, UniqueEntityExt},
+    traits::{BorrowPrimaryKey, Entity, KeyType, UniqueEntity, UniqueEntityExt},
 };
 
 /// Interface to fetch from the database either from the connection directly or through a
@@ -17,17 +19,17 @@ pub trait FetchFromDatabase: Send + Sync {
     /// Get an instance of `E` from the database by its primary key.
     async fn get<E>(&self, id: &<E as Entity>::PrimaryKey) -> CryptoKeystoreResult<Option<E>>
     where
-        E: Entity<ConnectionType = KeystoreDatabaseConnection>;
+        E: Entity<ConnectionType = KeystoreDatabaseConnection> + Clone + Send + Sync;
 
     /// Count the number of `E`s in the database.
     async fn count<E>(&self) -> CryptoKeystoreResult<u32>
     where
-        E: Entity<ConnectionType = KeystoreDatabaseConnection>;
+        E: Entity<ConnectionType = KeystoreDatabaseConnection> + Clone + Send + Sync;
 
     /// Load all `E`s from the database.
     async fn load_all<E>(&self) -> CryptoKeystoreResult<Vec<E>>
     where
-        E: Entity<ConnectionType = KeystoreDatabaseConnection>;
+        E: Entity<ConnectionType = KeystoreDatabaseConnection> + Clone + Send + Sync;
 
     /// Get an instance of `E` from the database by the borrowed form of its primary key.
     async fn get_borrowed<E>(
@@ -35,15 +37,24 @@ pub trait FetchFromDatabase: Send + Sync {
         id: &<E as BorrowPrimaryKey>::BorrowedPrimaryKey,
     ) -> CryptoKeystoreResult<Option<E>>
     where
-        E: BorrowPrimaryKey;
+        E: Entity<ConnectionType = KeystoreDatabaseConnection> + BorrowPrimaryKey + Clone + Send + Sync,
+        E::PrimaryKey: Borrow<E::BorrowedPrimaryKey>,
+        for<'a> &'a E::BorrowedPrimaryKey: KeyType;
 
     /// Get the requested unique entity from the database.
     async fn get_unique<'a, U>(&self) -> CryptoKeystoreResult<Option<U>>
     where
-        U: UniqueEntityExt<'a>;
+        U: UniqueEntityExt<'a> + Entity<ConnectionType = KeystoreDatabaseConnection> + Clone + Send + Sync,
+    {
+        self.get::<U>(&U::KEY).await
+    }
 
     /// Determine whether a unique entity is present in the database.
     async fn exists<'a, U>(&self) -> CryptoKeystoreResult<bool>
     where
-        U: UniqueEntityExt<'a>;
+        U: UniqueEntityExt<'a> + Entity<ConnectionType = KeystoreDatabaseConnection> + Clone + Send + Sync,
+    {
+        let count = self.count::<U>().await?;
+        Ok(count > 0)
+    }
 }
