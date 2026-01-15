ce07525198964870d5e45c64e62f64ad1337d671
Peter Goodspeed-Niklaus <peter.goodspeed-niklaus@wire.com>
2026-01-15 10:38:35 +0100

    chore(keystore): rm old entity trait impls

--- keystore/src/connection/platform/wasm/storage/transaction.rs
+++ keystore/src/connection/platform/wasm/storage/transaction.rs
@@ -7,7 +7,6 @@ use wasm_bindgen::JsValue;
 use super::{super::WasmConnection, InMemoryDB};
 use crate::{
     CryptoKeystoreError, CryptoKeystoreResult,
-    entities::Entity,
     traits::{Encrypting, Entity as NewEntity, KeyType},
 };
 
@@ -44,51 +43,6 @@ impl WasmStorageTransaction<'_> {
             return Err(CryptoKeystoreError::MlsKeyStoreError(
                 "Transaction aborted. Check console logs for details.".to_string(),
             ));
-        }
-        Ok(())
-    }
-
-    pub(crate) async fn delete(&self, collection_name: &'static str, id: impl AsRef<[u8]>) -> CryptoKeystoreResult<()> {
-        match self {
-            WasmStorageTransaction::Persistent { tx, cipher: _cipher } => {
-                let store = tx.object_store(collection_name)?;
-                let k = Uint8Array::from(id.as_ref());
-                store.delete(JsValue::from(k))?.await?;
-            }
-            WasmStorageTransaction::InMemory { db, cipher: _cipher } => {
-                db.borrow_mut().entry(collection_name.into()).and_modify(|store| {
-                    store.remove(id.as_ref());
-                });
-            }
-        }
-        Ok(())
-    }
-
-    pub(crate) async fn save<R: Entity<ConnectionType = WasmConnection>>(
-        &self,
-        mut entity: R,
-    ) -> CryptoKeystoreResult<()> {
-        let serializer = serde_wasm_bindgen::Serializer::json_compatible();
-        let collection_name = R::COLLECTION_NAME;
-        let key = entity.id()?;
-        match self {
-            WasmStorageTransaction::Persistent { tx, cipher } => {
-                entity.encrypt(cipher)?;
-                let js_value = entity.serialize(&serializer)?;
-                let store = tx.object_store(collection_name)?;
-                store.put(&js_value, Some(&key))?.await?;
-            }
-            WasmStorageTransaction::InMemory { db, cipher } => {
-                entity.encrypt(cipher)?;
-                let js_value = entity.serialize(&serializer)?;
-                let mut map = db.borrow_mut();
-                let entry = map.entry(collection_name.into()).or_default();
-                let id = key
-                    .as_string()
-                    .map(|s| CryptoKeystoreResult::Ok(s.as_bytes().into()))
-                    .unwrap_or_else(|| Ok(serde_wasm_bindgen::from_value(key)?))?;
-                entry.insert(id, js_value);
-            }
         }
         Ok(())
     }
--- keystore/src/entities/platform/generic/mls/credential.rs
+++ keystore/src/entities/platform/generic/mls/credential.rs
@@ -8,12 +8,9 @@ use async_trait::async_trait;
 use rusqlite::{OptionalExtension as _, Row, Transaction, params};
 
 use crate::{
-    CryptoKeystoreError, CryptoKeystoreResult, MissingKeyErrorKind, Sha256Hash,
+    CryptoKeystoreError, CryptoKeystoreResult, Sha256Hash,
     connection::{DatabaseConnection, KeystoreDatabaseConnection, TransactionWrapper},
-    entities::{
-        Entity, EntityBase, EntityFindParams, EntityIdStringExt as _, EntityTransactionExt, StoredCredential,
-        StringEntityId, count_helper, count_helper_tx, delete_helper,
-    },
+    entities::{StoredCredential, count_helper, count_helper_tx, delete_helper},
     traits::{
         BorrowPrimaryKey, Entity as NewEntity, EntityBase as NewEntityBase, EntityDatabaseMutation,
         EntityDeleteBorrowed, KeyType, PrimaryKey,
@@ -27,46 +24,22 @@ impl StoredCredential {
         created_at: u64,
         ciphersuite: u16,
     ) -> CryptoKeystoreResult<Self> {
-        let mut blob = transaction.blob_open(
-            rusqlite::MAIN_DB,
-            <Self as EntityBase>::COLLECTION_NAME,
-            "session_id",
-            rowid,
-            true,
-        )?;
+        let mut blob = transaction.blob_open(rusqlite::MAIN_DB, Self::COLLECTION_NAME, "session_id", rowid, true)?;
         let mut session_id = vec![];
         blob.read_to_end(&mut session_id)?;
         blob.close()?;
 
-        let mut blob = transaction.blob_open(
-            rusqlite::MAIN_DB,
-            <Self as EntityBase>::COLLECTION_NAME,
-            "credential",
-            rowid,
-            true,
-        )?;
+        let mut blob = transaction.blob_open(rusqlite::MAIN_DB, Self::COLLECTION_NAME, "credential", rowid, true)?;
         let mut credential = vec![];
         blob.read_to_end(&mut credential)?;
         blob.close()?;
 
-        let mut blob = transaction.blob_open(
-            rusqlite::MAIN_DB,
-            <Self as EntityBase>::COLLECTION_NAME,
-            "private_key",
-            rowid,
-            true,
-        )?;
+        let mut blob = transaction.blob_open(rusqlite::MAIN_DB, Self::COLLECTION_NAME, "private_key", rowid, true)?;
         let mut private_key = vec![];
         blob.read_to_end(&mut private_key)?;
         blob.close()?;
 
-        let mut blob = transaction.blob_open(
-            rusqlite::MAIN_DB,
-            <Self as EntityBase>::COLLECTION_NAME,
-            "public_key",
-            rowid,
-            true,
-        )?;
+        let mut blob = transaction.blob_open(rusqlite::MAIN_DB, Self::COLLECTION_NAME, "public_key", rowid, true)?;
         let mut public_key = vec![];
         blob.read_to_end(&mut public_key)?;
         blob.close()?;
@@ -97,192 +70,6 @@ impl StoredCredential {
             public_key,
             private_key,
         })
-    }
-}
-
-#[async_trait::async_trait]
-impl Entity for StoredCredential {
-    fn id_raw(&self) -> &[u8] {
-        &self.public_key
-    }
-
-    async fn find_all(
-        conn: &mut Self::ConnectionType,
-        params: EntityFindParams,
-    ) -> crate::CryptoKeystoreResult<Vec<Self>> {
-        let mut conn = conn.conn().await;
-        let transaction = conn.transaction()?;
-        let query: String = format!(
-            "SELECT rowid, unixepoch(created_at), ciphersuite FROM mls_credentials {}",
-            params.to_sql()
-        );
-
-        transaction
-            .prepare_cached(&query)?
-            .query_map([], |row| {
-                let rowid = row.get(0)?;
-                let created_at = row.get(1)?;
-                let ciphersuite = row.get(2)?;
-                Ok((rowid, created_at, ciphersuite))
-            })?
-            .map(|rowid_result| -> CryptoKeystoreResult<_> {
-                let (rowid, created_at, ciphersuite) = rowid_result?;
-                Self::load(&transaction, rowid, created_at, ciphersuite)
-            })
-            .collect()
-    }
-
-    async fn find_one(
-        conn: &mut Self::ConnectionType,
-        id: &StringEntityId,
-    ) -> crate::CryptoKeystoreResult<Option<Self>> {
-        let mut conn = conn.conn().await;
-        let transaction = conn.transaction()?;
-        use rusqlite::OptionalExtension as _;
-        let public_key_sha256 = id.sha256();
-        transaction
-            .query_row(
-                "SELECT rowid, unixepoch(created_at), ciphersuite FROM mls_credentials WHERE public_key_sha256 = ?",
-                [public_key_sha256],
-                |r| Ok((r.get::<_, i64>(0)?, r.get(1)?, r.get(2)?)),
-            )
-            .optional()?
-            .map(|(rowid, created_at, ciphersuite)| Self::load(&transaction, rowid, created_at, ciphersuite))
-            .transpose()
-    }
-
-    async fn count(conn: &mut Self::ConnectionType) -> crate::CryptoKeystoreResult<usize> {
-        let conn = conn.conn().await;
-        conn.query_row("SELECT COUNT(*) FROM mls_credentials", [], |r| r.get(0))
-            .map_err(Into::into)
-    }
-}
-
-#[async_trait::async_trait]
-impl EntityBase for StoredCredential {
-    type ConnectionType = KeystoreDatabaseConnection;
-    type AutoGeneratedFields = u64;
-    const COLLECTION_NAME: &'static str = "mls_credentials";
-
-    fn to_missing_key_err_kind() -> MissingKeyErrorKind {
-        MissingKeyErrorKind::StoredCredential
-    }
-
-    fn to_transaction_entity(self) -> crate::transaction::dynamic_dispatch::Entity {
-        crate::transaction::dynamic_dispatch::Entity::StoredCredential(self.into())
-    }
-}
-
-#[async_trait::async_trait]
-impl EntityTransactionExt for StoredCredential {
-    async fn save(&self, transaction: &TransactionWrapper<'_>) -> crate::CryptoKeystoreResult<()> {
-        Self::ConnectionType::check_buffer_size(self.session_id.len())?;
-        Self::ConnectionType::check_buffer_size(self.credential.len())?;
-        Self::ConnectionType::check_buffer_size(self.private_key.len())?;
-        Self::ConnectionType::check_buffer_size(self.public_key.len())?;
-
-        let pk_sha256 = self.id_sha256();
-        let zb_pk = rusqlite::blob::ZeroBlob(self.public_key.len() as i32);
-        let zb_id = rusqlite::blob::ZeroBlob(self.session_id.len() as i32);
-        let zb_cred = rusqlite::blob::ZeroBlob(self.credential.len() as i32);
-        let zb_sk = rusqlite::blob::ZeroBlob(self.private_key.len() as i32);
-
-        use rusqlite::ToSql as _;
-        let params: [rusqlite::types::ToSqlOutput; 7] = [
-            pk_sha256.to_sql()?,
-            zb_pk.to_sql()?,
-            zb_id.to_sql()?,
-            zb_cred.to_sql()?,
-            self.created_at.to_sql()?,
-            self.ciphersuite.to_sql()?,
-            zb_sk.to_sql()?,
-        ];
-
-        let sql = "INSERT INTO mls_credentials (
-                public_key_sha256,
-                public_key,
-                session_id,
-                credential,
-                created_at,
-                ciphersuite,
-                private_key
-            ) VALUES (?, ?, ?, ?, datetime(?, 'unixepoch'), ?, ?)";
-
-        transaction.execute(sql, params)?;
-        let row_id = transaction.last_insert_rowid();
-
-        let mut blob = transaction.blob_open(
-            rusqlite::MAIN_DB,
-            <Self as EntityBase>::COLLECTION_NAME,
-            "session_id",
-            row_id,
-            false,
-        )?;
-
-        blob.write_all(&self.session_id)?;
-        blob.close()?;
-
-        let mut blob = transaction.blob_open(
-            rusqlite::MAIN_DB,
-            <Self as EntityBase>::COLLECTION_NAME,
-            "credential",
-            row_id,
-            false,
-        )?;
-
-        blob.write_all(&self.credential)?;
-        blob.close()?;
-
-        let mut blob = transaction.blob_open(
-            rusqlite::MAIN_DB,
-            <Self as EntityBase>::COLLECTION_NAME,
-            "public_key",
-            row_id,
-            false,
-        )?;
-
-        blob.write_all(&self.public_key)?;
-        blob.close()?;
-
-        let mut blob = transaction.blob_open(
-            rusqlite::MAIN_DB,
-            <Self as EntityBase>::COLLECTION_NAME,
-            "private_key",
-            row_id,
-            false,
-        )?;
-
-        blob.write_all(&self.private_key)?;
-        blob.close()?;
-
-        Ok(())
-    }
-
-    async fn pre_save<'a>(&'a mut self) -> CryptoKeystoreResult<Self::AutoGeneratedFields> {
-        let now = SystemTime::now();
-        let created_at = now
-            .duration_since(SystemTime::UNIX_EPOCH)
-            .map_err(|_| CryptoKeystoreError::TimestampError)?
-            .as_secs();
-        self.created_at = created_at;
-        Ok(created_at)
-    }
-
-    async fn delete_fail_on_missing_id(
-        transaction: &TransactionWrapper<'_>,
-        id: StringEntityId<'_>,
-    ) -> crate::CryptoKeystoreResult<()> {
-        let public_key_sha256 = id.sha256();
-        let updated = transaction.execute(
-            "DELETE FROM mls_credentials WHERE public_key_sha256 = ?",
-            [public_key_sha256],
-        )?;
-
-        if updated > 0 {
-            Ok(())
-        } else {
-            Err(Self::to_missing_key_err_kind().into())
-        }
     }
 }
 
