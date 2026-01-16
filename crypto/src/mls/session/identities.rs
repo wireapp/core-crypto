0f07043b458d50b443960aec2dbf2fe8ffe95eee
SimonThormeyer <simon.thormeyer@wire.com>
2026-01-16 16:46:02 +0100

    feat: remove `Identities.find_credentials()`
    Also, refactor a test that would fail after this removal due to clumsy
    setup.

--- crypto/src/mls/session/identities.rs
+++ crypto/src/mls/session/identities.rs
@@ -4,7 +4,7 @@ use openmls::prelude::{Credential as MlsCredential, SignaturePublicKey};
 use openmls_traits::types::SignatureScheme;
 
 use crate::{
-    Credential, CredentialFindFilters, CredentialType, Session,
+    Credential, CredentialType, Session,
     mls::session::error::{Error, Result},
 };
 
@@ -38,37 +38,6 @@ impl Identities {
         credential_type: CredentialType,
     ) -> Option<&Vec<Arc<Credential>>> {
         self.credentials.get(&(signature_scheme, credential_type.into()))
-    }
-
-    /// Return an iterator over all credentials matching the given filters.
-    pub(crate) fn find_credential(&self, filters: CredentialFindFilters<'_>) -> impl Iterator<Item = Arc<Credential>> {
-        let CredentialFindFilters {
-            client_id,
-            public_key,
-            ciphersuite,
-            credential_type,
-            earliest_validity,
-        } = filters;
-
-        // we have an easy way to filter out a bunch of credentials if the right filters are set,
-        // but we have to search through all of them if it is not.
-        let values: Box<dyn Iterator<Item = Arc<Credential>>> = match ciphersuite.zip(credential_type) {
-            Some((ciphersuite, credential_type)) => {
-                match self.index(ciphersuite.signature_algorithm(), credential_type) {
-                    Some(set) => Box::new(set.iter().cloned()),
-                    None => Box::new(std::iter::empty()),
-                }
-            }
-            None => Box::new(self.credentials.values().flatten().cloned()),
-        };
-
-        values.filter(move |credential| {
-            ciphersuite.is_none_or(|ciphersuite| credential.signature_scheme() == ciphersuite.signature_algorithm())
-                && credential_type.is_none_or(|credential_type| credential.credential_type() == credential_type)
-                && earliest_validity.is_none_or(|earliest_validity| credential.earliest_validity == earliest_validity)
-                && client_id.is_none_or(|client_id| credential.client_id() == client_id)
-                && public_key.is_none_or(|public_key| credential.signature_key_pair.public() == public_key)
-        })
     }
 
     /// Return the first credential matching the supplied fields
