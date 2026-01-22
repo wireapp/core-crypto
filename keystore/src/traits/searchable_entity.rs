99ee06c45a65d95df0a9996cdd6cc386a1a507df
Ivan Stanković <ivan.stankovic@wire.com>
2026-01-22 10:00:30 +0100

    chore: keystore: fix typo

--- keystore/src/traits/searchable_entity.rs
+++ keystore/src/traits/searchable_entity.rs
@@ -14,7 +14,7 @@ use crate::{
 /// This trait can potentially be implemented multiple times per entity, in case there are
 /// a variety of interesting searches.
 ///
-/// While the trait design does not require it, implementaitons should take advantage of
+/// While the trait design does not require it, implementations should take advantage of
 /// database features such as indices to ensure that searching by a search key is efficient.
 #[cfg_attr(target_family = "wasm", async_trait(?Send))]
 #[cfg_attr(not(target_family = "wasm"), async_trait)]
