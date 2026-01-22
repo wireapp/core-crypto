bf25335021efd02b9278f09981f407cd4e12c664
Peter Goodspeed-Niklaus <peter.goodspeed-niklaus@wire.com>
2026-01-22 11:15:28 +0100

    feat(keystore): sql: `impl SearchableEntity<ParentGroupid> for PersistedMlsGroup`
    Additionally, this cleans up the implementation a little, fixes a bug
    where the id wasn't de-hexed, and udpates the `child_groups` impl to be
    in terms of this implementation.

--- keystore/src/entities/platform/generic/mls/group.rs
+++ keystore/src/entities/platform/generic/mls/group.rs
@@ -1,51 +1,41 @@
+use async_trait::async_trait;
+
 use crate::{
     CryptoKeystoreResult,
-    entities::{PersistedMlsGroup, PersistedMlsGroupExt},
-    traits::{BorrowPrimaryKey as _, EntityBase as _},
+    entities::{ParentGroupId, PersistedMlsGroup, PersistedMlsGroupExt},
+    traits::{BorrowPrimaryKey, SearchableEntity},
 };
 
-#[async_trait::async_trait]
+#[async_trait]
+impl<'a> SearchableEntity<ParentGroupId<'a>> for PersistedMlsGroup {
+    async fn find_all_matching(
+        conn: &mut Self::ConnectionType,
+        parent_id: &ParentGroupId<'a>,
+    ) -> CryptoKeystoreResult<Vec<Self>> {
+        let parent_id = *parent_id.as_ref();
+        let mut conn = conn.conn().await;
+        let mut query = conn.prepare_cached("SELECT id, parent_id, state FROM mls_groups WHERE parent_id = ?")?;
+        query
+            .query_and_then([parent_id], |row| {
+                let id = row.get::<_, String>("id")?;
+                let id = hex::decode(id)?;
+                let parent_id = row.get("parent_id")?;
+                let state = row.get("state")?;
+
+                Ok(PersistedMlsGroup { id, state, parent_id })
+            })?
+            .collect::<Result<_, _>>()
+    }
+}
+
+#[async_trait]
 impl PersistedMlsGroupExt for PersistedMlsGroup {
     fn parent_id(&self) -> Option<&[u8]> {
         self.parent_id.as_deref()
     }
 
     async fn child_groups(&self, conn: &mut Self::ConnectionType) -> CryptoKeystoreResult<Vec<Self>> {
-        let id = self.borrow_primary_key();
-        let mut conn = conn.conn().await;
-        let transaction = conn.transaction()?;
-        let mut query = transaction.prepare_cached("SELECT rowid FROM mls_groups WHERE parent_id = ?")?;
-        let mut rows = query.query_map([id], |r| r.get(0))?;
-
-        let entities = rows.try_fold(Vec::new(), |mut acc, rowid_result| {
-            use std::io::Read as _;
-            let rowid = rowid_result?;
-
-            let mut blob = transaction.blob_open(rusqlite::MAIN_DB, "mls_groups", "id", rowid, true)?;
-            let mut id = vec![];
-            blob.read_to_end(&mut id)?;
-            blob.close()?;
-
-            let mut blob = transaction.blob_open(rusqlite::MAIN_DB, "mls_groups", "state", rowid, true)?;
-            let mut state = vec![];
-            blob.read_to_end(&mut state)?;
-            blob.close()?;
-
-            let mut parent_id = None;
-            // Ignore errors because null blobs cause errors on open
-            if let Ok(mut blob) = transaction.blob_open(rusqlite::MAIN_DB, "mls_groups", "parent_id", rowid, true) {
-                if !blob.is_empty() {
-                    let mut tmp = Vec::with_capacity(blob.len());
-                    blob.read_to_end(&mut tmp)?;
-                    parent_id.replace(tmp);
-                }
-                blob.close()?;
-            }
-
-            acc.push(Self { id, parent_id, state });
-            crate::CryptoKeystoreResult::Ok(acc)
-        })?;
-
-        Ok(entities)
+        let parent_id = self.borrow_primary_key().into();
+        Self::find_all_matching(conn, &parent_id).await
     }
 }
