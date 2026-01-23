8ef58c5118439c64f8b567f48b151816b68d5a5d
istankovic <istankovic@users.noreply.github.com>
2026-01-23 11:09:14 +0000

    deploy: e5600c2a4e87ee6e160c5b25b6cb35e663a6ec8e

--- main/rust/trait.impl/core/convert/trait.Into.js
+++ main/rust/trait.impl/core/convert/trait.Into.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["mls_crypto_provider",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.92.0/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/1.92.0/alloc/string/struct.String.html\" title=\"struct alloc::string::String\">String</a>&gt; for <a class=\"enum\" href=\"mls_crypto_provider/enum.MlsProviderError.html\" title=\"enum mls_crypto_provider::MlsProviderError\">MlsProviderError</a>"]]]]);
+    var implementors = Object.fromEntries([["mls_crypto_provider",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.0/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/1.93.0/alloc/string/struct.String.html\" title=\"struct alloc::string::String\">String</a>&gt; for <a class=\"enum\" href=\"mls_crypto_provider/enum.MlsProviderError.html\" title=\"enum mls_crypto_provider::MlsProviderError\">MlsProviderError</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
