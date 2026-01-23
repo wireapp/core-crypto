97f07ffa3b800b5263f0c4c9ffc3d6ab8d1696ee
Peter Goodspeed-Niklaus <peter.goodspeed-niklaus@wire.com>
2026-01-23 09:29:47 +0100

    feat(keystore): idb: `impl SearchableEntity<ParentGroupId> for PersistedMlsGroup`

--- keystore/src/entities/platform/wasm/mls/group.rs
+++ keystore/src/entities/platform/wasm/mls/group.rs
@@ -1,8 +1,75 @@
-use crate::entities::{PersistedMlsGroup, PersistedMlsGroupExt};
+use async_trait::async_trait;
+use idb::TransactionMode;
+use js_sys::Uint8Array;
+use wasm_bindgen::JsValue;
 
-#[async_trait::async_trait(?Send)]
+use crate::{
+    CryptoKeystoreResult,
+    connection::storage::WasmStorageWrapper,
+    entities::{ParentGroupId, PersistedMlsGroup, PersistedMlsGroupExt},
+    traits::{BorrowPrimaryKey as _, Decryptable, Decrypting as _, EntityBase as _, SearchableEntity},
+};
+
+#[async_trait(?Send)]
+impl<'a> SearchableEntity<ParentGroupId<'a>> for PersistedMlsGroup {
+    async fn find_all_matching(
+        conn: &mut Self::ConnectionType,
+        parent_id: &ParentGroupId<'a>,
+    ) -> CryptoKeystoreResult<Vec<Self>> {
+        let parent_id = *parent_id.as_ref();
+        let storage = conn.storage();
+
+        let decrypt_mls_group = |js_value: JsValue| -> CryptoKeystoreResult<PersistedMlsGroup> {
+            let encrypted_group =
+                serde_wasm_bindgen::from_value::<<PersistedMlsGroup as Decryptable>::DecryptableFrom>(js_value)?;
+            let group = encrypted_group.decrypt(&storage.cipher)?;
+            Ok(group)
+        };
+
+        match &storage.storage {
+            WasmStorageWrapper::InMemory(map) => {
+                // don't bother with fancy indexing for in-memory storage
+                let map = map.borrow();
+                let Some(entities) = map.get(Self::COLLECTION_NAME) else {
+                    return Ok(Vec::new());
+                };
+                entities
+                    .values()
+                    .cloned()
+                    .map(decrypt_mls_group)
+                    .filter(|mls_group_result| {
+                        // any decode / decryption error, or a matching group will make it through this filter
+                        mls_group_result.as_ref().ok().is_none_or(|mls_group| {
+                            mls_group
+                                .parent_id
+                                .as_ref()
+                                .is_some_and(|group_parent_id| group_parent_id == parent_id)
+                        })
+                    })
+                    .collect()
+            }
+            WasmStorageWrapper::Persistent(database) => {
+                // _do_ bother with fancy indexing for real storage
+                let tx = database.transaction(&[Self::COLLECTION_NAME], TransactionMode::ReadOnly)?;
+                let object_store = tx.object_store(Self::COLLECTION_NAME)?;
+                let idx = object_store.index("parent_id")?;
+
+                let parent_id = JsValue::from(Uint8Array::from(parent_id));
+                let entities = idx.get_all(Some(parent_id.into()), None)?.await?;
+                entities.into_iter().map(decrypt_mls_group).collect()
+            }
+        }
+    }
+}
+
+#[async_trait(?Send)]
 impl PersistedMlsGroupExt for PersistedMlsGroup {
     fn parent_id(&self) -> Option<&[u8]> {
         self.parent_id.as_deref()
+    }
+
+    async fn child_groups(&self, conn: &mut Self::ConnectionType) -> CryptoKeystoreResult<Vec<Self>> {
+        let parent_id = self.borrow_primary_key().into();
+        Self::find_all_matching(conn, &parent_id).await
     }
 }
