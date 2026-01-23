2e4da6ec6c8b71b6a634259f8d05a6aa26a133b4
SimonThormeyer <simon.thormeyer@wire.com>
2026-01-23 16:03:17 +0100

    docs(ts): ignore warnings about ubrn items not included in docs
    If it was possible to include them, we'd like to, but the generated
    typescript module doesn't export them.

--- crypto-ffi/bindings/js/typedoc-ignore-warnings.cjs
+++ crypto-ffi/bindings/js/typedoc-ignore-warnings.cjs
@@ -11,12 +11,21 @@ exports.load = function (app) {
 
     function suppressUniffiWarnings(originalFunc, message, ...args) {
         if (
-            (typeof message === "string" &&
-                (message.includes("UniffiAbstractObject.uniffiDestroy") ||
-                    message.includes("__type.new") ||
-                    message.includes("__type.create") ||
-                    message.includes("__type.defaults"))) ||
-            message.includes("PkiEnvironmentHooksError.__type")
+            typeof message === "string" &&
+            (message.includes("UniffiAbstractObject.uniffiDestroy") ||
+                message.includes("__type.new") ||
+                message.includes("__type.create") ||
+                message.includes("__type.defaults") ||
+                // Suppresses warnings about some items missing referenced by these error variants.
+                // Unfortunately, ubrn doesn't let us include them, because the emitted typescript code doesn't export
+                // them
+                message.includes("PkiEnvironmentHooksError.__type") ||
+                message.includes("CoreCryptoError.__type") ||
+                message.includes("MlsError.__type") ||
+                message.includes("ProteusError.__type") ||
+                message.includes("EpochChangedReportingError.__type") ||
+                message.includes("LoggingError.__type") ||
+                message.includes("NewHistoryClientReportingError.__type"))
         ) {
             // Ignore these specific ubrn warnings
             return;
