700b45411bc0ef50a47359f641e186e0405a8eea
Peter Goodspeed-Niklaus <peter.goodspeed-niklaus@wire.com>
2026-01-28 12:26:00 +0100

    fixup! feat(keystore): sql: `impl SearchableEntity<ParentGroupid> for PersistedMlsGroup`

--- keystore/src/entities/platform/generic/mls/group.rs
+++ keystore/src/entities/platform/generic/mls/group.rs
@@ -14,16 +14,15 @@ impl<'a> SearchableEntity<ParentGroupId<'a>> for PersistedMlsGroup {
     ) -> CryptoKeystoreResult<Vec<Self>> {
         let parent_id = *parent_id.as_ref();
         let mut conn = conn.conn().await;
-        let mut query = conn.prepare_cached("SELECT id, parent_id, state FROM mls_groups WHERE parent_id = ?")?;
-        query
-            .query_and_then([parent_id], |row| {
-                let id = row.get::<_, String>("id")?;
-                let id = hex::decode(id)?;
-                let parent_id = row.get("parent_id")?;
-                let state = row.get("state")?;
+        let mut stmt = conn.prepare_cached("SELECT id_hex, parent_id, state FROM mls_groups WHERE parent_id = ?")?;
+        stmt.query_and_then([parent_id], |row| {
+            let id = row.get::<_, String>("id_hex")?;
+            let id = hex::decode(id)?;
+            let parent_id = row.get("parent_id")?;
+            let state = row.get("state")?;
 
-                Ok(PersistedMlsGroup { id, state, parent_id })
-            })?
-            .collect::<Result<_, _>>()
+            Ok(PersistedMlsGroup { id, state, parent_id })
+        })?
+        .collect()
     }
 }
