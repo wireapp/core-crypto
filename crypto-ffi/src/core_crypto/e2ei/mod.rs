09f53cad05986efaa8b566e01fa5594c3ca15b07
SimonThormeyer <simon.thormeyer@wire.com>
2026-02-04 11:10:35 +0100

    feat(ffi): expose `e2ei_conversation_state()` on `CoreCrypto` [WPB-22282]

--- crypto-ffi/src/core_crypto/e2ei/mod.rs
+++ crypto-ffi/src/core_crypto/e2ei/mod.rs
@@ -1,6 +1,6 @@
-use core_crypto::RecursiveError;
+use core_crypto::{RecursiveError, mls::conversation::Conversation as _};
 
-use crate::{Ciphersuite, CoreCryptoFfi, CoreCryptoResult};
+use crate::{Ciphersuite, ConversationId, CoreCryptoFfi, CoreCryptoResult, E2eiConversationState};
 
 pub(crate) mod identities;
 
@@ -24,6 +24,23 @@ impl CoreCryptoFfi {
             .e2ei_is_enabled(ciphersuite.into())
             .await
             .map_err(RecursiveError::mls_client("checking if e2ei is enabled"))
+            .map_err(Into::into)
+    }
+
+    /// See [core_crypto::mls::conversation::Conversation::e2ei_conversation_state]
+    pub async fn e2ei_conversation_state(
+        &self,
+        conversation_id: &ConversationId,
+    ) -> CoreCryptoResult<E2eiConversationState> {
+        self.inner
+            .mls_session()
+            .await?
+            .get_raw_conversation(conversation_id.as_ref())
+            .await
+            .map_err(RecursiveError::mls_client("getting conversation by id"))?
+            .e2ei_conversation_state()
+            .await
+            .map(Into::into)
             .map_err(Into::into)
     }
 }
