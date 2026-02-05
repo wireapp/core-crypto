25c99786507fcbe8b81293f81f0748b3f6d6ca2e
SimonThormeyer <SimonThormeyer@users.noreply.github.com>
2026-02-05 14:15:48 +0000

    deploy: cd679dfca6ba6c364c3a29acbc665dd274158d86

---
+++ v9.2.0/rust/trait.impl/clap_builder/derive/trait.Args.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["decode",[["impl Args for <a class=\"struct\" href=\"decode/struct.App.html\" title=\"struct decode::App\">App</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[119]}
