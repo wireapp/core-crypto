a6f300d9f658f124984713188166a25aa4c9168f
SimonThormeyer <simon.thormeyer@wire.com>
2026-02-19 10:20:12 +0000

    build(gradle): use gradle-nexus publish plugin
    And configure acccording to
    https://github.com/gradle-nexus/publish-plugin?tab=readme-ov-file#publishing-to-maven-central-via-sonatype-central

--- crypto-ffi/bindings/settings.gradle.kts
+++ crypto-ffi/bindings/settings.gradle.kts
@@ -2,8 +2,12 @@ rootProject.name = "core-crypto-kotlin"
 
 pluginManagement {
     repositories {
+        gradlePluginPortal()
         google()
         mavenCentral()
+    }
+    plugins {
+        id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
     }
 }
 
