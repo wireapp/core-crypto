cc269e3a940285427be084241fc93bedeeaaa7d0
Peter Goodspeed-Niklaus <peter.goodspeed-niklaus@wire.com>
2026-02-20 10:37:56 +0100

    chore: set up correct cfgs to build correctly in wasm
    If we could, we'd skip this on wasm entirely, but we can't due to
    some peculiarities of the build system. But we can at least exlude
    it when building with the wasm feature, so we do.

--- crypto-ffi/src/database/mod.rs
+++ crypto-ffi/src/database/mod.rs
@@ -57,25 +57,38 @@ pub async fn in_memory_database(key: Arc<DatabaseKey>) -> CoreCryptoResult<Datab
 /// The copy will be encrypted with the same key as the source database.
 ///
 /// # Platform Support
-/// This method is only available on platforms using SQLCipher (iOS, Android, JVM, native).
+/// This method is only useful on platforms using SQLCipher (iOS, Android, JVM, native).
 /// On WASM platforms, this function will return an error.
 ///
 /// # Arguments
 /// * `database` - The database instance to export
 /// * `destination_path` - The file path where the database copy should be created
-///
-/// # Errors
-/// Returns an error if:
-/// - Called on WASM platform (not supported)
-/// - The database is in-memory (cannot export in-memory databases)
-/// - The destination path is invalid or not writable
-/// - The export operation fails
-#[uniffi::export]
+//
+// These feature flags are ugly, but here's how they work:
+//
+// - If the `wasm` feature is enabled, exlude this; we don't want it.
+// - If we're compiling for wasm without the `wasm` feature, we have to have a function body, so just return an error
+// - If we're compiling for non-wasm, we have the real impl
+//
+// This works out because we only ever build real wasm builds with the `wasm` feature flag enabled.
+// The error could therefore be an `unimplemented!` but it's harmless to return a real error in this case.
 #[cfg(not(feature = "wasm"))]
+#[cfg_attr(all(not(feature = "wasm"), target_family = "wasm"), expect(unused_variables))]
+#[uniffi::export]
 pub async fn export_database_copy(database: &Database, destination_path: &str) -> CoreCryptoResult<()> {
-    database
-        .0
-        .export_copy(destination_path)
-        .await
-        .map_err(CoreCryptoError::generic())
+    // we need a noop here for the case where we're compiling for wasm but without feature wasm
+    #[cfg(target_family = "wasm")]
+    {
+        Err(CoreCryptoError::ad_hoc(
+            "export_database_copy is not implemented for wasm",
+        ))
+    }
+    #[cfg(not(target_family = "wasm"))]
+    {
+        database
+            .0
+            .export_copy(destination_path)
+            .await
+            .map_err(CoreCryptoError::generic())
+    }
 }
