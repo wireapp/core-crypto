674871f514199050ef77692edfc43776e24b7ae9
coriolinus <coriolinus@users.noreply.github.com>
2026-03-09 14:04:51 +0000

    deploy: f5defcfe2b2d20236afbe2f0d8332cb913c378b2

--- main/rust/trait.impl/core_crypto_ffi/core_crypto/mls_transport/trait.MlsTransport.js
+++ main/rust/trait.impl/core_crypto_ffi/core_crypto/mls_transport/trait.MlsTransport.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["interop",[["impl <a class=\"trait\" href=\"core_crypto_ffi/core_crypto/mls_transport/trait.MlsTransport.html\" title=\"trait core_crypto_ffi::core_crypto::mls_transport::MlsTransport\">MlsTransport</a> for <a class=\"struct\" href=\"interop/util/struct.MlsTransportSuccessProvider.html\" title=\"struct interop::util::MlsTransportSuccessProvider\">MlsTransportSuccessProvider</a>",0]]]]);
+    const implementors = Object.fromEntries([["interop",[["impl MlsTransport for <a class=\"struct\" href=\"interop/util/struct.MlsTransportSuccessProvider.html\" title=\"struct interop::util::MlsTransportSuccessProvider\">MlsTransportSuccessProvider</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[387]}
+//{"start":59,"fragment_lengths":[215]}
