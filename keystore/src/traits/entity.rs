69145a2aee6258e83b8aa18eb50e0160bdf41bf6
Peter Goodspeed-Niklaus <peter.goodspeed-niklaus@wire.com>
2026-03-13 15:38:51 +0100

    feat(keystore): add new entity traits for unified db
    Major changes:
    
    1. everything is sync because sqlite is not async
    2. connection, transaction are specified explicitly instead of as associated types
    3. eliminated `EntityBase`

--- keystore/src/traits/entity.rs
+++ keystore/src/traits/entity.rs
@@ -1,6 +1,7 @@
 use std::borrow::Borrow;
 
 use async_trait::async_trait;
+use rusqlite::Connection;
 
 use crate::{
     CryptoKeystoreResult,
@@ -43,6 +44,41 @@ pub trait EntityGetBorrowed: Entity + BorrowPrimaryKey {
         conn: &mut Self::ConnectionType,
         key: &Self::BorrowedPrimaryKey,
     ) -> CryptoKeystoreResult<Option<Self>>
+    where
+        for<'pk> &'pk Self::BorrowedPrimaryKey: KeyType;
+}
+
+/// Something which can be stored in our database.
+///
+/// It has a primary key, which uniquely identifies it.
+//
+// During WPB-23775 we need to rename this and all other `Unified*` traits to just the bare form
+pub trait UnifiedEntity: PrimaryKey + Sized {
+    /// The table name for this entity
+    const COLLECTION_NAME: &'static str;
+
+    /// Get an entity by its primary key.
+    ///
+    /// For entites whose primary key has a distinct borrowed type, it is best to implement this as a direct
+    /// passthrough:
+    ///
+    /// ```rust,ignore
+    /// fn get(conn: &Connection, key: &Self::PrimaryKey) -> CoreCryptoKeystoreResult<Option<Self>> {
+    ///     Self::get_borrowed(conn, key).await
+    /// }
+    /// ```
+    fn get(conn: &Connection, key: &Self::PrimaryKey) -> CryptoKeystoreResult<Option<Self>>;
+
+    /// Count the number of entities of this type in the database.
+    fn count(conn: &Connection) -> CryptoKeystoreResult<u32>;
+
+    /// Retrieve all entities of this type from the database.
+    fn load_all(conn: &Connection) -> CryptoKeystoreResult<Vec<Self>>;
+}
+
+pub trait UnifiedEntityGetBorrowed: UnifiedEntity + BorrowPrimaryKey {
+    /// Get an entity by a borrowed form of its primary key.
+    fn get_borrowed(conn: &Connection, key: &Self::BorrowedPrimaryKey) -> CryptoKeystoreResult<Option<Self>>
     where
         for<'pk> &'pk Self::BorrowedPrimaryKey: KeyType;
 }
--- keystore/src/traits/unique_entity.rs
+++ keystore/src/traits/unique_entity.rs
@@ -1,17 +1,24 @@
 use async_trait::async_trait;
+use rusqlite::{Connection, Transaction};
 #[cfg(not(target_os = "unknown"))]
 use rusqlite::{OptionalExtension as _, ToSql, params};
 #[cfg(target_os = "unknown")]
 use serde::de::DeserializeOwned;
 
 #[cfg(not(target_os = "unknown"))]
-use crate::entities::{count_helper, count_helper_tx, delete_helper, load_all_helper};
+use crate::entities::{
+    count_helper, count_helper_tx, delete_helper, load_all_helper, unified_count_helper, unified_load_all_helper,
+};
 #[cfg(target_os = "unknown")]
 use crate::traits::{Decryptable, Decrypting, Encrypting, KeyType as _};
 use crate::{
     CryptoKeystoreResult,
     connection::{KeystoreDatabaseConnection, TransactionWrapper},
-    traits::{Entity, EntityBase, PrimaryKey, entity_database_mutation::EntityDatabaseMutation},
+    entities::{unified_count_helper_tx, unified_delete_helper},
+    traits::{
+        Entity, EntityBase, PrimaryKey, UnifiedEntity,
+        entity_database_mutation::{EntityDatabaseMutation, UnifiedEntityDatabaseMutation},
+    },
 };
 
 /// A unique entity can appear either 0 or 1 times in the database.
@@ -302,5 +309,171 @@ where
 
     async fn delete(tx: &Self::Transaction, id: &Self::PrimaryKey) -> CryptoKeystoreResult<bool> {
         delete_helper::<Self>(tx, "id", id).await
+    }
+}
+
+/// A unique entity can appear either 0 or 1 times in the database.
+pub trait UnifiedUniqueEntity: PrimaryKey {
+    /// The id used as they key when storing this entity in a KV store.
+    const KEY: Self::PrimaryKey;
+}
+
+/// Unique entities get some convenience methods implemented automatically.
+pub trait UnifiedUniqueEntityExt: UnifiedUniqueEntity + UnifiedEntityDatabaseMutation {
+    /// Get this unique entity from the database.
+    fn get_unique(conn: &Connection) -> CryptoKeystoreResult<Option<Self>>;
+
+    /// Set this unique entity into the database, replacing it if it already exists.
+    ///
+    /// Returns `true` if the entity previously existed and was replaced, or
+    /// `false` if it was not removed and this was a pure insertion.
+    fn set_and_replace(&self, tx: &Transaction) -> CryptoKeystoreResult<bool>;
+
+    /// Set this unique entity into the database if it does not already exist.
+    ///
+    /// Returns `true` if the entity was saved, or `false` if it aborted due to an already-existing entity.
+    fn set_if_absent(&self, tx: &Transaction) -> CryptoKeystoreResult<bool>;
+
+    /// Returns whether or not the database contains an instance of this unique entity.
+    fn exists(conn: &Connection) -> CryptoKeystoreResult<bool>;
+}
+
+impl<E> UnifiedUniqueEntityExt for E
+where
+    E: UnifiedUniqueEntity + UnifiedEntityDatabaseMutation + Sync,
+{
+    /// Get this unique entity from the database.
+    fn get_unique(conn: &Connection) -> CryptoKeystoreResult<Option<Self>> {
+        Self::get(conn, &Self::KEY)
+    }
+
+    /// Set this unique entity into the database, replacing it if it already exists.
+    ///
+    /// Returns `true` if the entity previously existed and was replaced, or
+    /// `false` if it was not removed and this was a pure insertion.
+    fn set_and_replace(&self, tx: &Transaction) -> CryptoKeystoreResult<bool> {
+        let deleted = Self::delete(tx, &Self::KEY)?;
+        self.save(tx)?;
+        Ok(deleted)
+    }
+
+    /// Set this unique entity into the database if it does not already exist.
+    ///
+    /// Returns `true` if the entity was saved, or `false` if it aborted due to an already-existing entity.
+    fn set_if_absent(&self, tx: &Transaction) -> CryptoKeystoreResult<bool> {
+        let count = <Self as UnifiedEntityDatabaseMutation>::count(tx)?;
+        if count > 0 {
+            return Ok(false);
+        }
+        self.save(tx)?;
+        Ok(true)
+    }
+
+    /// Returns whether or not the database contains an instance of this unique entity.
+    fn exists(conn: &Connection) -> CryptoKeystoreResult<bool> {
+        <Self as UnifiedEntity>::count(conn).map(|count| count > 0)
+    }
+}
+
+/// Unique entity implementation/migration helper.
+///
+/// The old `trait UniqueEntity` required two methods:
+///
+/// - `fn new(content: Vec<u8>) -> Self;`
+/// - `fn content(&self) -> &[u8];`
+///
+/// It then provided a bunch of default methods.
+///
+/// The whole structure of the traits has changed, now.
+/// But we can stil offer that same kind of convenience, and replicate those defaults.
+///
+/// If you implement this trait, you get the following traits auto-implemented:
+///
+/// - `PrimaryKey`
+/// - `UniqueEntity`
+/// - `Entity`
+/// - `EntityDatabaseMutation`
+///
+/// ## Warning
+///
+/// If your old `UniqueEntity` implementation defined anything other than the two required methods,
+/// you mut implement these traits manually to preserve the existing behaviors. Otherwise the
+/// behaviors will change!
+pub trait UnifiedUniqueEntityImplementationHelper {
+    /// Table name for this entity.
+    const COLLECTION_NAME: &str;
+    fn new(content: Vec<u8>) -> Self;
+    fn content(&self) -> &[u8];
+}
+
+// unfortunately we have to implement this trait twice, with nearly-identical but distinct bounds
+
+#[cfg(target_os = "unknown")]
+impl<T> UnifiedUniqueEntity for T
+where
+    T: UnifiedEntity + UnifiedUniqueEntityImplementationHelper + PrimaryKey<PrimaryKey = u32>,
+{
+    const KEY: u32 = 0;
+}
+
+#[cfg(not(target_os = "unknown"))]
+impl<T> UnifiedUniqueEntity for T
+where
+    T: UnifiedEntity + UnifiedUniqueEntityImplementationHelper + PrimaryKey<PrimaryKey = u64>,
+{
+    const KEY: u64 = 0;
+}
+
+impl<T> UnifiedEntity for T
+where
+    T: UnifiedEntity + UnifiedUniqueEntityImplementationHelper,
+    <T as PrimaryKey>::PrimaryKey: ToSql,
+{
+    const COLLECTION_NAME: &'static str = <Self as UnifiedUniqueEntityImplementationHelper>::COLLECTION_NAME;
+
+    fn get(conn: &Connection, key: &Self::PrimaryKey) -> CryptoKeystoreResult<Option<Self>> {
+        let mut statement = conn.prepare_cached(&format!(
+            "SELECT content FROM {collection_name} WHERE id = ?",
+            collection_name = <Self as UnifiedEntity>::COLLECTION_NAME
+        ))?;
+        statement
+            .query_row([key], |row| Ok(Self::new(row.get("content")?)))
+            .optional()
+            .map_err(Into::into)
+    }
+
+    /// Count the number of entities of this type in the database.
+    fn count(conn: &Connection) -> CryptoKeystoreResult<u32> {
+        unified_count_helper::<Self>(conn)
+    }
+
+    /// Retrieve all entities of this type from the database.
+    fn load_all(conn: &Connection) -> CryptoKeystoreResult<Vec<Self>> {
+        unified_load_all_helper::<Self, _>(conn, |row| Ok(Self::new(row.get("content")?)))
+    }
+}
+
+impl<T> UnifiedEntityDatabaseMutation for T
+where
+    T: UnifiedEntity + UniqueEntityImplementationHelper + UnifiedUniqueEntity,
+    <T as PrimaryKey>::PrimaryKey: ToSql,
+{
+    type AutoGeneratedFields = ();
+
+    fn save(&self, tx: &Transaction) -> CryptoKeystoreResult<()> {
+        let mut stmt = tx.prepare_cached(&format!(
+            "INSERT OR REPLACE INTO {collection_name} (id, content) VALUES (?, ?)",
+            collection_name = Self::COLLECTION_NAME,
+        ))?;
+        stmt.execute(params![Self::KEY, self.content()])?;
+        Ok(())
+    }
+
+    fn count(tx: &Transaction) -> CryptoKeystoreResult<u32> {
+        unified_count_helper_tx::<Self>(tx)
+    }
+
+    fn delete(tx: &Transaction, id: &Self::PrimaryKey) -> CryptoKeystoreResult<bool> {
+        unified_delete_helper::<Self>(tx, "id", id)
     }
 }
