c445e7f6dafd942f7b887488c48d347dc3d6498e
Peter Goodspeed-Niklaus <peter.goodspeed-niklaus@wire.com>
2026-03-23 12:01:46 +0100

    chore(ffi/ts): use new `Database.open` constructor not free func

--- crypto-ffi/bindings/js/test/bun/napi/utils.ts
+++ crypto-ffi/bindings/js/test/bun/napi/utils.ts
@@ -17,8 +17,8 @@ import {
     setMaxLogLevel,
     ClientId,
     Credential,
-    openDatabase,
 } from "@wireapp/core-crypto/native";
+import { Database } from "../../../src/browser/CoreCrypto";
 
 // Logging can be adjusted via the CC_TEST_LOG_LEVEL variable:
 // 0 = no logs
@@ -83,7 +83,7 @@ async function openTestDatabase(databaseName?: string) {
     const key = new DatabaseKey(keyBytes.buffer);
     const location = databaseName ?? `bun-test-db-${crypto.randomUUID()}`;
 
-    const database = await openDatabase(location, key);
+    const database = await Database.open(location, key);
 
     const resolvedLocation = await database.getLocation();
     assert(resolvedLocation !== undefined);
