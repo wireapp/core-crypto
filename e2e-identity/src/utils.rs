71bdd7dc10da954a18525d21bb0a9832590b5a25
Ivan Stanković <ivan.stankovic@wire.com>
2026-03-26 11:33:18 +0100

    chore: e2e-identity: add a utility to obtain a JWK public key from a PEM keypair

--- e2e-identity/src/utils.rs
+++ e2e-identity/src/utils.rs
@@ -1,5 +1,8 @@
-use jwt_simple::prelude::{ES256KeyPair, ES384KeyPair, ES512KeyPair, Ed25519KeyPair};
-use rusty_jwt_tools::prelude::{JwsAlgorithm, Pem};
+use jwt_simple::prelude::{ES256KeyPair, ES384KeyPair, ES512KeyPair, Ed25519KeyPair, Jwk};
+use rusty_jwt_tools::{
+    jwk::TryIntoJwk as _,
+    prelude::{JwsAlgorithm, Pem},
+};
 
 use crate::error::E2eIdentityResult;
 
@@ -11,4 +14,14 @@ pub fn generate_key(sign_alg: JwsAlgorithm) -> E2eIdentityResult<Pem> {
         JwsAlgorithm::Ed25519 => Ed25519KeyPair::generate().to_pem(),
     };
     Ok(pem.into())
+}
+
+pub fn public_jwk_from_pem_keypair(alg: JwsAlgorithm, keypair: &Pem) -> E2eIdentityResult<Jwk> {
+    let jwk = match alg {
+        JwsAlgorithm::P256 => ES256KeyPair::from_pem(keypair)?.public_key().try_into_jwk()?,
+        JwsAlgorithm::P384 => ES384KeyPair::from_pem(keypair)?.public_key().try_into_jwk()?,
+        JwsAlgorithm::P521 => ES512KeyPair::from_pem(keypair)?.public_key().try_into_jwk()?,
+        JwsAlgorithm::Ed25519 => Ed25519KeyPair::from_pem(keypair)?.public_key().try_into_jwk()?,
+    };
+    Ok(jwk)
 }
