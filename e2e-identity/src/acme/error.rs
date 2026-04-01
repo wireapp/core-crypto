c504270d500faf2a2dbf3cd912c39a7035eaac46
Ivan Stanković <ivan.stankovic@wire.com>
2026-04-01 16:45:41 +0200

    chore: e2e-identity: move CertificateError to the acquisition module
    This is where those errors belong and where they will be returned from.

--- e2e-identity/src/acme/error.rs
+++ e2e-identity/src/acme/error.rs
@@ -61,45 +61,4 @@ pub enum RustyAcmeError {
     /// UTF-8 parsing error
     #[error(transparent)]
     Utf8(#[from] std::str::Utf8Error),
-    /// Invalid/incomplete certificate
-    #[error(transparent)]
-    InvalidCertificate(#[from] CertificateError),
-}
-
-/// Given x509 certificate is invalid and does not follow Wire's format
-#[derive(Debug, thiserror::Error)]
-pub enum CertificateError {
-    /// ClientId does not match expected one
-    #[error("ClientId does not match expected one")]
-    ClientIdMismatch,
-    /// Display name does not match expected one
-    #[error("Display name does not match expected one")]
-    DisplayNameMismatch,
-    /// Handle does not match expected one
-    #[error("Handle does not match expected one")]
-    HandleMismatch,
-    /// Domain does not match expected one
-    #[error("Domain does not match expected one")]
-    DomainMismatch,
-    /// DisplayName is missing from the certificate
-    #[error("DisplayName is missing from the certificate")]
-    MissingDisplayName,
-    /// Handle is missing from the certificate
-    #[error("Handle is missing from the certificate")]
-    MissingHandle,
-    /// Domain is missing from the certificate
-    #[error("Domain is missing from the certificate")]
-    MissingDomain,
-    /// ClientId is missing from the certificate
-    #[error("ClientId is missing from the certificate")]
-    MissingClientId,
-    /// X509 lacks required standard fields
-    #[error("X509 lacks required standard fields")]
-    InvalidFormat,
-    /// Advertised public key does not match algorithm
-    #[error("Advertised public key does not match algorithm")]
-    InvalidPublicKey,
-    /// Advertised public key is not supported
-    #[error("Advertised public key is not supported")]
-    UnsupportedPublicKey,
 }
