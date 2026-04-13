06376e648c95a6f26396f3839d42a88b697ec4c0
Felix Werner <felix.werner@wire.com>
2026-04-13 12:51:31 +0200

    chore(ts): adjust src imports after move

--- crypto-ffi/bindings/js/packages/native/src/CoreCrypto.ts
+++ crypto-ffi/bindings/js/packages/native/src/CoreCrypto.ts
@@ -14,4 +14,4 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see http://www.gnu.org/licenses/.
 
-export * from "../shared/CoreCrypto";
+export * from "../../shared/src/CoreCrypto";
