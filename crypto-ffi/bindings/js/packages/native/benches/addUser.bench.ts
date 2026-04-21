93a2377580e27804929a820b661862c5006e10f8
Felix Werner <felix.werner@wire.com>
2026-04-21 17:01:00 +0200

    test(ts-native): add "Add User" Benchmark

---
+++ crypto-ffi/bindings/js/packages/native/benches/addUser.bench.ts
@@ -0,0 +1,101 @@
+import {
+    tinybench_setup,
+    userBenchmarkParameters,
+} from "../../shared/benches/utils";
+import {
+    Ciphersuite,
+    ClientId,
+    ConversationId,
+    type KeyPackage,
+    Credential,
+    CredentialType,
+} from "@wireapp/core-crypto/native";
+import { Bench } from "tinybench";
+import { setup, ccInit, teardown } from "../test/utils";
+
+async function run() {
+    await setup();
+    const parameters = await userBenchmarkParameters();
+
+    const bench = new Bench({
+        name: "Adding a User",
+        time: 1000,
+        iterations: 5,
+        warmupIterations: 1,
+        setup: tinybench_setup,
+        teardown: teardown,
+    });
+    for (const { userCount, cipherSuite } of parameters) {
+        bench.add(
+            `cipherSuite=${Ciphersuite[cipherSuite]} userCount=${userCount}`,
+            async () => {
+                const aliceId = new ClientId(
+                    Buffer.from(crypto.randomUUID()).buffer
+                );
+                const aliceCc = await ccInit(aliceId);
+
+                const aliceCredential = Credential.basic(cipherSuite, aliceId);
+
+                await aliceCc.transaction(async (ctx) => {
+                    await ctx.addCredential(aliceCredential);
+                });
+                const conversationIdStr = crypto.randomUUID();
+                const conversationId = new ConversationId(
+                    new TextEncoder().encode(conversationIdStr).buffer
+                );
+
+                await aliceCc.transaction(async (ctx) => {
+                    const [credentialRef] = await ctx.getCredentials();
+                    await ctx.createConversation(
+                        conversationId,
+                        credentialRef!
+                    );
+                });
+
+                const keyPackages: KeyPackage[] = [];
+
+                for (let i = 0; i < userCount; i++) {
+                    const bobId = new ClientId(
+                        Buffer.from(crypto.randomUUID()).buffer
+                    );
+                    const bobCc = await ccInit(bobId);
+
+                    const bobCredential = Credential.basic(cipherSuite, bobId);
+
+                    await bobCc.transaction(async (ctx) => {
+                        await ctx.addCredential(bobCredential);
+                    });
+
+                    const kp = await bobCc.transaction(async (ctx) => {
+                        const [credentialRef] = await ctx.findCredentials({
+                            ciphersuite: cipherSuite,
+                            credentialType: CredentialType.Basic,
+                        });
+                        return await ctx.generateKeyPackage(credentialRef!);
+                    });
+
+                    keyPackages.push(kp);
+                }
+
+                const start = bench.now();
+                await aliceCc.transaction(
+                    async (ctx) =>
+                        await ctx.addClientsToConversation(
+                            conversationId,
+                            keyPackages
+                        )
+                );
+                const end = bench.now();
+                return { overriddenDuration: end - start };
+            }
+        );
+    }
+
+    console.log(`Starting ${bench.name}`);
+    await bench.run();
+
+    console.log(bench.name);
+    console.table(bench.table());
+}
+
+await run();
