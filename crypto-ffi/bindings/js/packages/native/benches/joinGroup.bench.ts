ffdc22d0a8b5e8696d542364d8fb3500eabfe1d2
Felix Werner <felix.werner@wire.com>
2026-05-04 13:01:28 +0200

    test(ts-native): add "Join Group Benchmark"

---
+++ crypto-ffi/bindings/js/packages/native/benches/joinGroup.bench.ts
@@ -0,0 +1,91 @@
+import {
+    tinybench_setup,
+    userBenchmarkParameters,
+} from "../../shared/benches/utils";
+import { Ciphersuite, type KeyPackage } from "@wireapp/core-crypto/native";
+import { Bench } from "tinybench";
+import {
+    setup,
+    ccInit,
+    teardown,
+    DELIVERY_SERVICE,
+    createConversation,
+    generateKeyPackage,
+} from "../test/utils";
+
+async function run() {
+    await setup();
+    const parameters = await userBenchmarkParameters();
+
+    const bench = new Bench({
+        name: "Join Group",
+        time: 1000,
+        iterations: 5,
+        warmupIterations: 1,
+        setup: tinybench_setup,
+        teardown: teardown,
+    });
+    for (const { userCount, cipherSuite } of parameters) {
+        bench.add(
+            `cipherSuite=${Ciphersuite[cipherSuite]} userCount=${userCount}`,
+            async () => {
+                const aliceCc = await ccInit({
+                    withBasicCredential: true,
+                    cipherSuite,
+                });
+
+                const conversationId = await createConversation(aliceCc);
+
+                const keyPackages: KeyPackage[] = [];
+
+                if (userCount > 1) {
+                    for (let i = 0; i < userCount; i++) {
+                        const bobCc = await ccInit({
+                            withBasicCredential: true,
+                            cipherSuite,
+                        });
+                        const kp = await generateKeyPackage(bobCc, cipherSuite);
+                        keyPackages.push(kp);
+                    }
+
+                    await aliceCc.transaction(
+                        async (ctx) =>
+                            await ctx.addClientsToConversation(
+                                conversationId,
+                                keyPackages
+                            )
+                    );
+                }
+
+                const charlieCc = await ccInit({
+                    withBasicCredential: true,
+                    cipherSuite,
+                });
+                const kp = await generateKeyPackage(charlieCc, cipherSuite);
+                await aliceCc.transaction(
+                    async (ctx) =>
+                        await ctx.addClientsToConversation(conversationId, [kp])
+                );
+                const commitBundle =
+                    await DELIVERY_SERVICE.getLatestCommitBundle();
+
+                const start = bench.now();
+
+                await charlieCc.transaction((ctx) =>
+                    ctx.processWelcomeMessage(commitBundle.welcome!)
+                );
+
+                const end = bench.now();
+                return { overriddenDuration: end - start };
+            }
+        );
+    }
+
+    console.log(`Starting ${bench.name}`);
+    await bench.run();
+
+    console.log(bench.name);
+    console.table(bench.table());
+}
+
+await run();
