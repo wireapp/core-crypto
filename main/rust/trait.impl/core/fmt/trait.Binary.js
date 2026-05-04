2ccfba58a91944191dd6dafac993e70b8f07495b
SimonThormeyer <SimonThormeyer@users.noreply.github.com>
2026-05-04 08:16:29 +0000

    deploy: da50e39e16df1a36e4d2c838a301f98048df4bd2

--- main/rust/trait.impl/core/fmt/trait.Binary.js
+++ main/rust/trait.impl/core/fmt/trait.Binary.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["interop",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/fmt/trait.Binary.html\" title=\"trait core::fmt::Binary\">Binary</a> for <a class=\"struct\" href=\"interop/clients/struct.EmulatedClientProtocol.html\" title=\"struct interop::clients::EmulatedClientProtocol\">EmulatedClientProtocol</a>",0]]]]);
+    const implementors = Object.fromEntries([["interop",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/fmt/trait.Binary.html\" title=\"trait core::fmt::Binary\">Binary</a> for <a class=\"struct\" href=\"interop/clients/struct.EmulatedClientProtocol.html\" title=\"struct interop::clients::EmulatedClientProtocol\">EmulatedClientProtocol</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
