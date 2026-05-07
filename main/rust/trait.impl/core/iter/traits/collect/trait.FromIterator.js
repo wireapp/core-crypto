c9e23f36d3a8adefb1548f27da62f255eaae9133
fewerner <fewerner@users.noreply.github.com>
2026-05-07 08:09:08 +0000

    deploy: 7489d7beaee31823b41a9874e4c5f91caf22165e

--- main/rust/trait.impl/core/iter/traits/collect/trait.FromIterator.js
+++ main/rust/trait.impl/core/iter/traits/collect/trait.FromIterator.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["interop",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/iter/traits/collect/trait.FromIterator.html\" title=\"trait core::iter::traits::collect::FromIterator\">FromIterator</a>&lt;<a class=\"struct\" href=\"interop/clients/struct.EmulatedClientProtocol.html\" title=\"struct interop::clients::EmulatedClientProtocol\">EmulatedClientProtocol</a>&gt; for <a class=\"struct\" href=\"interop/clients/struct.EmulatedClientProtocol.html\" title=\"struct interop::clients::EmulatedClientProtocol\">EmulatedClientProtocol</a>",0]]]]);
+    const implementors = Object.fromEntries([["interop",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/iter/traits/collect/trait.FromIterator.html\" title=\"trait core::iter::traits::collect::FromIterator\">FromIterator</a>&lt;<a class=\"struct\" href=\"interop/clients/struct.EmulatedClientProtocol.html\" title=\"struct interop::clients::EmulatedClientProtocol\">EmulatedClientProtocol</a>&gt; for <a class=\"struct\" href=\"interop/clients/struct.EmulatedClientProtocol.html\" title=\"struct interop::clients::EmulatedClientProtocol\">EmulatedClientProtocol</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- main/rust/trait.impl/core/ops/arith/trait.Sub.js
+++ main/rust/trait.impl/core/ops/arith/trait.Sub.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["interop",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/ops/arith/trait.Sub.html\" title=\"trait core::ops::arith::Sub\">Sub</a> for <a class=\"struct\" href=\"interop/clients/struct.EmulatedClientProtocol.html\" title=\"struct interop::clients::EmulatedClientProtocol\">EmulatedClientProtocol</a>",0]]]]);
+    const implementors = Object.fromEntries([["interop",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/ops/arith/trait.Sub.html\" title=\"trait core::ops::arith::Sub\">Sub</a> for <a class=\"struct\" href=\"interop/clients/struct.EmulatedClientProtocol.html\" title=\"struct interop::clients::EmulatedClientProtocol\">EmulatedClientProtocol</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
