a8e5e9ea6c633ceb6ea63bfefadb53a11cece3fd
SimonThormeyer <simon.thormeyer@wire.com>
2026-05-22 11:27:52 +0200

    feat(e2ei): allow creating an X509 acquisition from pem
    This is temporary. See https://wearezeta.atlassian.net/wiki/x/RABtrQ.

--- e2e-identity/src/acquisition/initial.rs
+++ e2e-identity/src/acquisition/initial.rs
@@ -1,6 +1,7 @@
 use std::sync::Arc;
 
 use obfuscate::Obfuscated;
+use rusty_jwt_tools::prelude::Pem;
 
 use super::{X509CredentialAcquisition, X509CredentialConfiguration, states};
 use crate::{
@@ -25,6 +26,35 @@ impl X509CredentialAcquisition<states::Initialized> {
             config.sign_alg,
             config.acme_url
         );
+        Ok(Self {
+            pki_env,
+            config,
+            sign_kp,
+            acme_kp,
+            acme_jwk,
+            data: states::Initialized,
+        })
+    }
+
+    /// Create the acquisition object using an existing sign keypair.
+    /// This API is temporary until our system decouples client identities from a client's public signature key.
+    /// See <https://wearezeta.atlassian.net/wiki/x/RABtrQ>.
+    //
+    // We're intentionally not factoring this into `try_new()`, so that this can be removed more cleanly.
+    pub fn try_new_from_pem(
+        pki_env: Arc<PkiEnvironment>,
+        config: X509CredentialConfiguration,
+        sign_kp: Pem,
+    ) -> E2eIdentityResult<Self> {
+        let acme_kp = generate_key(config.sign_alg)?;
+        let acme_jwk = public_jwk_from_pem_keypair(config.sign_alg, &acme_kp)?;
+        log::info!(
+            "created acquisition from existing {:?}, sign_alg = {}, acme_url = {}",
+            Obfuscated::from(&sign_kp),
+            config.sign_alg,
+            config.acme_url
+        );
+
         Ok(Self {
             pki_env,
             config,
