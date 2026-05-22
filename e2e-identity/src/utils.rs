d37350c4d3bf57918c9da767b8210f45ed8bea7d
SimonThormeyer <simon.thormeyer@wire.com>
2026-05-22 11:29:00 +0200

    chore(e2ei): allow parsing `Pem` from bytes

--- e2e-identity/src/utils.rs
+++ e2e-identity/src/utils.rs
@@ -20,6 +20,16 @@ pub fn generate_key(sign_alg: JwsAlgorithm) -> E2eIdentityResult<Pem> {
     Ok(pem.into())
 }
 
+pub fn pem_from_bytes(bytes: &[u8], sign_alg: JwsAlgorithm) -> E2eIdentityResult<Pem> {
+    let pem = match sign_alg {
+        JwsAlgorithm::P256 => ES256KeyPair::from_bytes(bytes)?.to_pem()?,
+        JwsAlgorithm::P384 => ES384KeyPair::from_bytes(bytes)?.to_pem()?,
+        JwsAlgorithm::P521 => ES512KeyPair::from_bytes(bytes)?.to_pem()?,
+        JwsAlgorithm::Ed25519 => Ed25519KeyPair::from_bytes(bytes)?.to_pem(),
+    };
+    Ok(pem.into())
+}
+
 pub fn public_jwk_from_pem_keypair(alg: JwsAlgorithm, keypair: &Pem) -> E2eIdentityResult<Jwk> {
     let jwk = match alg {
         JwsAlgorithm::P256 => ES256KeyPair::from_pem(keypair)?.public_key().try_into_jwk()?,
