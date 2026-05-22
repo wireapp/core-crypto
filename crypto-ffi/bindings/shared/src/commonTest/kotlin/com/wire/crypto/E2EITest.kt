319c280fb2326044827dae5ad8c56ae9c6dab32b
SimonThormeyer <simon.thormeyer@wire.com>
2026-05-22 14:56:06 +0200

    test(kotlin): instantiating x509 acquisition from credential ref

--- crypto-ffi/bindings/shared/src/commonTest/kotlin/com/wire/crypto/E2EITest.kt
+++ crypto-ffi/bindings/shared/src/commonTest/kotlin/com/wire/crypto/E2EITest.kt
@@ -90,6 +90,33 @@ internal class E2EITest {
     }
 
     @Test
+    fun testInstantiateX509CredentialAcquisitionFromCredentialRef() = runTest {
+        val db = newDatabase()
+        val pkiEnv = PkiEnvironment.new(MockPkiEnvironmentHooks(), db)
+        val clientId = ClientId("LcksJb74Tm6N12cDjFy7lQ:8e6424430d3b28be@world.com".encodeToByteArray())
+        val config = X509CredentialAcquisitionConfiguration(
+            acmeUrl = "acme.example.com",
+            ciphersuite = CIPHERSUITE_DEFAULT,
+            displayName = "Alice Smith",
+            clientId = clientId,
+            handle = "alice_wire",
+            domain = "world.com",
+            team = null,
+            validityPeriodSecs = 3600uL
+        )
+
+        val cc = ccInit(CcInitOptions.WithBasicCredential(clientId = clientId, database = db))
+
+        val credentialRef = cc.transaction { ctx ->
+            ctx.findCredentials(clientId = clientId).first()
+        }
+
+        val acquisition = X509CredentialAcquisition.newFromCredentialRef(pkiEnv, config, credentialRef)
+
+        assertThat(acquisition).isNotNull
+    }
+
+    @Test
     fun conversation_should_be_not_verified_when_at_least_1_of_the_members_uses_a_Basic_credential() =
         runTest {
             val alice = ccInit()
