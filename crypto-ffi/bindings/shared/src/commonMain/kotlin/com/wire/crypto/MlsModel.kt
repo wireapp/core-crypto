084a6e18cddc8aabd554d2ae556964796e3d1cd5
Felix Werner <felix.werner@wire.com>
2026-05-26 15:02:00 +0200

    docs(kotlin): consistent cipher suite spelling

--- crypto-ffi/bindings/shared/src/commonMain/kotlin/com/wire/crypto/MlsModel.kt
+++ crypto-ffi/bindings/shared/src/commonMain/kotlin/com/wire/crypto/MlsModel.kt
@@ -1,9 +1,9 @@
 package com.wire.crypto
 
-/** The default ciphersuite: `MLS_128_DHKEMX25519_AES128GCM_SHA256_ED25519` */
+/** The default cipher suite: `MLS_128_DHKEMX25519_AES128GCM_SHA256_ED25519` */
 val CIPHERSUITE_DEFAULT = CipherSuite.MLS_128_DHKEMX25519_AES128GCM_SHA256_ED25519
 
-/** The default set of ciphersuites: `[MLS_128_DHKEMX25519_AES128GCM_SHA256_ED25519]` */
+/** The default set of cipher suites: `[MLS_128_DHKEMX25519_AES128GCM_SHA256_ED25519]` */
 val CIPHERSUITES_DEFAULT: List<CipherSuite> = listOf(CIPHERSUITE_DEFAULT)
 
 /** Default credential type */
