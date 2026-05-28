a0b1a72238e45a6642c45b5b2e3daee95b4fc465
coriolinus <coriolinus@users.noreply.github.com>
2026-05-28 13:30:53 +0000

    deploy: a70de9cbc299821429f1cf6fbc62ef474a08b3a0

--- main/rust/trait.impl/openmls_traits/trait.OpenMlsCryptoProvider.js
+++ main/rust/trait.impl/openmls_traits/trait.OpenMlsCryptoProvider.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["core_crypto",[["impl OpenMlsCryptoProvider for <a class=\"struct\" href=\"core_crypto/struct.MlsCryptoProvider.html\" title=\"struct core_crypto::MlsCryptoProvider\">MlsCryptoProvider</a>",0]]]]);
+    const implementors = Object.fromEntries([["core_crypto",[["impl OpenMlsCryptoProvider for <a class=\"struct\" href=\"core_crypto/struct.CryptoProvider.html\" title=\"struct core_crypto::CryptoProvider\">CryptoProvider</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[195]}
+//{"start":59,"fragment_lengths":[186]}
