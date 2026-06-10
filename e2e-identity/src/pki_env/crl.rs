668f96c0646834e4f76edc6599d079d42c128259
Ivan Stanković <ivan.stankovic@wire.com>
2026-06-10 14:51:40 +0200

    chore: e2e-identity: make sure to add a CrlStore when adding a new CRL

--- e2e-identity/src/pki_env/crl.rs
+++ e2e-identity/src/pki_env/crl.rs
@@ -5,7 +5,7 @@ use core_crypto_keystore::entities::E2eiCrl;
 use super::{Error, Result};
 use crate::{
     pki_env::{PkiEnvironment, hooks::HttpMethod},
-    x509_check::revocation::PkiEnvironment as RjtPkiEnvironment,
+    x509_check::revocation::{CrlStore, PkiEnvironment as RjtPkiEnvironment, now},
 };
 
 impl PkiEnvironment {
@@ -36,7 +36,14 @@ impl PkiEnvironment {
     /// Validate the CRL (trust anchors must be configured prior to this) and
     /// save it to the database.
     pub async fn save_crl(&self, crl_dp: &str, crl_der: &[u8]) -> Result<()> {
-        let crl = self.rjt_pki_env.lock().await.validate_crl_with_raw(crl_der)?;
+        let mut guard = self.rjt_pki_env.lock().await;
+        let crl = guard.validate_crl_with_raw(crl_der)?;
+
+        let crl_source = CrlStore::from([crl.clone()].as_slice());
+        crl_source.index_crls(now()?)?;
+
+        guard.add_crl_source(Box::new(crl_source));
+
         let crl_data = E2eiCrl {
             content: RjtPkiEnvironment::encode_crl_to_der(&crl)?,
             distribution_point: crl_dp.to_owned(),
