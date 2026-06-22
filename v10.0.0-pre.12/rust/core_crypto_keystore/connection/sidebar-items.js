ba9ab80105cdafa114a6fd99c4573662e72a9e54
coriolinus <coriolinus@users.noreply.github.com>
2026-06-22 15:42:14 +0000

    deploy: d02a7c22f9db19128b93d52d8aea291f2295dc26

---
+++ v10.0.0-pre.12/rust/core_crypto_keystore/connection/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"constant":["MAX_BLOB_LEN"],"enum":["ConnectionType"],"mod":["platform"],"struct":["ConnectionGuard","Database"],"trait":["DatabaseConnection","DatabaseConnectionRequirements"]};
