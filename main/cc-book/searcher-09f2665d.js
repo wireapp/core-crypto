4af52de9efb7f0b14a2ab19a59ae8c6d91372e5d
SimonThormeyer <SimonThormeyer@users.noreply.github.com>
2026-06-23 14:58:18 +0000

    deploy: 5fd3548be9656029ed62ee46287c807bf4869ffa

--- main/cc-book/searcher-09f2665d.js
+++ main/cc-book/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-45263d00.js',
+                path_to_root + 'searchindex-196e0233.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
