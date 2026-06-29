28c4f0f2037a4605525704b3b2e4516fbe84025c
Felix Werner <felix.werner@wire.com>
2026-06-29 09:27:07 +0200

    test(ts): migrate `metadata.test.ts`

--- crypto-ffi/bindings/js/shared/test/metadata.test.ts
+++ crypto-ffi/bindings/js/shared/test/metadata.test.ts
@@ -1,6 +1,6 @@
-import { browser, expect } from "@wdio/globals";
-import { setup, teardown } from "./utils";
+import { runOnPlatform, setup, teardown } from "./utils";
 import { afterEach, beforeEach, describe } from "mocha";
+import { expect } from "chai";
 
 beforeEach(async () => {
     await setup();
@@ -12,15 +12,16 @@ afterEach(async () => {
 
 describe("metadata", () => {
     it("metadata can be retrieved and contains key 'gitDescribe'", async () => {
-        await expect(
-            browser.execute(async () => ccModule.buildMetadata())
-        ).resolves.toHaveProperty("gitDescribe");
+        const result = await runOnPlatform(async () =>
+            ccModule.buildMetadata()
+        );
+        expect(result).to.haveOwnProperty("gitDescribe");
     });
 
     it("version can be retrieved and is a semantic version number", async () => {
-        await expect(
-            browser.execute(async () => ccModule.version())
-        ).resolves.toMatch(
+        const result = await runOnPlatform(async () => ccModule.version());
+
+        expect(result).to.match(
             RegExp(
                 // Regex for matching semantic versions from https://semver.org
                 "^(0|[1-9]\\d*)\\.(0|[1-9]\\d*)\\.(0|[1-9]\\d*)(?:-((?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\\.(?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\\+([0-9a-zA-Z-]+(?:\\.[0-9a-zA-Z-]+)*))?$"
