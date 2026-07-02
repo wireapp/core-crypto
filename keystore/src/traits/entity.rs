8e98f509c0598e17b696be988194a9aeb8bc2e98
Peter Goodspeed-Niklaus <peter.goodspeed-niklaus@wire.com>
2026-07-02 15:56:25 +0200

    refactor(keystore): remove the old entity traits
    We're deprecating those in favor of the new `UnifiedEntity` traits
    and will eventually update the names to remove the `Unified*` prefix.
    
    The old traits still exist and are implemented in the
    `idb_migration::legacy` module.

--- keystore/src/traits/entity.rs
+++ keystore/src/traits/entity.rs
@@ -1,52 +1,12 @@
-use std::borrow::Borrow;
-
-use async_trait::async_trait;
 use rusqlite::Connection;
 
 use crate::{
     CryptoKeystoreResult,
     traits::{
-        EntityBase, KeyType, OwnedKeyType,
+        KeyType,
         primary_key::{BorrowPrimaryKey, PrimaryKey},
     },
 };
-
-/// Something which can be stored in our database.
-///
-/// It has a primary key, which uniquely identifies it.
-#[cfg_attr(target_os = "unknown", async_trait(?Send))]
-#[cfg_attr(not(target_os = "unknown"), async_trait)]
-pub trait Entity: EntityBase + PrimaryKey {
-    /// Get an entity by its primary key.
-    ///
-    /// For entites whose primary key has a distinct borrowed type, it is best to implement this as a direct
-    /// passthrough:
-    ///
-    /// ```rust,ignore
-    /// async fn get(conn: &mut Self::ConnectionType, key: &Self::PrimaryKey) -> CoreCryptoKeystoreResult<Option<Self>> {
-    ///     Self::get_borrowed(conn, key).await
-    /// }
-    /// ```
-    async fn get(conn: &mut Self::ConnectionType, key: &Self::PrimaryKey) -> CryptoKeystoreResult<Option<Self>>;
-
-    /// Count the number of entities of this type in the database.
-    async fn count(conn: &mut Self::ConnectionType) -> CryptoKeystoreResult<u32>;
-
-    /// Retrieve all entities of this type from the database.
-    async fn load_all(conn: &mut Self::ConnectionType) -> CryptoKeystoreResult<Vec<Self>>;
-}
-
-#[cfg_attr(target_os = "unknown", async_trait(?Send))]
-#[cfg_attr(not(target_os = "unknown"), async_trait)]
-pub trait EntityGetBorrowed: Entity + BorrowPrimaryKey {
-    /// Get an entity by a borrowed form of its primary key.
-    async fn get_borrowed(
-        conn: &mut Self::ConnectionType,
-        key: &Self::BorrowedPrimaryKey,
-    ) -> CryptoKeystoreResult<Option<Self>>
-    where
-        for<'pk> &'pk Self::BorrowedPrimaryKey: KeyType;
-}
 
 /// Something which can be stored in our database.
 ///
