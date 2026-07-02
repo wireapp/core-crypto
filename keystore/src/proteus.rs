27ec10660c998df0e1a80a639b005fcfdb16f888
Peter Goodspeed-Niklaus <peter.goodspeed-niklaus@wire.com>
2026-07-02 16:18:59 +0200

    refactor(keystore): rm `trait CryptoKeystoreProteus` & update
    We'd previously had a pointless trait which was implemented on
    the database and on nothing else, even for testing. That wasn't
    helping anything, so we got rid of it.
    
    Also updates the proteus-specific methods to use the new entity traits.

--- keystore/src/proteus.rs
+++ keystore/src/proteus.rs
@@ -1,21 +1,11 @@
 use crate::{
-    CryptoKeystoreError, CryptoKeystoreResult, connection::Database, entities::ProteusPrekey,
-    traits::FetchFromDatabase as _,
+    CryptoKeystoreError, CryptoKeystoreResult, Database, entities::ProteusPrekey, traits::FetchFromDatabase as _,
 };
 
-#[cfg_attr(target_os = "unknown", async_trait::async_trait(?Send))]
-#[cfg_attr(not(target_os = "unknown"), async_trait::async_trait)]
-pub trait CryptoKeystoreProteus {
-    async fn proteus_store_prekey(&self, id: u16, prekey: &[u8]) -> CryptoKeystoreResult<()>;
-}
-
-#[cfg_attr(target_os = "unknown", async_trait::async_trait(?Send))]
-#[cfg_attr(not(target_os = "unknown"), async_trait::async_trait)]
-impl CryptoKeystoreProteus for Database {
-    async fn proteus_store_prekey(&self, id: u16, prekey: &[u8]) -> CryptoKeystoreResult<()> {
-        let entity = ProteusPrekey::from_raw(id, prekey.to_vec());
-        self.save(entity).await?;
-        Ok(())
+impl Database {
+    pub async fn proteus_store_prekey(&self, id: u16, prekey: &[u8]) -> CryptoKeystoreResult<()> {
+        self.with_transaction(async |tx| tx.save(ProteusPrekey::from_raw(id, prekey.to_vec())).await)
+            .await
     }
 }
 
@@ -27,10 +17,12 @@ impl proteus_traits::PreKeyStore for Database {
     async fn prekey(&self, id: proteus_traits::RawPreKeyId) -> Result<Option<proteus_traits::RawPreKey>, Self::Error> {
         self.get::<ProteusPrekey>(&id)
             .await
-            .map(|db_prekey| db_prekey.map(|mut db_prekey| std::mem::take(&mut db_prekey.prekey)))
+            .map(|maybe_prekey| maybe_prekey.map(|mut db_prekey| std::mem::take(&mut db_prekey.prekey)))
     }
 
     async fn remove(&self, id: proteus_traits::RawPreKeyId) -> Result<(), Self::Error> {
-        Database::remove::<ProteusPrekey>(self, &id).await
+        self.with_transaction(async |tx| tx.remove::<ProteusPrekey>(&id).await)
+            .await
+            .map(|_| ())
     }
 }
