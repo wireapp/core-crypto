2e48953b3d5e2eb8566e1fc70144fb8a10a2160a
typfel <typfel@users.noreply.github.com>
2026-07-09 10:03:00 +0000

    deploy: ca73fc98f2baef019f14427e45c372aef06cc1b1

---
+++ v10.1.0-pre1/rust/trait.impl/proteus_traits/trait.ProteusErrorCode.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["core_crypto_keystore",[["impl ProteusErrorCode for <a class=\"enum\" href=\"core_crypto_keystore/enum.CryptoKeystoreError.html\" title=\"enum core_crypto_keystore::CryptoKeystoreError\">CryptoKeystoreError</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[217]}
