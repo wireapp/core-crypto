cc795b106775957d078ea6d98f7ce8f798a94f7a
Emil Abramov <emil.abramov@wire.com>
2026-07-16 13:52:00 +0200

    feat: add ML-KEM-1024 + P-384 HPKE KEM
    Codepoint 0x0051 from draft-ietf-hpke-pq, a hybrid of ML-KEM-1024 and NIST P-384.

--- crypto/src/mls_provider/crypto_provider.rs
+++ crypto/src/mls_provider/crypto_provider.rs
@@ -511,6 +511,15 @@ impl OpenMlsCrypto for RustCrypto {
                     &mut hpke_rng,
                 )
             }
+            HpkeConfig(HpkeKemType::MlKem1024P384, HpkeKdfType::HkdfSha384, HpkeAeadType::AesGcm256) => {
+                hpke_core::hpke_seal::<hpke::aead::AesGcm256, hpke::kdf::HkdfSha384, hpke::kem::MlKem1024P384>(
+                    pk_r,
+                    info,
+                    aad,
+                    ptxt,
+                    &mut hpke_rng,
+                )
+            }
             _ => Err(CryptoError::UnsupportedKem),
         }
     }
@@ -599,6 +608,15 @@ impl OpenMlsCrypto for RustCrypto {
             }
             HpkeConfig(HpkeKemType::MlKem768P256, HpkeKdfType::HkdfSha384, HpkeAeadType::AesGcm256) => {
                 hpke_core::hpke_open::<hpke::aead::AesGcm256, hpke::kdf::HkdfSha384, hpke::kem::MlKem768P256>(
+                    sk_r,
+                    input.kem_output.as_slice(),
+                    info,
+                    aad,
+                    input.ciphertext.as_slice(),
+                )?
+            }
+            HpkeConfig(HpkeKemType::MlKem1024P384, HpkeKdfType::HkdfSha384, HpkeAeadType::AesGcm256) => {
+                hpke_core::hpke_open::<hpke::aead::AesGcm256, hpke::kdf::HkdfSha384, hpke::kem::MlKem1024P384>(
                     sk_r,
                     input.kem_output.as_slice(),
                     info,
@@ -698,6 +716,15 @@ impl OpenMlsCrypto for RustCrypto {
                         &mut hpke_rng,
                     )?
                 }
+                HpkeConfig(HpkeKemType::MlKem1024P384, HpkeKdfType::HkdfSha384, HpkeAeadType::AesGcm256) => {
+                    hpke_core::hpke_export_tx::<hpke::aead::AesGcm256, hpke::kdf::HkdfSha384, hpke::kem::MlKem1024P384>(
+                        pk_r,
+                        info,
+                        exporter_context,
+                        exporter_length,
+                        &mut hpke_rng,
+                    )?
+                }
                 _ => return Err(CryptoError::UnsupportedKem),
             };
 
@@ -789,6 +816,15 @@ impl OpenMlsCrypto for RustCrypto {
                         exporter_length,
                     )?
                 }
+                HpkeConfig(HpkeKemType::MlKem1024P384, HpkeKdfType::HkdfSha384, HpkeAeadType::AesGcm256) => {
+                    hpke_core::hpke_export_rx::<hpke::aead::AesGcm256, hpke::kdf::HkdfSha384, hpke::kem::MlKem1024P384>(
+                        enc,
+                        sk_r,
+                        info,
+                        exporter_context,
+                        exporter_length,
+                    )?
+                }
                 _ => return Err(CryptoError::UnsupportedKem),
             };
 
@@ -805,6 +841,7 @@ impl OpenMlsCrypto for RustCrypto {
             HpkeKemType::DhKem25519 => hpke_core::hpke_derive_keypair::<hpke::kem::X25519HkdfSha256>(ikm),
             HpkeKemType::MlKem768X25519 => hpke_core::hpke_derive_keypair::<hpke::kem::XWing>(ikm),
             HpkeKemType::MlKem768P256 => hpke_core::hpke_derive_keypair::<hpke::kem::MlKem768P256>(ikm),
+            HpkeKemType::MlKem1024P384 => hpke_core::hpke_derive_keypair::<hpke::kem::MlKem1024P384>(ikm),
             _ => Err(CryptoError::UnsupportedKem),
         }
     }
