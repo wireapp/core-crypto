b9d3f9da92bdcd07ceab708f3209163b5cd59079
Felix Werner <felix.werner@wire.com>
2026-07-17 15:56:14 +0200

    test(ts-native): add benchmark helpers
    1. set tinybench on globalThis to support sharing access patterns with
    browser
    
    2.  `runBenchmark` will setup any given benchmark and execute it

---
+++ crypto-ffi/bindings/js/packages/native/benches/utils.ts
@@ -0,0 +1,19 @@
+import {
+    logResults,
+    setup as sharedSetup,
+} from "../../../shared/benches/utils";
+
+async function setup() {
+    await sharedSetup();
+    if (globalThis.tinybench === undefined) {
+        globalThis.tinybench = await import("tinybench");
+    }
+}
+
+export async function runBenchmark(benchmarkSetup: () => Promise<void>) {
+    await setup();
+    await benchmarkSetup();
+    console.log(`Starting ${bench.name}`);
+    await bench.run();
+    await logResults(bench.name, bench.table());
+}
