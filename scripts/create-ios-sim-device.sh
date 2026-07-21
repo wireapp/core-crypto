18438b8a4c4efc1b592cd0302a94ccaa6bb77e98
Felix Werner <felix.werner@wire.com>
2026-07-21 14:44:00 +0200

    ci: use XCode 26.6 and iOS 26.5
    These are the version installed on our self-hosted runners.

--- scripts/create-ios-sim-device.sh
+++ scripts/create-ios-sim-device.sh
@@ -5,6 +5,6 @@ NAME="$1"
 
 UDID=$(xcrun simctl create "$NAME" \
         com.apple.CoreSimulator.SimDeviceType.iPhone-16 \
-        com.apple.CoreSimulator.SimRuntime.iOS-18-6)
+        com.apple.CoreSimulator.SimRuntime.iOS-26-5)
 
 echo "$UDID"
