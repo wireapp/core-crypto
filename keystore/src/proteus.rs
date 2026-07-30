ad0c495e66f2f381aa10d78457f10834f89c21d3
Peter Goodspeed-Niklaus <peter.goodspeed-niklaus@wire.com>
2026-07-30 14:40:20 +0200

    refactor(keystore): `Database` no longer impls `PreKeyStore`
    This removes the potential to accidentally hand a `Database` to proteus
    when it needs to write something; by forcing it to always hand a
    `Transaction` instead, we remove the capacity for a category of error.
    
    Unfortunately we can't do the same thing for MLS because OpenMLS ties
    together a bunch of foundational traits for no reason, so _not_ implementing
    `OpenMlsKeyStore` would mean that `Database` could not be a `KeyStoreProvider`,
    which means we couldn't pass in the `OpenMlsCryptoProvider` when we needed to.
    https://github.com/wireapp/openmls/blob/c9cde17076508968c9cbead5728454f0a1f60c4f/traits/src/traits.rs#L20

--- keystore/src/proteus.rs
+++ keystore/src/proteus.rs
@@ -1,28 +1,8 @@
-use crate::{
-    CryptoKeystoreError, CryptoKeystoreResult, Database, entities::ProteusPrekey, traits::FetchFromDatabase as _,
-};
+use crate::{CryptoKeystoreResult, Database, entities::ProteusPrekey};
 
 impl Database {
     pub async fn proteus_store_prekey(&self, id: u16, prekey: &[u8]) -> CryptoKeystoreResult<()> {
         self.with_transaction(async |tx| tx.save(ProteusPrekey::from_raw(id, prekey.to_vec())).await)
             .await
-    }
-}
-
-#[cfg_attr(target_os = "unknown", async_trait::async_trait(?Send))]
-#[cfg_attr(not(target_os = "unknown"), async_trait::async_trait)]
-impl proteus_traits::PreKeyStore for Database {
-    type Error = CryptoKeystoreError;
-
-    async fn prekey(&self, id: proteus_traits::RawPreKeyId) -> Result<Option<proteus_traits::RawPreKey>, Self::Error> {
-        self.get::<ProteusPrekey>(&id)
-            .await
-            .map(|maybe_prekey| maybe_prekey.map(|mut db_prekey| std::mem::take(&mut db_prekey.prekey)))
-    }
-
-    async fn remove(&self, id: proteus_traits::RawPreKeyId) -> Result<(), Self::Error> {
-        self.with_transaction(async |tx| tx.remove::<ProteusPrekey>(&id).await)
-            .await
-            .map(|_| ())
     }
 }
