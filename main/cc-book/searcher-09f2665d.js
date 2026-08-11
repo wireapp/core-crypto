40eca797829ed064603e225b7ed560ff9ed53b32
fewerner <fewerner@users.noreply.github.com>
2026-08-11 10:10:02 +0000

    deploy: 23a29fca4d28c462248b85fc1aaf007d7baa1294

--- main/cc-book/searcher-09f2665d.js
+++ main/cc-book/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-1093346c.js',
+                path_to_root + 'searchindex-bedc5987.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
