81af334974f9d6739f19d7dc8e40e70ca62c61ba
Peter Goodspeed-Niklaus <peter.goodspeed-niklaus@wire.com>
2026-08-12 12:12:05 +0200

    feat(keystore): add `Operation` type
    This is the first step toward a really consistent view of transaction
    operations. Given this type, the operation set is a simple
    `Vec<Operation>`, which means there is a single consistent ordering
    for operations. This in turn means that we can determine the transaction's
    state for a given entity by walking that list backwards,
    and checking for the final matching operation:
    
    - upsert -> read the upserted entity; that's the output
    - delete -> the entity was deleted
    - bulk-delete if the entity matches: the entity was deleted
    
    Of course there are further optimizations possible, but the existence
    of a simple algorithm lets us apply those optimization in turn.

--- keystore/src/transaction/dynamic_dispatch/mod.rs
+++ keystore/src/transaction/dynamic_dispatch/mod.rs
@@ -1,7 +1,9 @@
 mod entity;
 mod entity_id;
 mod entity_type;
+mod operation;
 
 pub(crate) use entity::Entity;
 pub(crate) use entity_id::EntityId;
 pub(crate) use entity_type::EntityType;
+pub(super) use operation::Operation;
