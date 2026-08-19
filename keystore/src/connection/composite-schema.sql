dd8ff50d236ad3412289668fa07933b9367e98c8
SimonThormeyer <simon.thormeyer@wire.com>
2026-08-19 14:17:46 +0200

    feat(keystore): add `targeted_message_rx_counters` table and corresponding struct

--- keystore/src/connection/composite-schema.sql
+++ keystore/src/connection/composite-schema.sql
@@ -106,3 +106,12 @@ CREATE TABLE tnt_secrets (
   PRIMARY KEY (conversation_id, epoch),
   FOREIGN KEY (conversation_id) REFERENCES mls_groups(id) ON DELETE CASCADE
 );
+
+CREATE TABLE targeted_message_rx_counters (
+  conversation_id BLOB NOT NULL,
+  sender INTEGER NOT NULL,
+  epoch INTEGER NOT NULL,
+  count INTEGER NOT NULL DEFAULT 0,
+  PRIMARY KEY (conversation_id, sender, epoch),
+  FOREIGN KEY (conversation_id) REFERENCES mls_groups(id) ON DELETE CASCADE
+);
