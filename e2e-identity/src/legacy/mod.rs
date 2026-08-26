1b3962a11aeed661ed0fc3b2992277edb0fc61c0
Peter Goodspeed-Niklaus <peter.goodspeed-niklaus@wire.com>
2026-08-26 14:34:34 +0200

    chore(e2ei): rm `legacy::CrlRegistration`

--- e2e-identity/src/legacy/mod.rs
+++ e2e-identity/src/legacy/mod.rs
@@ -2,12 +2,3 @@ pub mod device_status;
 pub mod id;
 
 use super::error::{E2eIdentityError as Error, E2eIdentityResult as Result};
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-/// Supporting struct for CRL registration result
-pub struct CrlRegistration {
-    /// Whether this CRL modifies the old CRL (i.e. has a different revoked cert list)
-    pub dirty: bool,
-    /// Optional expiration timestamp
-    pub expiration: Option<u64>,
-}
