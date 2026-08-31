41931120200c78113669e0d4024cef8e1627f312
Peter Goodspeed-Niklaus <peter.goodspeed-niklaus@wire.com>
2026-08-31 15:45:10 +0200

    refactor(keystore): simplify `FetchFromDatabase` for `Database` impl
    It's pretty easy now, actually.
    
    (cherry picked from commit b25bb56a45bb9c213c5ecb4549813d95fa7b815e)

--- keystore/src/connection/fetch_from_database.rs
+++ keystore/src/connection/fetch_from_database.rs
@@ -3,19 +3,9 @@ use std::sync::Arc;
 use async_trait::async_trait;
 
 use crate::{
-    CryptoKeystoreError, CryptoKeystoreResult, Database,
+    CryptoKeystoreResult, Database,
     traits::{BorrowPrimaryKey, Entity, EntityGetBorrowed, FetchFromDatabase, SearchableEntity},
-    transaction::ReadOutcome,
 };
-
-/// It's not an error if there is no transaction when fetching from database,
-/// so map that to an empty read outcome (no found entity, no filters).
-fn no_transaction_to_default_read_outcome<E>(err: CryptoKeystoreError) -> CryptoKeystoreResult<ReadOutcome<E>> {
-    match err {
-        CryptoKeystoreError::MutatingOperationWithoutTransaction => Ok(ReadOutcome::default()),
-        _ => Err(err),
-    }
-}
 
 #[cfg_attr(target_os = "unknown", async_trait(?Send))]
 #[cfg_attr(not(target_os = "unknown"), async_trait)]
@@ -24,19 +14,8 @@ impl FetchFromDatabase for Database {
     where
         E: 'static + Entity + Clone + Send + Sync,
     {
-        let read_outcome = self
-            .with_transaction(async |transaction| Ok(transaction.get::<E>(id).await))
-            .await
-            .or_else(no_transaction_to_default_read_outcome)?;
-
-        if let Some(record) = read_outcome.entity {
-            return Ok(record);
-        }
-
-        // Otherwise get it from the database
         let conn = self.conn().await;
-        let db_entity = E::get(&conn, id)?.filter(|entity| !read_outcome.should_omit(entity));
-
+        let db_entity = E::get(&conn, id)?;
         Ok(db_entity.map(Arc::new))
     }
 
@@ -47,18 +26,8 @@ impl FetchFromDatabase for Database {
     where
         E: 'static + EntityGetBorrowed + Clone + Send + Sync,
     {
-        let read_outcome = self
-            .with_transaction(async |transaction| Ok(transaction.get_borrowed::<E>(&id).await))
-            .await
-            .or_else(no_transaction_to_default_read_outcome)?;
-
-        if let Some(cached_record) = read_outcome.entity {
-            return Ok(cached_record);
-        }
-
-        // Otherwise get it from the database
         let conn = self.conn().await;
-        let db_entity = E::get_borrowed(&conn, id)?.filter(|entity| !read_outcome.should_omit(entity));
+        let db_entity = E::get_borrowed(&conn, id)?;
         Ok(db_entity.map(Arc::new))
     }
 
@@ -66,15 +35,8 @@ impl FetchFromDatabase for Database {
     where
         E: 'static + Entity + Clone + Send + Sync,
     {
-        if self.transaction.lock().await.is_some() {
-            // Unfortunately, we have to do this because of possible record id overlap
-            // between cache and db.
-            let count = self.load_all::<E>().await?.len();
-            Ok(count as _)
-        } else {
-            let conn = self.conn().await;
-            E::count(&conn)
-        }
+        let conn = self.conn().await;
+        E::count(&conn)
     }
 
     async fn load_all<E>(&self) -> CryptoKeystoreResult<Vec<Arc<E>>>
@@ -82,12 +44,8 @@ impl FetchFromDatabase for Database {
         E: 'static + Entity + Clone + Send + Sync,
     {
         let conn = self.conn().await;
-        let persisted_records = E::load_all(&conn)?.into_iter().map(Arc::new);
-
-        self.merge_with_transaction(persisted_records, async |transaction, persisted_records| {
-            Ok(transaction.find_all(persisted_records).await.collect())
-        })
-        .await
+        let items = E::load_all(&conn)?.into_iter().map(Arc::new).collect();
+        Ok(items)
     }
 
     async fn search<E, SearchKey>(&self, search_key: &SearchKey) -> CryptoKeystoreResult<Vec<Arc<E>>>
@@ -96,11 +54,10 @@ impl FetchFromDatabase for Database {
         SearchKey: Send + Sync + ?Sized,
     {
         let conn = self.conn().await;
-        let persisted_records = E::find_all_matching(&conn, search_key)?.into_iter().map(Arc::new);
-
-        self.merge_with_transaction(persisted_records, async |transaction, persisted_records| {
-            Ok(transaction.search(persisted_records, search_key).await.collect())
-        })
-        .await
+        let items = E::find_all_matching(&conn, search_key)?
+            .into_iter()
+            .map(Arc::new)
+            .collect();
+        Ok(items)
     }
 }
