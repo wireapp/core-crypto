c67be62ee3d7f244dab4102bebc54a915d603cff
Peter Goodspeed-Niklaus <peter.goodspeed-niklaus@wire.com>
2026-09-02 14:13:58 +0200

    refactor(keystore): move ancillary stuff to `ancillary` module
    They are not actually entities, after all.

--- keystore/src/ancillary/conversation_epochs_older_than.rs
+++ keystore/src/ancillary/conversation_epochs_older_than.rs
@@ -1,4 +1,4 @@
-use crate::entities::ConversationId;
+use crate::ancillary::ConversationId;
 
 /// Some data, e.g., [`TntSecret`][crate::entities::mls::TntSecret]s, need to be cleaned up if they belong to a
 /// conversation epoch that is older than a given epoch. To enable this, `SearchableEntity` is implemented with this
--- keystore/src/ancillary/message_rx_counter_key.rs
+++ keystore/src/ancillary/message_rx_counter_key.rs
@@ -1,19 +1,19 @@
-use crate::entities::{ConversationId, ConversationIdRef};
+use crate::ancillary::{ConversationId, ConversationIdRef};
 
 /// Owned key shared by the targeted and transient message receive counters.
 #[derive(Clone, Debug, Hash, PartialEq, Eq, derive_more::Constructor)]
 pub struct MessageRxCounterPk {
-    pub(super) conversation_id: ConversationId,
-    pub(super) sender_idx: u32,
-    pub(super) epoch: u64,
+    pub(crate) conversation_id: ConversationId,
+    pub(crate) sender_idx: u32,
+    pub(crate) epoch: u64,
 }
 
 /// Borrowed form of [`MessageRxCounterPk`].
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, derive_more::Constructor)]
 pub struct MessageRxCounterPkRef<'a> {
-    pub(super) conversation_id: &'a ConversationIdRef,
-    pub(super) sender_idx: u32,
-    pub(super) epoch: u64,
+    pub(crate) conversation_id: &'a ConversationIdRef,
+    pub(crate) sender_idx: u32,
+    pub(crate) epoch: u64,
 }
 
 impl From<MessageRxCounterPkRef<'_>> for MessageRxCounterPk {
