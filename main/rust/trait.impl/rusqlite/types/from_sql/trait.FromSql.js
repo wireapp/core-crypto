5ac14cd917807b42ee4c7e7418b5ef0ccce456e2
coriolinus <coriolinus@users.noreply.github.com>
2026-09-09 07:51:34 +0000

    deploy: 4973ec7564387010911796ce5c18aec1849e74da

--- main/rust/trait.impl/rusqlite/types/from_sql/trait.FromSql.js
+++ main/rust/trait.impl/rusqlite/types/from_sql/trait.FromSql.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["core_crypto_keystore",[["impl FromSql for <a class=\"struct\" href=\"core_crypto_keystore/ancillary/struct.ConversationId.html\" title=\"struct core_crypto_keystore::ancillary::ConversationId\">ConversationId</a>",0]]]]);
+    const implementors = Object.fromEntries([["core_crypto_keystore",[["impl FromSql for <a class=\"struct\" href=\"core_crypto_keystore/ancillary/struct.ConversationId.html\" title=\"struct core_crypto_keystore::ancillary::ConversationId\">ConversationId</a>",0],["impl FromSql for <a class=\"struct\" href=\"core_crypto_keystore/struct.Sha256Hash.html\" title=\"struct core_crypto_keystore::Sha256Hash\">Sha256Hash</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[220]}
+//{"start":59,"fragment_lengths":[381]}
