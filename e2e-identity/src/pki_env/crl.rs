40adc1156720ee700a3b1d84dfc5b07589049dc5
Ivan Stanković <ivan.stankovic@wire.com>
2026-09-11 15:41:11 +0200

    chore: e2e-identity: adjust the single caller of validate_crl_with_raw

--- e2e-identity/src/pki_env/crl.rs
+++ e2e-identity/src/pki_env/crl.rs
@@ -35,7 +35,7 @@ impl PkiEnvironment {
     pub async fn save_crl(&self, tx: &Transaction, crl_dp: &str, crl_der: &[u8]) -> Result<()> {
         let guard = self.rjt_pki_env.lock().await;
 
-        let crl = guard.validate_crl_with_raw(crl_der)?;
+        let crl = crate::x509_check::validate_crl(&guard, crl_der)?;
         guard.add_crl(crl_der, &crl, crl_dp).map_err(Error::Certval)?;
 
         let crl_data = X509Crl {
