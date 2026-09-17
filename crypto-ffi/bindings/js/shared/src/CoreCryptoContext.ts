3fe33c0cd22606b22f84317b15dd5ea80637fa29
SimonThormeyer <simon.thormeyer@wire.com>
2026-09-17 12:31:40 +0200

    refactor!(ffi/ts): deprecate `proteus_new_prekey()` [WPB-28571]
    In platforms using UBRN, we have to do this manually in the wrapper.

--- crypto-ffi/bindings/js/shared/src/CoreCryptoContext.ts
+++ crypto-ffi/bindings/js/shared/src/CoreCryptoContext.ts
@@ -9,4 +9,23 @@ export class CoreCryptoContext extends CoreCryptoContextFfi {
     static instanceOf(obj: unknown): obj is CoreCryptoContextFfi {
         return super.instanceOf(obj);
     }
+
+    /**
+     * Creates a new Proteus prekey with the given ID and returns its CBOR-serialized bundle.
+     *
+     * Fails if the ID is already in use. Prekeys are not replaceable: the ID has been published to
+     * peers in a bundle, and overwriting it would strand anyone still holding that bundle. Use
+     * `proteus_new_prekey_auto` to have a free ID chosen instead.
+     *
+     * Warning: the Proteus client must be initialized with `proteus_init` first or an error will be returned.
+     *
+     * @deprecated Use {@link CoreCryptoContext.proteusNewPrekeyAuto} instead.
+     */
+    // We're just overriding this to deprecrate it.
+    public proteusNewPrekey(
+        prekeyId: number,
+        asyncOpts_?: { signal: AbortSignal }
+    ): Promise<Uint8Array> {
+        return super.proteusNewPrekey(prekeyId, asyncOpts_);
+    }
 }
