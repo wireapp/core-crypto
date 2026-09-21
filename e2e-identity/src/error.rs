7da48f4615a9b2d33963b3bc91bf150fba041caa
Ivan Stanković <ivan.stankovic@wire.com>
2026-09-21 13:05:45 +0200

    chore: e2e-identity: remove doc comments from error variants
    They don't bring much value and we're already using allow(missing_docs).
    Also the comment about the Dpop token was misleading.

--- e2e-identity/src/error.rs
+++ e2e-identity/src/error.rs
@@ -4,16 +4,12 @@
 
 pub type E2eIdentityResult<T> = Result<T, E2eIdentityError>;
 
-/// All e2e identity related errors
 #[derive(Debug, thiserror::Error)]
 pub enum E2eIdentityError {
-    /// Acme error
     #[error(transparent)]
     AcmeError(#[from] crate::acme::RustyAcmeError),
-    /// Error creating the client Dpop token
     #[error(transparent)]
     JwtError(#[from] rusty_jwt_tools::prelude::RustyJwtError),
-    /// Core JWT error
     #[error(transparent)]
     JwtSimpleError(#[from] jwt_simple::Error),
     #[error(transparent)]
