49bf703fa554d116d9835a388d59f6db4061442e
Ivan Stanković <ivan.stankovic@wire.com>
2026-09-22 10:36:33 +0200

    chore: e2e-identity: acme: expose functions used in the ACME flow

--- e2e-identity/src/acme/mod.rs
+++ e2e-identity/src/acme/mod.rs
@@ -9,14 +9,15 @@ mod identifier;
 mod jws;
 mod order;
 
-pub(crate) use account::AcmeAccount;
-pub(crate) use authz::AcmeAuthzError;
-pub(crate) use chall::{AcmeChallenge, AcmeChallengeType};
-pub(crate) use directory::AcmeDirectory;
+pub(crate) use account::{AcmeAccount, new_account_request, new_account_response};
+pub(crate) use authz::{AcmeAuthzError, new_authz_request, new_authz_response};
+pub(crate) use certificate::{certificate_req, certificate_response};
+pub(crate) use chall::{AcmeChallenge, AcmeChallengeType, dpop_chall_request, new_chall_response, oidc_chall_request};
+pub(crate) use directory::{AcmeDirectory, acme_directory_response};
 pub(crate) use error::{RustyAcmeError, RustyAcmeResult};
-pub(crate) use finalize::AcmeFinalize;
+pub(crate) use finalize::{AcmeFinalize, finalize_req, finalize_response};
 pub(crate) use identifier::AcmeIdentifier;
 pub(crate) use jws::AcmeJws;
-pub(crate) use order::AcmeOrder;
+pub(crate) use order::{AcmeOrder, new_order_request, new_order_response};
 
 pub(crate) struct RustyAcme;
